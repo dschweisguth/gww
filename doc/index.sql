ff0b8a568f612b19b77c6121edd12859b4d5505a
Dave Schweisguth <dave@schweisguth.org>
2010-01-19 21:54:00 -0800

    Added index.sql until I'm ready to add a proper migration

---
+++ doc/index.sql
@@ -0,0 +1,5 @@
+create index photos_person_id on photos(person_id);
+create index guesses_person_id on guesses(person_id);
+create index photos_flickrid on photos(flickrid);
+create index guesses_photo_id on guesses(photo_id);
+create index people_flickrid on people(flickrid);
