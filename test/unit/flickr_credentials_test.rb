5bab4bf7266ee7c37b3e7dddc7ae909cb4cf3eba
Dave Schweisguth <dave@schweisguth.org>
2010-01-24 21:13:37 -0800

    Corrected asserts

--- test/unit/flickr_credentials_test.rb
+++ test/unit/flickr_credentials_test.rb
@@ -1,8 +1,8 @@
 class FlickrCredentialsTest < Test::Unit::TestCase
     def test_all
 	credentials = FlickrCredentials.new
-	assert_not_nil ! credentials.secret.empty?
-	assert_not_nil ! credentials.api_key.empty?
-	assert_not_nil ! credentials.auth_token.empty?
+	assert ! credentials.secret.empty?
+	assert ! credentials.api_key.empty?
+	assert ! credentials.auth_token.empty?
     end
 end
