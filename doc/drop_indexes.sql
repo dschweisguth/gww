539632928ba2ccd6172a57aed85796f4991f1fa5
Dave Schweisguth <dave@schweisguth.org>
2010-01-25 22:52:55 -0800

    Now that all one production instances have used doc/drop_indexes.sql and run migration 11, remove the foromer

--- doc/drop_indexes.sql
+++
@@ -1,5 +0,0 @@
-alter table photos drop index photos_person_id;
-alter table guesses drop index guesses_person_id;
-alter table photos drop index photos_flickrid;
-alter table guesses drop index guesses_photo_id;
-alter table people drop index people_flickrid;
