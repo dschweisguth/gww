943315e0f3d2fadc7e1d541794992c397c4fe69f
Dave Schweisguth <dave@schweisguth.org>
2010-01-31 10:33:03 -0800

    Removed unused people bits. Reordered methods in PeopleController.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,12 +1,6 @@
 class PeopleController < ApplicationController
-  def index
-    list
-    render :action => 'list'
-  end
-
   # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
-  verify :method => :post, :only => [ :destroy, :create, :update ],
-         :redirect_to => { :action => :list }
+  verify :method => :post, :only => [], :redirect_to => { :action => :list }
 
   def list
     raw_people = Person.find(:all)
@@ -22,6 +16,180 @@ class PeopleController < ApplicationController
       @people.push(add_person)
     end
     @people.sort! {|x,y| y[:guesscount] <=> x[:guesscount]}
+  end
+
+  def show
+    @person = Person.find(params[:id])
+    
+    # put together a lookup of who has guessed the photos this person posted
+    raw_photos = Photo.find_all_by_person_id(params[:id])
+    @posted_count = raw_photos.length
+    photos_by_guesser = {}
+    # step through the photos
+    raw_photos.each do |photo|
+      # find all the guesses on this photo
+      photo_guesses = Guess.find_all_by_photo_id(photo[:id])
+      # step through the guesses (usually just one)
+      photo_guesses.each do |guess|
+        guess_person = Person.find(guess[:person_id])
+        if !photos_by_guesser[guess_person]
+          photos_by_guesser[guess_person] = []
+        end
+        photos_by_guesser[guess_person].push(photo)
+      end
+    end
+    @photo_lookup = []
+    photos_by_guesser.each do |person, photos|
+      @photo_lookup.push({:username => person[:username], :person_id => person[:id], :photos => photos, :count => photos.length})
+    end
+    @photo_lookup.sort! {|x,y| y[:count] <=> x[:count]}
+    
+    # get unfound & revealed lists
+    @unfound_photos = Photo.find_all_by_person_id_and_game_status(params[:id], 'unfound')
+    @unfound_photos.concat(Photo.find_all_by_person_id_and_game_status(params[:id], 'unconfirmed'))
+    @revealed_photos = Photo.find_all_by_person_id_and_game_status(params[:id], 'revealed')
+    
+    missing_person = Person.new
+    missing_person[:username] = 'unknown'
+    # and one of who has posted the photos this person guessed
+    raw_guesses = Guess.find_all_by_person_id(params[:id])
+    @guessed_count = raw_guesses.length
+    guessed_photos_by_poster = {}
+    raw_guesses.each do |guess|
+      guess_photo = Photo.find(guess[:photo_id])
+      if guess_photo[:person_id] == 0
+        poster = missing_person
+      else
+        poster = Person.find(guess_photo[:person_id])
+      end
+      if !guessed_photos_by_poster[poster]
+        guessed_photos_by_poster[poster] = []
+      end
+      guessed_photos_by_poster[poster].push(guess_photo)
+    end
+    @guess_lookup = []
+    guessed_photos_by_poster.each do |person, photos|
+      @guess_lookup.push({:username => person[:username], :person_id => person[:id], :photos => photos, :count => photos.length})
+    end
+    @guess_lookup.sort! {|x,y| y[:count] <=> x[:count]}
+  end
+
+  def latest_guesses
+    @person = Person.find(params[:id])
+    @guesses = Guess.find(:all, :conditions => ["person_id = ?", params[:id]])
+    @guesses = Guess.find_all_by_person_id(params[:id])
+    @guesses.sort! {|x,y| y[:guessed_at] <=> x[:guessed_at]}
+  end
+
+  def latest_posts
+    @person = Person.find(params[:id])
+    @photos = Photo.find_all_by_person_id(params[:id])
+    @photos.sort! {|x,y| y[:dateadded] <=> x[:dateadded]}
+  end
+  
+  def commented_on
+    @person = Person.find(params[:id])
+    @comments = Comment.find_all_by_userid(@person[:flickrid])
+    @photos = []
+    @comments.each do |comment|
+      photo = Photo.find(comment[:photo_id])
+      @photos.push(photo) if !@photos.include?(photo)
+    end
+    @photos.sort! {|x,y| y[:lastupdate] <=> x[:lastupdate]}
+  end
+  
+  def guesses
+    lastupdate = FlickrUpdate.find(:all).last
+    lasttime = lastupdate[:updated_at] - 28800;
+    
+    @last_days = []
+    (1..7).each do |num|
+      dates = {:begin => (lasttime - num.day).beginning_of_day, :end => (lasttime - (num - 1).day).beginning_of_day}
+      scores = get_scores_from_date(dates[:begin], dates[:end])
+      @last_days.push({:dates => dates, :scores => scores})
+    end
+    
+    thisweek_dates = {:begin => lasttime.beginning_of_week - 1.day, :end => lasttime}
+    thisweek_scores = get_scores_from_date(thisweek_dates[:begin], nil)
+    @last_weeks = [{:dates => thisweek_dates, :scores => thisweek_scores}]
+    (1..5).each do |num|
+      dates = {
+        :begin => (lasttime - num.week).beginning_of_week - 1.day,
+        :end => (lasttime - (num - 1).week).beginning_of_week - 1.day
+      }
+      scores = get_scores_from_date(dates[:begin], dates[:end])
+      @last_weeks.push({:dates => dates, :scores => scores})
+    end
+    
+    thismonth_dates = {:begin => lasttime.beginning_of_month, :end => lasttime}
+    thismonth_scores = get_scores_from_date(thismonth_dates[:begin], nil)
+    @last_months = [{:dates => thismonth_dates, :scores => thismonth_scores}]
+    (1..5).each do |num|
+      dates = {
+        :begin => (lasttime - num.month).beginning_of_month,
+        :end => (lasttime - (num - 1).month).beginning_of_month
+      }
+      scores = get_scores_from_date(dates[:begin], dates[:end])
+      @last_months.push({:dates => dates, :scores => scores})
+    end
+    
+    thisyear_dates = {:begin => lasttime.beginning_of_year, :end => lasttime}
+    thisyear_scores = get_scores_from_date(thisyear_dates[:begin], nil)
+    @last_years = [{:dates => thisyear_dates, :scores => thisyear_scores}]
+    (1..2).each do |num|
+      dates = {
+        :begin => (lasttime - num.year).beginning_of_year,
+        :end => (lasttime - (num - 1).year).beginning_of_year
+      }
+      scores = get_scores_from_date(dates[:begin], dates[:end])
+      @last_years.push({:dates => dates, :scores => scores})
+    end
+    
+    # get the date
+    @report_date = lasttime
+  end
+  
+  def get_scores_from_date(begin_date,end_date)
+    if begin_date && end_date
+      guesses = Guess.find(:all, :conditions => ["guessed_at > ? and guessed_at < ?", begin_date, end_date])
+    elsif begin_date
+      guesses = Guess.find(:all, :conditions => ["guessed_at > ?", begin_date])
+    else
+      guesses = Guess.find(:all)
+    end
+    good_guesses = []
+    guessers = {}
+    guesses.each do |guess|
+      guessers[guess[:person_id]] ||= []
+      guessers[guess[:person_id]].push(guess)
+    end
+
+    return_people = []
+    guessers.each do |person_id, guesses|
+      # create an object descriptive of this person
+      add_person = {
+        :person => Person.find(person_id),
+        :guesscount => guesses.length
+      }
+      # step through existing entries in return_people
+      found = nil
+      return_people.each do |person_list|
+        # if we find an item in the array with the same guess count
+        if person_list[:guesscount] == add_person[:guesscount]
+          # add this person to the list
+          person_list[:people].push(add_person)
+          found = :true
+          break
+        end
+      end
+      # if it wasn't found
+      if !found
+        # create a new entry
+        return_people.push({:guesscount => add_person[:guesscount], :people => [add_person]})
+      end
+    end
+    # sort & return people lists
+    return_people.sort! {|x,y| y[:guesscount] <=> x[:guesscount]}
   end
 
   def report
@@ -111,210 +279,4 @@ class PeopleController < ApplicationController
     XmlSimple.xml_in(page_xml)['group'][0]['members'][0]
   end
 
-  def get_scores_from_date(begin_date,end_date)
-    if begin_date && end_date
-      guesses = Guess.find(:all, :conditions => ["guessed_at > ? and guessed_at < ?", begin_date, end_date])
-    elsif begin_date
-      guesses = Guess.find(:all, :conditions => ["guessed_at > ?", begin_date])
-    else
-      guesses = Guess.find(:all)
-    end
-    good_guesses = []
-    guessers = {}
-    guesses.each do |guess|
-      guessers[guess[:person_id]] ||= []
-      guessers[guess[:person_id]].push(guess)
-    end
-
-    return_people = []
-    guessers.each do |person_id, guesses|
-      # create an object descriptive of this person
-      add_person = {
-        :person => Person.find(person_id),
-        :guesscount => guesses.length
-      }
-      # step through existing entries in return_people
-      found = nil
-      return_people.each do |person_list|
-        # if we find an item in the array with the same guess count
-        if person_list[:guesscount] == add_person[:guesscount]
-          # add this person to the list
-          person_list[:people].push(add_person)
-          found = :true
-          break
-        end
-      end
-      # if it wasn't found
-      if !found
-        # create a new entry
-        return_people.push({:guesscount => add_person[:guesscount], :people => [add_person]})
-      end
-    end
-    # sort & return people lists
-    return_people.sort! {|x,y| y[:guesscount] <=> x[:guesscount]}
-  end
-
-  def guesses
-    lastupdate = FlickrUpdate.find(:all).last
-    lasttime = lastupdate[:updated_at] - 28800;
-    
-    @last_days = []
-    (1..7).each do |num|
-      dates = {:begin => (lasttime - num.day).beginning_of_day, :end => (lasttime - (num - 1).day).beginning_of_day}
-      scores = get_scores_from_date(dates[:begin], dates[:end])
-      @last_days.push({:dates => dates, :scores => scores})
-    end
-    
-    thisweek_dates = {:begin => lasttime.beginning_of_week - 1.day, :end => lasttime}
-    thisweek_scores = get_scores_from_date(thisweek_dates[:begin], nil)
-    @last_weeks = [{:dates => thisweek_dates, :scores => thisweek_scores}]
-    (1..5).each do |num|
-      dates = {
-        :begin => (lasttime - num.week).beginning_of_week - 1.day,
-        :end => (lasttime - (num - 1).week).beginning_of_week - 1.day
-      }
-      scores = get_scores_from_date(dates[:begin], dates[:end])
-      @last_weeks.push({:dates => dates, :scores => scores})
-    end
-    
-    thismonth_dates = {:begin => lasttime.beginning_of_month, :end => lasttime}
-    thismonth_scores = get_scores_from_date(thismonth_dates[:begin], nil)
-    @last_months = [{:dates => thismonth_dates, :scores => thismonth_scores}]
-    (1..5).each do |num|
-      dates = {
-        :begin => (lasttime - num.month).beginning_of_month,
-        :end => (lasttime - (num - 1).month).beginning_of_month
-      }
-      scores = get_scores_from_date(dates[:begin], dates[:end])
-      @last_months.push({:dates => dates, :scores => scores})
-    end
-    
-    thisyear_dates = {:begin => lasttime.beginning_of_year, :end => lasttime}
-    thisyear_scores = get_scores_from_date(thisyear_dates[:begin], nil)
-    @last_years = [{:dates => thisyear_dates, :scores => thisyear_scores}]
-    (1..2).each do |num|
-      dates = {
-        :begin => (lasttime - num.year).beginning_of_year,
-        :end => (lasttime - (num - 1).year).beginning_of_year
-      }
-      scores = get_scores_from_date(dates[:begin], dates[:end])
-      @last_years.push({:dates => dates, :scores => scores})
-    end
-    
-    # get the date
-    @report_date = lasttime
-  end
-  
-  def latest_guesses
-    @person = Person.find(params[:id])
-    @guesses = Guess.find(:all, :conditions => ["person_id = ?", params[:id]])
-    @guesses = Guess.find_all_by_person_id(params[:id])
-    @guesses.sort! {|x,y| y[:guessed_at] <=> x[:guessed_at]}
-  end
-
-  def latest_posts
-    @person = Person.find(params[:id])
-    @photos = Photo.find_all_by_person_id(params[:id])
-    @photos.sort! {|x,y| y[:dateadded] <=> x[:dateadded]}
-  end
-  
-  def commented_on
-    @person = Person.find(params[:id])
-    @comments = Comment.find_all_by_userid(@person[:flickrid])
-    @photos = []
-    @comments.each do |comment|
-      photo = Photo.find(comment[:photo_id])
-      @photos.push(photo) if !@photos.include?(photo)
-    end
-    @photos.sort! {|x,y| y[:lastupdate] <=> x[:lastupdate]}
-  end
-  
-  def show
-    @person = Person.find(params[:id])
-    
-    # put together a lookup of who has guessed the photos this person posted
-    raw_photos = Photo.find_all_by_person_id(params[:id])
-    @posted_count = raw_photos.length
-    photos_by_guesser = {}
-    # step through the photos
-    raw_photos.each do |photo|
-      # find all the guesses on this photo
-      photo_guesses = Guess.find_all_by_photo_id(photo[:id])
-      # step through the guesses (usually just one)
-      photo_guesses.each do |guess|
-        guess_person = Person.find(guess[:person_id])
-        if !photos_by_guesser[guess_person]
-          photos_by_guesser[guess_person] = []
-        end
-        photos_by_guesser[guess_person].push(photo)
-      end
-    end
-    @photo_lookup = []
-    photos_by_guesser.each do |person, photos|
-      @photo_lookup.push({:username => person[:username], :person_id => person[:id], :photos => photos, :count => photos.length})
-    end
-    @photo_lookup.sort! {|x,y| y[:count] <=> x[:count]}
-    
-    # get unfound & revealed lists
-    @unfound_photos = Photo.find_all_by_person_id_and_game_status(params[:id], 'unfound')
-    @unfound_photos.concat(Photo.find_all_by_person_id_and_game_status(params[:id], 'unconfirmed'))
-    @revealed_photos = Photo.find_all_by_person_id_and_game_status(params[:id], 'revealed')
-    
-    missing_person = Person.new
-    missing_person[:username] = 'unknown'
-    # and one of who has posted the photos this person guessed
-    raw_guesses = Guess.find_all_by_person_id(params[:id])
-    @guessed_count = raw_guesses.length
-    guessed_photos_by_poster = {}
-    raw_guesses.each do |guess|
-      guess_photo = Photo.find(guess[:photo_id])
-      if guess_photo[:person_id] == 0
-        poster = missing_person
-      else
-        poster = Person.find(guess_photo[:person_id])
-      end
-      if !guessed_photos_by_poster[poster]
-        guessed_photos_by_poster[poster] = []
-      end
-      guessed_photos_by_poster[poster].push(guess_photo)
-    end
-    @guess_lookup = []
-    guessed_photos_by_poster.each do |person, photos|
-      @guess_lookup.push({:username => person[:username], :person_id => person[:id], :photos => photos, :count => photos.length})
-    end
-    @guess_lookup.sort! {|x,y| y[:count] <=> x[:count]}
-  end
-
-  def new
-    @person = Person.new
-  end
-
-  def create
-    @person = Person.new(params[:person])
-    if @person.save
-      flash[:notice] = 'Person was successfully created.'
-      redirect_to :action => 'list'
-    else
-      render :action => 'new'
-    end
-  end
-
-  def edit
-    @person = Person.find(params[:id])
-  end
-
-  def update
-    @person = Person.find(params[:id])
-    if @person.update_attributes(params[:person])
-      flash[:notice] = 'Person was successfully updated.'
-      redirect_to :action => 'show', :id => @person
-    else
-      render :action => 'edit'
-    end
-  end
-
-  def destroy
-    Person.find(params[:id]).destroy
-    redirect_to :action => 'list'
-  end
 end
