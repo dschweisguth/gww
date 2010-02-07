17e2f5ab6e0cc07cc4225bb5e0cbbdbecdc7fc9e
Dave Schweisguth <dave@schweisguth.org>
2010-02-07 13:56:20 -0800

    Added counts of things in reports to index page

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,13 @@
 class IndexController < ApplicationController
   def index
     @last_update_time = last_update_time
+    @unfound_photos_count = Photo.count(:all,
+      :conditions => "game_status in ('unfound', 'unconfirmed')")
+    @unverified_photos_count = Photo.count(:all,
+      :conditions =>
+        ["seen_at < ? and game_status in ('unfound', 'unconfirmed')",
+          adj_last_update_time])
+    @guesses_count = Guess.count();
+    @people_count = Person.count();
   end
 end
