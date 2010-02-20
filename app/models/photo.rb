8609d090b09bf6104e97179cbde679b5abe13739
Dave Schweisguth <dave@schweisguth.org>
2010-02-20 13:37:01 -0800

    Extracted photo page and image URL construction into Photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,4 +3,15 @@ class Photo < ActiveRecord::Base
   has_many :guesses
   has_many :comments
   has_one :revelation
+
+  def page_url
+    "http://www.flickr.com/photos/" + person.flickrid + "/" + flickrid +
+      "/in/pool-guesswheresf/";
+  end
+
+  def image_url(size)
+    "http://static.flickr.com/" + server + "/" + flickrid + "_" + secret +
+      "_" + size + ".jpg"
+  end
+
 end
