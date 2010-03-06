32d9a44b9df3ca2e10c2b729ec074b0df5d51375
Dave Schweisguth <dave@schweisguth.org>
2010-03-06 07:53:01 -0800

    Turned off log colorization

--- config/environment.rb
+++ config/environment.rb
@@ -39,6 +39,7 @@ Rails::Initializer.run do |config|
   config.active_record.default_timezone = :utc
   
   # See Rails::Configuration for more options
+  config.active_record.colorize_logging = false
 end
 
 # Add new inflection rules using the following format 
