c6c5e1ad6ef78fdd3e145b1955ee3bf44d2d6583
Dave Schweisguth <dave@schweisguth.org>
2010-03-07 10:05:07 -0800

    Dropped more bogus defaults

--- db/development_structure.sql
+++ db/development_structure.sql
@@ -7,14 +7,14 @@ CREATE TABLE `comments` (
   `comment_text` text,
   PRIMARY KEY  (`id`),
   KEY `comments_photo_id_index` (`photo_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=316409 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=316375 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `flickr_updates` (
   `id` int(11) NOT NULL auto_increment,
   `created_at` datetime NOT NULL,
   `completed_at` datetime default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=844 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=843 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `guesses` (
   `id` int(11) NOT NULL auto_increment,
@@ -26,42 +26,42 @@ CREATE TABLE `guesses` (
   PRIMARY KEY  (`id`),
   KEY `guesses_person_id_index` (`person_id`),
   KEY `guesses_photo_id_index` (`photo_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21955 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=21956 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `people` (
   `id` int(11) NOT NULL auto_increment,
-  `flickrid` varchar(255) NOT NULL default '',
-  `username` varchar(255) NOT NULL default '',
+  `flickrid` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `people_flickrid_index` (`flickrid`)
-) ENGINE=InnoDB AUTO_INCREMENT=1088 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1086 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `photos` (
   `id` int(11) NOT NULL auto_increment,
-  `flickrid` varchar(255) NOT NULL default '',
-  `secret` varchar(255) NOT NULL default '',
-  `server` varchar(255) NOT NULL default '',
-  `dateadded` datetime NOT NULL default '0000-00-00 00:00:00',
-  `lastupdate` datetime NOT NULL default '0000-00-00 00:00:00',
-  `seen_at` datetime NOT NULL default '0000-00-00 00:00:00',
-  `game_status` varchar(255) NOT NULL default '',
-  `flickr_status` varchar(255) NOT NULL default '',
-  `mapped` varchar(255) NOT NULL default '',
-  `person_id` int(11) NOT NULL default '0',
-  `farm` varchar(255) NOT NULL default '',
+  `flickrid` varchar(255) NOT NULL,
+  `secret` varchar(255) NOT NULL,
+  `server` varchar(255) NOT NULL,
+  `dateadded` datetime NOT NULL,
+  `lastupdate` datetime NOT NULL,
+  `seen_at` datetime NOT NULL,
+  `game_status` varchar(255) NOT NULL,
+  `flickr_status` varchar(255) NOT NULL,
+  `mapped` varchar(255) NOT NULL,
+  `person_id` int(11) NOT NULL,
+  `farm` varchar(255) NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `photos_person_id_index` (`person_id`),
   KEY `photos_flickrid_index` (`flickrid`),
   KEY `photos_person_id_dateadded` (`person_id`,`dateadded`),
   KEY `photos_game_status_index` (`game_status`)
-) ENGINE=InnoDB AUTO_INCREMENT=25220 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=25138 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `revelations` (
   `id` int(11) NOT NULL auto_increment,
-  `revelation_text` varchar(255) NOT NULL default '',
-  `revealed_at` datetime NOT NULL default '0000-00-00 00:00:00',
-  `photo_id` int(11) NOT NULL default '0',
-  `person_id` int(11) NOT NULL default '0',
+  `revelation_text` varchar(255) NOT NULL,
+  `revealed_at` datetime NOT NULL,
+  `photo_id` int(11) NOT NULL,
+  `person_id` int(11) NOT NULL,
   `added_at` datetime NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=368 DEFAULT CHARSET=latin1;
@@ -70,4 +70,4 @@ CREATE TABLE `schema_info` (
   `version` int(11) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-INSERT INTO schema_info (version) VALUES (20)
+INSERT INTO schema_info (version) VALUES (21)
--- test/unit/guess_test.rb
+++ test/unit/guess_test.rb
@@ -14,21 +14,25 @@ class GuessTest < Test::Unit::TestCase
     photo1.lastupdate = now
     photo1.seen_at = now
     photo1.dateadded = 10.seconds.ago
+    photo1.person = person1
     photo1.save
 
     photo2 = Photo.new
     photo2.lastupdate = now
     photo2.seen_at = now
     photo2.dateadded = 5.seconds.ago
+    photo2.person = person2
     photo2.save
 
     @guess1 = Guess.new
+    @guess1.person = person1
     @guess1.photo = photo2
     @guess1.guessed_at = now
     @guess1.added_at = now
     @guess1.save
 
     @guess2 = Guess.new
+    @guess2.person = person2
     @guess2.photo = photo1
     @guess2.guessed_at = now
     @guess2.added_at = now
