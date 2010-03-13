1ef08424e72903165ccee79f950ecee31484edff
Dave Schweisguth <dave@schweisguth.org>
2010-03-13 10:52:14 -0800

    Reordered columns

--- db/development_structure.sql
+++ db/development_structure.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `comments` (
   `id` int(11) NOT NULL auto_increment,
-  `username` varchar(255) NOT NULL,
-  `flickrid` varchar(255) NOT NULL,
-  `commented_at` datetime NOT NULL,
   `photo_id` int(11) NOT NULL,
+  `flickrid` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
   `comment_text` text NOT NULL,
+  `commented_at` datetime NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `comments_photo_id_index` (`photo_id`),
   CONSTRAINT `comments_photo_id_fk` FOREIGN KEY (`photo_id`) REFERENCES `photos` (`id`)
@@ -19,10 +19,10 @@ CREATE TABLE `flickr_updates` (
 
 CREATE TABLE `guesses` (
   `id` int(11) NOT NULL auto_increment,
-  `guessed_at` datetime NOT NULL,
   `photo_id` int(11) NOT NULL,
   `person_id` int(11) NOT NULL,
   `guess_text` text,
+  `guessed_at` datetime NOT NULL,
   `added_at` datetime NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `guesses_person_id_index` (`person_id`),
@@ -43,16 +43,16 @@ CREATE TABLE `people` (
 
 CREATE TABLE `photos` (
   `id` int(11) NOT NULL auto_increment,
+  `person_id` int(11) NOT NULL,
   `flickrid` varchar(255) NOT NULL,
-  `secret` varchar(255) NOT NULL,
+  `farm` varchar(255) NOT NULL,
   `server` varchar(255) NOT NULL,
+  `secret` varchar(255) NOT NULL,
   `dateadded` datetime NOT NULL,
+  `mapped` enum('false','true') NOT NULL,
   `lastupdate` datetime NOT NULL,
   `seen_at` datetime NOT NULL,
   `game_status` enum('unfound','unconfirmed','found','revealed') NOT NULL,
-  `mapped` enum('true','false') NOT NULL,
-  `person_id` int(11) NOT NULL,
-  `farm` varchar(255) NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `photos_flickrid_unique` (`flickrid`),
   KEY `photos_person_id_index` (`person_id`),
@@ -63,9 +63,9 @@ CREATE TABLE `photos` (
 
 CREATE TABLE `revelations` (
   `id` int(11) NOT NULL auto_increment,
+  `photo_id` int(11) NOT NULL,
   `revelation_text` varchar(255) NOT NULL,
   `revealed_at` datetime NOT NULL,
-  `photo_id` int(11) NOT NULL,
   `added_at` datetime NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `revelations_photo_id_unique` (`photo_id`),
@@ -76,4 +76,4 @@ CREATE TABLE `schema_info` (
   `version` int(11) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-INSERT INTO schema_info (version) VALUES (30)
+INSERT INTO schema_info (version) VALUES (31)
