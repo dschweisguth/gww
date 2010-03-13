6cd0476d1c102230173acaeeac52ba5f686fe154
Dave Schweisguth <dave@schweisguth.org>
2010-03-13 09:57:15 -0800

    Renamed comments.userid to flickrid for consistency

--- db/development_structure.sql
+++ db/development_structure.sql
@@ -1,14 +1,14 @@
 CREATE TABLE `comments` (
   `id` int(11) NOT NULL auto_increment,
   `username` varchar(255) NOT NULL,
-  `userid` varchar(255) NOT NULL,
+  `flickrid` varchar(255) default NULL,
   `commented_at` datetime NOT NULL,
   `photo_id` int(11) NOT NULL,
   `comment_text` text,
   PRIMARY KEY  (`id`),
   KEY `comments_photo_id_index` (`photo_id`),
   CONSTRAINT `comments_photo_id_fk` FOREIGN KEY (`photo_id`) REFERENCES `photos` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=321637 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=321641 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `flickr_updates` (
   `id` int(11) NOT NULL auto_increment,
@@ -29,7 +29,7 @@ CREATE TABLE `guesses` (
   KEY `guesses_photo_id_index` (`photo_id`),
   CONSTRAINT `guesses_person_id_fk` FOREIGN KEY (`person_id`) REFERENCES `people` (`id`),
   CONSTRAINT `guesses_photo_id_fk` FOREIGN KEY (`photo_id`) REFERENCES `photos` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22285 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=22286 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `people` (
   `id` int(11) NOT NULL auto_increment,
@@ -76,4 +76,4 @@ CREATE TABLE `schema_info` (
   `version` int(11) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-INSERT INTO schema_info (version) VALUES (28)
+INSERT INTO schema_info (version) VALUES (29)
