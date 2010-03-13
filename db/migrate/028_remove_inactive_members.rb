5690ebe0b9ee726e59ec704ef7bb5271b9a68fa3
Dave Schweisguth <dave@schweisguth.org>
2010-03-13 09:11:34 -0800

    Removed people with no guesses or posts

---
+++ db/migrate/028_remove_inactive_members.rb
@@ -0,0 +1,16 @@
+class RemoveInactiveMembers < ActiveRecord::Migration
+  def self.up
+    Person.delete(439)
+    Person.delete(402)
+  end
+
+  def self.down
+    if RAILS_ENV != 'test'
+      execute "insert into people (flickrid, username) values ('12037949663@N01', 'caterina')"
+      execute "insert into people (flickrid, username) values ('26575274@N00', 'spot20')"
+    end
+    execute "update people set id = 439 where username = 'caterina'"
+    execute "update people set id = 402 where username = 'spot20'"
+  end
+
+end
