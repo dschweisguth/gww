b826b580d226856e310f8766f2a599042a282777
Dave Schweisguth <dave@schweisguth.org>
2010-03-14 14:24:13 -0700

    Fix error when cache is cleared twice in a row without adding anything

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
 
   def expire_cached_pages
-    FileUtils.rm_r RAILS_ROOT + "/public/cache"
+    cache_dir = RAILS_ROOT + "/public/cache"
+    if File.exist? cache_dir
+      FileUtils.rm_r cache_dir
+    end
   end
 
 end
