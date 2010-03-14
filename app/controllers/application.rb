6b04caba535810340e97d23cb9cf2c53ba0c41b2
Dave Schweisguth <dave@schweisguth.org>
2010-03-14 12:44:38 -0700

    Moved pages to expire into data structure

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  CACHED_PAGES = {
+    :index => [ :index ]
+  }
+
   def expire_cached_pages
-    expire_page :controller => "index", :action => "index"
+    CACHED_PAGES.each do |controller, actions|
+      actions.each do |action|
+        expire_page :controller => controller.to_s, :action => action.to_s
+      end
+    end
   end
+
 end
