496ad4d7392396edda1a180ca123bc58f16671aa
Dave Schweisguth <dave@schweisguth.org>
2010-04-10 11:35:16 -0700

    Fixed test

--- test/unit/flickr_update_test.rb
+++ test/unit/flickr_update_test.rb
@@ -2,11 +2,15 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class FlickrUpdateTest < Test::Unit::TestCase
   def test_latest
-    FlickrUpdate.new.save
+    update1 = FlickrUpdate.new
+    update1.member_count = 1
+    update1.save
     # Times from the database don't have usec. Sleep so that the following
     # update is at least one second later than the previous.
     sleep 1
-    FlickrUpdate.new.save
+    update2 = FlickrUpdate.new
+    update2.member_count = 2
+    update2.save
     latest = FlickrUpdate.latest
 
     updates = FlickrUpdate.find :all, :order => :created_at
