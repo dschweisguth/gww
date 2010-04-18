67a5162bffa2cb93927bf9c38e34d3fc6d6acf1e
Dave Schweisguth <dave@schweisguth.org>
2010-04-17 17:48:44 -0700

    Made edit bookmarklet work on photos/show, too

--- public/javascripts/bookmarklets/editInGWW.js
+++ public/javascripts/bookmarklets/editInGWW.js
@@ -1,10 +1,16 @@
 function editInGWW() {
   var match = /^http:\/\/www.flickr.com\/photos\/[^/]+\/\d+/.exec(window.location);
-  if (match == null) {
-    alert('Try it on a Flickr photo page.')
-  } else {
+  if (match != null) {
     window.location = "http://guesswheresf.org/photos/edit_in_gww?from=" +
-      encodeURIComponent(window.location)
+      encodeURIComponent(window.location);
+  } else {
+    match = /^(http:\/\/[^/]+\/photos\/)show(\/\d+)/.exec(window.location);
+    if (match != null) {
+      window.location = match[1] + 'edit' + match[2];
+    } else {
+      alert('Try it on a Flickr photo page.');
+    }
   }
+  return false;
 }
 editInGWW();
