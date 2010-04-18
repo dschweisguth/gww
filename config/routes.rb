1ef42c215d399c78828d91b6147f1c92b9708a7a
Dave Schweisguth <dave@schweisguth.org>
2010-04-18 07:58:36 -0700

    Added about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,15 @@
 ActionController::Routing::Routes.draw do |map|
-  # The priority is based upon order of creation: first created -> highest priority.
 
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
-
-  # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
-
-  # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-  
-  # Sample resource route with more complex sub-resources
-  #   map.resources :products do |products|
-  #     products.resources :comments
-  #     products.resources :sales, :collection => { :recent => :get }
-  #   end
-
-  # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
+  map.connect 'admin', :controller => 'admin/root'
+  map.connect 'admin/:action', :controller => 'admin/root'
 
   map.root :controller => 'root'
-  map.connect 'bookmarklet', :controller => 'root', :action => 'bookmarklet'
+  map.connect ':action', :controller => 'root', :action => 'bookmarklet'
 
   map.show_person 'people/show/:id', :controller => 'people', :action => 'show'
 
   map.show_photo 'photos/show/:id', :controller => 'photos', :action => 'show'
   map.edit_photo 'photos/edit/:id', :controller => 'photos', :action => 'edit'
-
-  map.connect 'admin', :controller => 'admin/root'
-  map.connect 'admin/bookmarklet', :controller => 'admin/root', :action => 'bookmarklet'
 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
