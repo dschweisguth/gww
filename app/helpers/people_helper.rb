7dc91a6bf9478ccc6fca030743a0233b445aacc5
Dave Schweisguth <dave@schweisguth.org>
2010-05-24 23:31:17 -0700

    Fixed positional prefix in displays of weekly and monthly high scorers

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,2 +1,20 @@
 module PeopleHelper
+  def position(high_scorers, person)
+    position = 0
+    previous_score = nil
+    high_scorers.each do |high_scorer|
+      if high_scorer[:score] != previous_score
+        position += 1
+      end
+      break if high_scorer == person
+      previous_score = high_scorer[:score]
+    end
+
+    case position
+    when 1 then ''
+    when 2 then 'second-'
+    when 3 then 'third-'
+    end
+
+  end
 end
