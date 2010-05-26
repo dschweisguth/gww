ce326fce4772f294c4c92766531092c4dc2dac02
Dave Schweisguth <dave@schweisguth.org>
2010-05-25 22:23:48 -0700

    Began adding old_usernames

---
+++ doc/old_usernames/20100526043831_create_old_usernames.rb
@@ -0,0 +1,17 @@
+class CreateOldUsernames < ActiveRecord::Migration
+  def self.up
+    create_table :old_usernames do |t|
+      t.integer :person_id, :null => false
+      t.string :old_username, :null => false
+      t.datetime :created_at, :null => false
+    end
+    execute "alter table old_usernames add constraint old_usernames_person_id_fk foreign key(person_id) references people(id)"
+    execute "alter table old_usernames add constraint old_usernames_old_username_unique unique key (old_username)"
+    execute "insert into old_usernames select distinct null, p.id person_id, c.username old_username, now() from people p, comments c where p.flickrid = c.flickrid and p.username != c.username order by p.id"
+  end
+
+  def self.down
+    drop_table :old_usernames
+  end
+
+end
