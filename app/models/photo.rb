6800f648c9054d0d93cf1d535551a2486761bb08
Dave Schweisguth <dave@schweisguth.org>
2010-06-15 22:18:55 -0700

    Refactored order-by code. Updated link from home page to photos/link.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -43,54 +43,40 @@ class Photo < ActiveRecord::Base
     }
   end
 
-  ORDER_BY_CLAUSE = {
-    'username' => 
-      { :column => 'lower(poster.username)', :default_order => '+' },
-    'date-added' => 
-      { :column => 'dateadded', :default_order => '-' },
-    'last-updated' => 
-      { :column => 'lastupdate', :default_order => '-' },
-    'views' => 
-      { :column => 'views', :default_order => '-' },
-    'member-comments' => 
-      { :column => 'member_comments', :default_order => '-' },
-    'member-questions' => 
-      { :column => 'member_questions', :default_order => '-' }
-  }
-
-  def self.clause(sorted_by, order)
-    clause = ORDER_BY_CLAUSE[sorted_by][:column]
-    if ORDER_BY_CLAUSE[sorted_by][:default_order] != order
-      clause += ' desc'
-    end
-    clause
-  end
-
   def self.all_with_stats(sorted_by, order, page, per_page)
-    order_by = (
-      case sorted_by
-      when 'username'
-        clause('username', order) + ', ' + clause('date-added', order)
-      when 'date-added'
-        clause('date-added', order) + ', ' + clause('username', order)
-      when 'last-updated'
-        clause('last-updated', order) + ', ' + clause('username', order)
-      when 'views'
-	clause('views', order) + ', ' + clause('username', order)
-      when 'member-comments'
-	clause('member-comments', order) + ', ' +
-	  clause('date-added', order) + ', ' + clause('username', order)
-      when 'member-questions'
-	clause('member-questions', order) + ', ' +
-	  clause('date-added', order) + ', ' + clause('username', order)
-      end
-    )
     Photo.paginate_by_sql(
       'select p.* ' +
         'from photos p, people poster ' +
         'where p.person_id = poster.id ' +
-        'order by ' + order_by,
+        'order by ' + order_by(sorted_by, order),
       :page => page, :per_page => per_page)
+  end
+
+  SORTED_BY = {
+    'username' => { :secondary => [ 'date-added' ],
+      :column => 'lower(poster.username)', :default_order => '+' },
+    'date-added' => { :secondary => [ 'username' ],
+      :column => 'dateadded', :default_order => '-' },
+    'last-updated' => { :secondary => [ 'username' ],
+      :column => 'lastupdate', :default_order => '-' },
+    'views' => { :secondary => [ 'username' ],
+      :column => 'views', :default_order => '-' },
+    'member-comments' => { :secondary => [ 'date-added', 'username' ],
+      :column => 'member_comments', :default_order => '-' },
+    'member-questions' => { :secondary => [ 'date-added', 'username' ],
+      :column => 'member_questions', :default_order => '-' }
+  }
+
+  def self.order_by(sorted_by, order)
+    term_names = [ sorted_by, *SORTED_BY[sorted_by][:secondary] ]
+    terms = term_names.map do |term_name|
+      term = SORTED_BY[term_name][:column]
+      if SORTED_BY[term_name][:default_order] != order
+	term += ' desc'
+      end
+      term
+    end
+    terms.join ', '
   end
 
   def self.unfound_or_unconfirmed
