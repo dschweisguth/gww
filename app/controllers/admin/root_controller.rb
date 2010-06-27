8fa9aa2606d9311e12dc408fa76ffa71717bd50b
Dave Schweisguth <dave@schweisguth.org>
2010-06-27 08:22:28 -0700

    Removed unnecessary :all

--- app/controllers/admin/root_controller.rb
+++ app/controllers/admin/root_controller.rb
@@ -3,13 +3,12 @@ class Admin::RootController < ApplicationController
   caches_page :index
   def index
     @latest = FlickrUpdate.latest
-    @unfound_photos_count = Photo.count :all,
-      :conditions => "game_status in ('unfound', 'unconfirmed')"
-    @inaccessible_photos_count = Photo.count :all,
-      :conditions =>
-        [ "seen_at < ? and game_status in ('unfound', 'unconfirmed')",
-          @latest.created_at ]
-    @multipoint_photos_count = Guess.count(:all, :group => :photo_id).
+    @unfound_photos_count =
+      Photo.count :conditions => "game_status in ('unfound', 'unconfirmed')"
+    @inaccessible_photos_count = Photo.count :conditions =>
+      [ "seen_at < ? and game_status in ('unfound', 'unconfirmed')",
+        @latest.created_at ]
+    @multipoint_photos_count = Guess.count(:group => :photo_id).
       find_all { |photo_id, count| count > 1 }.length
   end
 
