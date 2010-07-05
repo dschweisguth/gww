80efe8a539e2ce0025eb0075e5bc71f56c481a71
Dave Schweisguth <dave@schweisguth.org>
2010-07-05 07:51:12 -0700

    Added gem dependencies to config to simplify installation

--- config/environment.rb
+++ config/environment.rb
@@ -15,7 +15,9 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
+  config.gem 'mysql', :version => '~> 2.8.1'
   config.gem 'will_paginate', :version => '~> 2.3.12', :source => 'http://gemcutter.org'
+  config.gem 'xml-simple', :lib => 'xmlsimple', :version => '~> 1.0.12'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
