74cba285836ddd0b4f25716f09dc8264aefb255b
Dave Schweisguth <dave@schweisguth.org>
2010-08-17 22:09:03 -0700

    Escape usernames with not just three word chars after a dot, but two or more

--- test/unit/helpers/admin/guesses_helper_test.rb
+++ test/unit/helpers/admin/guesses_helper_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class Admin::GuessesHelperTest < ActionView::TestCase
 
   def test_escape_username_safe_1
-    assert_equal 'KayVee . INC', escape_username('KayVee .INC')
+    assert_equal 'KayVee .INC', escape_username('KayVee .INC')
   end
 
   def test_escape_username_safe_2
@@ -12,6 +12,10 @@ class Admin::GuessesHelperTest < ActionView::TestCase
 
   def test_escape_username_unsafe_1
     assert_equal 'KayVee . INC', escape_username('KayVee.INC')
+  end
+
+  def test_escape_username_unsafe_2
+    assert_equal 'm . bibelot', escape_username('m.bibelot')
   end
 
 end
