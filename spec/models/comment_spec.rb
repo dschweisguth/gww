84ab39d652e2b1f99ef8268a0ef3cd4ee762cf07
Dave Schweisguth <dave@schweisguth.org>
2011-01-28 22:14:52 -0800

    Removed redundant tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -6,16 +6,32 @@ describe Comment do
     should belong_to :photo
   end
 
+  it "requires a flickrid" do
+    should validate_presence_of :flickrid
+  end
+
   it "doesn't allow flickrid to be updated" do
     should have_readonly_attribute :flickrid
+  end
+
+  it "requires a username" do
+    should validate_presence_of :username
   end
 
   it "doesn't allow username to be updated" do
     should have_readonly_attribute :username
   end
 
+  it "requires comment text" do
+    should validate_presence_of :comment_text
+  end
+
   it "doesn't allow comment_text to be updated" do
     should have_readonly_attribute :comment_text
+  end
+
+  it "requires commented_at" do
+    should validate_presence_of :commented_at
   end
 
   it "doesn't allow commented_at to be updated" do
@@ -23,41 +39,10 @@ describe Comment do
   end
 
   describe '.new' do
-    VALID_ATTRS = { :flickrid => 'flickrid', :username => 'username',
-      :comment_text => 'comment text', :commented_at => Time.now }
-
     it 'creates a valid object given all required attributes' do
-      Comment.new(VALID_ATTRS).should be_valid
+      Comment.new({ :flickrid => 'flickrid', :username => 'username',
+      :comment_text => 'comment text', :commented_at => Time.now }).should be_valid
     end
-
-    it 'creates an invalid object if flickrid is missing' do
-      Comment.new(VALID_ATTRS - :flickrid).should_not be_valid
-    end
-
-    it 'creates an invalid object if flickrid is blank' do
-      Comment.new(VALID_ATTRS.merge({ :flickrid => '' })).should_not be_valid
-    end
-
-    it 'creates an invalid object if username is missing' do
-      Comment.new(VALID_ATTRS - :username).should_not be_valid
-    end
-
-    it 'creates an invalid object if username is blank' do
-      Comment.new(VALID_ATTRS.merge({ :username => '' })).should_not be_valid
-    end
-
-    it 'creates an invalid object if comment text is missing' do
-      Comment.new(VALID_ATTRS - :comment_text).should_not be_valid
-    end
-
-    it 'creates an invalid object if comment_text is blank' do
-      Comment.new(VALID_ATTRS.merge({ :comment_text => '' })).should_not be_valid
-    end
-
-    it 'creates an invalid object if commented_at is missing' do
-      Comment.new(VALID_ATTRS - :commented_at).should_not be_valid
-    end
-
   end
 
 end
