efd0b647d673634ea9baad15400f6090ae70ecb1
Dave Schweisguth <dave@schweisguth.org>
2011-01-29 23:02:49 -0800

    Tighten validation of member_count

--- app/models/flickr_update.rb
+++ app/models/flickr_update.rb
@@ -1,6 +1,7 @@
 class FlickrUpdate < ActiveRecord::Base
   validates_presence_of :member_count
-  validates_numericality_of :member_count
+  validates_numericality_of :member_count, :only_integer => true,
+    :greater_than_or_equal_to => 0
   attr_readonly :member_count
 
   def self.latest
--- spec/models/flickr_update_spec.rb
+++ spec/models/flickr_update_spec.rb
@@ -5,6 +5,12 @@ describe FlickrUpdate do
   describe '#member_count' do
     it { should validate_presence_of :member_count }
     it { should validate_numericality_of :member_count }
+    it "requires member_count >= 0" do
+      FlickrUpdate.new(:member_count => -1).should_not be_valid
+    end
+    it "requires integral member_count" do
+      FlickrUpdate.new(:member_count => 0.1).should_not be_valid
+    end
     it { should have_readonly_attribute :member_count }
   end
 
