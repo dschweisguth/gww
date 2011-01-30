01265dbdba2504f6cc021dc0190ff10594782042
Dave Schweisguth <dave@schweisguth.org>
2011-01-29 16:46:09 -0800

    Added describes

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -5,41 +5,70 @@ describe Photo do
     it { should belong_to :person }
   end
   
-  it { should have_many :comments }
+  describe '#comments' do
+    it { should have_many :comments }
+  end
 
-  it { should have_many :guesses }
+  describe '#guesses' do
+    it { should have_many :guesses }
+  end
 
-  it { should have_one :revelation }
+  describe '#revelation' do
+    it { should have_one :revelation }
+  end
 
   describe '#flickrid' do
     it { should validate_presence_of :flickrid }
     it { should have_readonly_attribute :flickrid }
+    # TODO Dave should be unique
   end
 
-  it { should validate_presence_of :farm }
+  describe '#farm' do
+    it { should validate_presence_of :farm }
+  end
 
-  it { should validate_presence_of :server }
+  describe '#server' do
+    it { should validate_presence_of :server }
+  end
 
-  it { should validate_presence_of :secret }
+  describe '#secret' do
+    it { should validate_presence_of :secret }
+  end
 
-  it { should validate_presence_of :dateadded }
+  describe '#dateadded' do
+    it { should validate_presence_of :dateadded }
+  end
 
-  it { should validate_presence_of :mapped }
+  describe '#mapped' do
+    it { should validate_presence_of :mapped }
+  end
 
   # TODO Dave mapped must be 'true' or 'false'
 
-  it { should validate_presence_of :lastupdate }
+  describe '#lastupdate' do
+    it { should validate_presence_of :lastupdate }
+  end
 
-  it { should validate_presence_of :seen_at }
+  describe '#seen_at' do
+    it { should validate_presence_of :seen_at }
+  end
 
-  it { should validate_presence_of :game_status }
+  describe '#game_status' do
+    it { should validate_presence_of :game_status }
+  end
 
   # TODO Dave game_status must have certain values
 
-  it { should validate_presence_of :views }
+  describe '#views' do
+    it { should validate_presence_of :views }
+  end
 
-  it { should validate_presence_of :member_comments }
+  describe '#member_comments' do
+    it { should validate_presence_of :member_comments }
+  end
 
-  it { should validate_presence_of :member_questions }
+  describe '#member_questions' do
+    it { should validate_presence_of :member_questions }
+  end
 
 end
