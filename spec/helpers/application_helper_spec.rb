7f65552b11f072dd9e51f806d055cb5b96cab261
Dave Schweisguth <dave@schweisguth.org>
2011-01-31 12:51:38 -0800

    Tested ordinal

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -10,12 +10,33 @@ describe ApplicationHelper do
       helper.singularize('delete', 0).should == 'delete'
     end
 
-    irregular_verbs = { 'were' => 'was', 'have' => 'has' }
-    irregular_verbs.keys.each do |plural|
-      it "singularizes #{plural} to #{irregular_verbs[plural]}" do
-        helper.singularize(plural, 1).should == irregular_verbs[plural]
+    irregular_plural_verbs = { 'were' => 'was', 'have' => 'has' }
+    irregular_plural_verbs.keys.each do |plural|
+      it "singularizes the irregular plural verb #{plural} to #{irregular_plural_verbs[plural]}" do
+        helper.singularize(plural, 1).should == irregular_plural_verbs[plural]
       end
     end
 
   end
+
+  describe '#ordinal' do
+    expected = {
+      1 => '1st',
+      2 => '2nd',
+      3 => '3rd',
+      4 => '4th',
+      11 => '11th',
+      12 => '12th',
+      13 => '13th',
+      21 => '21st',
+      22 => '22nd',
+      23 => '23rd'
+    }
+    expected.keys.sort.each do |cardinal|
+      it "converts the number #{cardinal} to its ordinal, #{expected[cardinal]}" do
+        helper.ordinal(cardinal).should == expected[cardinal]
+      end
+    end
+  end
+
 end
