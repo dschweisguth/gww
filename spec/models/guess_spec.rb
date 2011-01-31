a50715eb84a8ced9771f933eca1cc44e558d61d7
Dave Schweisguth <dave@schweisguth.org>
2011-01-30 17:35:30 -0800

    Tested .longest

--- spec/models/guess_spec.rb
+++ spec/models/guess_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'support/model_factory'
 
 describe Guess do
   describe '#photo' do
@@ -22,6 +23,29 @@ describe Guess do
   describe '#added_at' do
     it { should validate_presence_of :added_at }
     it { should have_readonly_attribute :added_at }
+  end
+
+  describe '.longest' do
+    it 'lists the guesses with the longest time between post and guess' do
+      photo = Photo.create_for_test! :dateadded => Time.utc(2010)
+      guess = Guess.create_for_test! :photo => photo, :guessed_at => Time.utc(2011)
+      Guess.longest.should == [ guess ]
+    end
+
+    it 'sorts by time between post and guess' do
+      photo1 = Photo.create_for_test! :label => 1, :dateadded => Time.utc(2000)
+      guess1 = Guess.create_for_test! :label => 1, :photo => photo1, :guessed_at => Time.utc(2001)
+      photo2 = Photo.create_for_test! :label => 2, :dateadded => Time.utc(2002)
+      guess2 = Guess.create_for_test! :label => 2, :photo => photo2, :guessed_at => Time.utc(2004)
+      Guess.longest.should == [ guess2, guess1 ]
+    end
+
+    it 'ignores a guess made before it was posted' do
+      photo = Photo.create_for_test! :dateadded => Time.utc(2011)
+      Guess.create_for_test! :photo => photo, :guessed_at => Time.utc(2010)
+      Guess.longest.should == []
+    end
+
   end
 
 end
