a79ba28b26fb70c37a249772a4d84bd4d9e5b25b
Dave Schweisguth <dave@schweisguth.org>
2011-01-31 16:01:06 -0800

    Extracted method

--- spec/helpers/people_helper_spec.rb
+++ spec/helpers/people_helper_spec.rb
@@ -3,18 +3,18 @@ require 'spec_helper'
 describe PeopleHelper do
   describe '#list_path' do
     it 'returns the URI to the list sorted by the given criterion' do
-      should_return 'date-added', '+', 'username', '/people/list/sorted-by/username/order/+'
+      list_path_should_return 'date-added', '+', 'username', '/people/list/sorted-by/username/order/+'
     end
 
     it 'reverses the sort order if the list is already sorted by the given criterion' do
-      should_return 'username', '+', 'username', '/people/list/sorted-by/username/order/-'
+      list_path_should_return 'username', '+', 'username', '/people/list/sorted-by/username/order/-'
     end
 
     it 'restores the sort order if the list is already reverse-sorted by the given criterion' do
-      should_return 'username', '-', 'username', '/people/list/sorted-by/username/order/+'
+      list_path_should_return 'username', '-', 'username', '/people/list/sorted-by/username/order/+'
     end
 
-    def should_return(current_criterion, current_order, requested_criterion, expected_uri)
+    def list_path_should_return(current_criterion, current_order, requested_criterion, expected_uri)
       params[:sorted_by] = current_criterion
       params[:order] = current_order
       helper.list_path(requested_criterion).should == expected_uri
@@ -28,35 +28,42 @@ describe PeopleHelper do
     end
 
     it "returns the appropriate prefix for '-most': '', if the person is first" do
-      helper.position([ @person ], @person).should == ''
+      position_should_return [], ''
     end
 
     it "returns the appropriate prefix for '-most': '', if the person is tied for first" do
-      helper.position([ person_with(0), @person ], @person).should == ''
+      position_should_return [0], ''
     end
 
     it "returns the appropriate prefix for '-most': 'second-', if the person is second" do
-      helper.position([ person_with(1), @person ], @person).should == 'second-'
+      position_should_return [1], 'second-'
     end
 
     it "returns the appropriate prefix for '-most': 'second-', if the person is tied for second" do
-      helper.position([ person_with(1), person_with(0), @person ], @person).should == 'second-'
+      position_should_return [1, 0], 'second-'
     end
 
     it "returns the appropriate prefix for '-most': 'third-', if the person is third" do
-      helper.position([ person_with(2), person_with(1), @person ], @person).should == 'third-'
+      position_should_return [2, 1], 'third-'
     end
 
     it "returns the appropriate prefix for '-most': 'third-', if the person is behind two others tied for first" do
-      helper.position([ person_with(1), person_with(1), @person ], @person).should == 'third-'
+      position_should_return [1, 1], 'third-'
     end
 
     it "returns the appropriate prefix for '-most': 'fourth-', if the person is fourth" do
-      helper.position([ person_with(1), person_with(1), person_with(1), @person ], @person).should == 'fourth-'
+      position_should_return [1, 1, 1], 'fourth-'
     end
 
     it "returns the appropriate prefix for '-most': 'fifth-', if the person is fifth" do
-      helper.position([ person_with(1), person_with(1), person_with(1), person_with(1), @person ], @person).should == 'fifth-'
+      position_should_return [1, 1, 1, 1], 'fifth-'
+    end
+
+    def position_should_return(higher_scores, expected)
+      high_scorers = higher_scores.map { |score| person_with score }
+      #noinspection RubyResolve
+      high_scorers.push @person
+      helper.position(high_scorers, @person).should == expected
     end
 
     def person_with(score)
