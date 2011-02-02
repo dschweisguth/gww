4b348a867c2cbae617829166a8755bfbfe4a9f10
Dave Schweisguth <dave@schweisguth.org>
2011-02-02 13:54:55 -0800

    Moved methods from controller to model

--- app/models/person.rb
+++ app/models/person.rb
@@ -152,6 +152,96 @@ class Person < ActiveRecord::Base
   end
   private_class_method :statistic_by_person
 
+  def self.top_guessers
+    now = Time.now
+
+    days = []
+    (0..6).each do |num|
+      dates = { :begin => (now - num.day).beginning_of_day,
+        :end => (now - (num - 1).day).beginning_of_day }
+      scores = get_scores_from_date dates[:begin], dates[:end]
+      days.push({ :dates => dates, :scores => scores })
+    end
+
+    thisweek_dates = { :begin => now.beginning_of_week - 1.day, :end => now }
+    thisweek_scores = get_scores_from_date thisweek_dates[:begin], nil
+    weeks = [ { :dates => thisweek_dates, :scores => thisweek_scores } ]
+    (1..5).each do |num|
+      dates = {
+        :begin => (now - num.week).beginning_of_week - 1.day,
+        :end => (now - (num - 1).week).beginning_of_week - 1.day }
+      scores = get_scores_from_date dates[:begin], dates[:end]
+      weeks.push({ :dates => dates, :scores => scores })
+    end
+
+    thismonth_dates = { :begin => now.beginning_of_month, :end => now }
+    thismonth_scores = get_scores_from_date thismonth_dates[:begin], nil
+    months =
+      [ { :dates => thismonth_dates, :scores => thismonth_scores } ]
+    (1..5).each do |num|
+      dates = { :begin => (now - num.month).beginning_of_month,
+        :end => (now - (num - 1).month).beginning_of_month }
+      scores = get_scores_from_date dates[:begin], dates[:end]
+      months.push({ :dates => dates, :scores => scores })
+    end
+
+    thisyear_dates = { :begin => now.beginning_of_year, :end => now }
+    thisyear_scores = get_scores_from_date thisyear_dates[:begin], nil
+    years = [ {:dates => thisyear_dates, :scores => thisyear_scores} ]
+    years_of_guessing = Time.now.getutc.year - Guess.first.guessed_at.year
+    (1..years_of_guessing).each do |num|
+      dates = { :begin => (now - num.year).beginning_of_year,
+        :end => (now - (num - 1).year).beginning_of_year }
+      scores = get_scores_from_date dates[:begin], dates[:end]
+      years.push({ :dates => dates, :scores => scores })
+    end
+
+    return days, weeks, months, years
+  end
+
+  def self.get_scores_from_date(begin_date, end_date)
+    if begin_date && end_date
+      conditions = [ "guessed_at > ? and guessed_at < ?",
+        begin_date.getutc, end_date.getutc ]
+    elsif begin_date
+      conditions = [ "guessed_at > ?", begin_date.getutc ]
+    else
+      conditions = []
+    end
+    guesses = Guess.all :conditions => conditions, :include => :person
+
+    guessers = {}
+    guesses.each do |guess|
+      guesser = guessers[guess.person.id]
+      if guesser
+        guesser[:score] += 1
+      else
+        guess.person[:score] = 1
+        guessers[guess.person.id] = guess.person
+      end
+    end
+
+    scores = {}
+    guessers.values.each do |guesser|
+      score = scores[guesser[:score]]
+      if score
+        score.push guesser
+      else
+        scores[guesser[:score]] = [ guesser ]
+      end
+    end
+
+    scores.values.each do |guessers_with_score|
+      guessers_with_score.each \
+        { |guesser| guesser[:downcased_username] = guesser.username.downcase }
+      guessers_with_score.sort! \
+        { |a, b| a[:downcased_username] <=> b[:downcased_username] }
+    end
+
+    scores
+  end
+  private_class_method :get_scores_from_date
+
   def self.high_scorers(days)
     people = find_by_sql [
       'select p.*, count(*) score from people p, guesses g ' +
