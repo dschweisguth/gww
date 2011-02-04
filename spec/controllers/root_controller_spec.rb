404c42a4655520ff7e7d05eb670cfff47acfa767
Dave Schweisguth <dave@schweisguth.org>
2011-02-04 15:47:30 -0800

    Tested completed Flickr update

--- spec/controllers/root_controller_spec.rb
+++ spec/controllers/root_controller_spec.rb
@@ -10,6 +10,14 @@ describe RootController do
       response.should render_template 'index'
       response.should have_text /The most recent update from Flickr began Saturday, January 01, 00:00 PST and is still running. An update takes about six minutes./
     end
+
+    it 'reports a completed update' do
+      stub(FlickrUpdate).latest { FlickrUpdate.new_for_test :created_at => Time.local(2011), :completed_at => Time.local(2001, 1, 1, 0, 6) }
+      get :index
+      response.should render_template 'index'
+      response.should have_text /The most recent update from Flickr began Saturday, January 01, 00:00 PST and completed at Monday, January 01, 00:06 PST./
+    end
+
   end
 
 end
