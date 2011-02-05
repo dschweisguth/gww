2de66a9dc29d9e4a988d9356e10f70993c46a1ab
Dave Schweisguth <dave@schweisguth.org>
2011-02-04 22:11:55 -0800

    Tested index

--- spec/controllers/admin/root_controller_spec.rb
+++ spec/controllers/admin/root_controller_spec.rb
@@ -3,6 +3,24 @@ require 'spec_helper'
 describe Admin::RootController do
   integrate_views
 
+  describe '#index' do
+    it 'renders the page' do
+      stub(FlickrUpdate).latest { FlickrUpdate.new_for_test :created_at => Time.local(2011) }
+      stub(Photo).unfound_or_unconfirmed_count { 111 }
+      stub(Photo).count { 222 }
+      stub(Guess).count { { 1 => 2, 2 => 2 }  }
+      get :index
+
+      #noinspection RubyResolve
+      response.should be_success
+      response.should have_text /The most recent update from Flickr began Saturday, January 01, 00:00 PST and is still running. An update takes about six minutes./
+      response.should have_text /\(111\)/
+      response.should have_text /\(222\)/
+      response.should have_text /\(2\)/
+
+    end
+  end
+
   describe '#bookmarklet' do
     it 'renders the page' do
       get :bookmarklet
