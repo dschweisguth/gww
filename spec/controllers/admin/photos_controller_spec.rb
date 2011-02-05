d08e2ca15c9673fef9609572abeaf88967dba159
Dave Schweisguth <dave@schweisguth.org>
2011-02-05 12:56:15 -0800

    Stub expire_cached_pages

--- spec/controllers/admin/photos_controller_spec.rb
+++ spec/controllers/admin/photos_controller_spec.rb
@@ -4,6 +4,7 @@ describe Admin::PhotosController do
   describe '.update_statistics' do
     it 'does some work and redirects to the admin index' do
       mock(Photo).update_statistics
+      any_instance_of(Admin::PhotosController) { |i| stub(i).expire_cached_pages }
       get :update_statistics
       #noinspection RubyResolve
       response.should redirect_to admin_root_path
