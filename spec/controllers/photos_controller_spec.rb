f1349ab43e2ff6d21ac8490bd7a690fe098a8c40
Dave Schweisguth <dave@schweisguth.org>
2011-02-04 18:46:39 -0800

    Call action methods with params, and specify stubbed method arguments where reasonable to require the params to be present

--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -5,12 +5,16 @@ describe PhotosController do
 
   describe '#list' do
     it 'renders the page' do
+      SORTED_BY_PARAM = 'username'
+      ORDER_PARAM = '+'
+      PAGE_PARAM = '1'
+
       # Mock methods from will_paginate's version of Array
       paginated_photos = [ Photo.new_for_test ]
       stub(paginated_photos).offset { 0 }
       stub(paginated_photos).total_pages { 1 }
-      stub(Photo).all_sorted_and_paginated.with('username', '+', '1', 30) { paginated_photos }
-      get :list, :sorted_by => 'username', :order => '+', :page => 1
+      stub(Photo).all_sorted_and_paginated.with(SORTED_BY_PARAM, ORDER_PARAM, PAGE_PARAM, 30) { paginated_photos }
+      get :list, :sorted_by => 'username', :order => ORDER_PARAM, :page => PAGE_PARAM
 
       #noinspection RubyResolve
       response.should be_success
