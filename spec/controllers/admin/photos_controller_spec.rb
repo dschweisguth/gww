f9f995ac553ecfd2ff33e97e932674303e5eea04
Dave Schweisguth <dave@schweisguth.org>
2011-02-05 17:06:35 -0800

    Tested edit without loading comments

--- spec/controllers/admin/photos_controller_spec.rb
+++ spec/controllers/admin/photos_controller_spec.rb
@@ -72,6 +72,28 @@ describe Admin::PhotosController do
       end
 
     end
+
+    it 'loads comments and renders the page' do
+      photo = Photo.new_for_test :dateadded => Time.local(2011)
+      stub(photo).id { 1 }
+      stub(Photo).find(photo.id.to_s, anything) { photo }
+      stub(photo).load_comments { [ Comment.new_for_test ] }
+      any_instance_of(Admin::PhotosController) { |i| stub(i).expire_cached_pages }
+      get :edit, :id => photo.id
+
+      #noinspection RubyResolve
+      response.should be_success
+      response.should have_text /Added to the group at 12:00 AM, January 01, 2011/
+      response.should have_text /This photo is unfound./
+      response.should have_tag 'form[action=/admin/photos/change_game_status/1]', :text => /Change this photo's status from unfound to/ do
+        with_tag 'input[value=unconfirmed]'
+      end
+      response.should have_tag 'form[action=/admin/photos/add_guess/1]' do
+        with_tag 'strong', :text => 'comment_username says:'
+      end
+
+    end
+
   end
 
 end
