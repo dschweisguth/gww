048411861f236292d2c780819a84e0c7da66c810
Dave Schweisguth <dave@schweisguth.org>
2011-02-07 22:10:32 -0800

    More tests of PeopleController routing

--- spec/routing/people_routing_spec.rb
+++ spec/routing/people_routing_spec.rb
@@ -6,5 +6,15 @@ describe PeopleController do
       { :get => '/people/list/sorted-by/foo/order/bar' }.should route_to \
         :controller => 'people', :action => 'list', :sorted_by => 'foo', :order => 'bar'
     end
+
+    it 'routes to show' do
+      { :get => '/people/show/666' }.should route_to \
+        :controller => 'people', :action => 'show', :id => '666'
+    end
+
+    it 'routes to a plain action' do
+      { :get => '/people/top_guessers' }.should route_to :controller => 'people', :action => 'top_guessers'
+    end
+
   end
 end
