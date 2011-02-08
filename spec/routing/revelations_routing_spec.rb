517b9ecbf1b48da8c7b2281587be1b0a73201402
Dave Schweisguth <dave@schweisguth.org>
2011-02-07 22:04:57 -0800

    Fiddled with wording

--- spec/routing/revelations_routing_spec.rb
+++ spec/routing/revelations_routing_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe RevelationsController do
   describe 'routing' do
-    it 'routes to an action' do
+    it 'routes to a plain action' do
       { :get => '/revelations/longest' }.should route_to :controller => 'revelations', :action => 'longest'
     end
   end
