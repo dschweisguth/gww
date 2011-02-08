55a053b6c1e8c4a12698faf9952980fecc77c240
Dave Schweisguth <dave@schweisguth.org>
2011-02-07 23:31:22 -0800

    Added tests of named routes

--- spec/routing/guesses_routing_spec.rb
+++ spec/routing/guesses_routing_spec.rb
@@ -2,9 +2,17 @@ require 'spec_helper'
 
 describe GuessesController do
   describe 'routing' do
-    it 'routes to a plain action' do
-      { :get => '/guesses/longest_and_shortest' }.should route_to \
-        :controller => 'guesses', :action => 'longest_and_shortest'
+    describe 'longest_and_shortest' do
+      it 'is routed to' do
+        { :get => '/guesses/longest_and_shortest' }.should route_to \
+          :controller => 'guesses', :action => 'longest_and_shortest'
+      end
+
+      it 'has a named route' do
+        #noinspection RubyResolve
+        longest_and_shortest_path.should == '/guesses/longest_and_shortest'
+      end
+
     end
   end
 end
