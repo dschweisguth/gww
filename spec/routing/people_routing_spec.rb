c1bf8b2f653dbdfc7ecf156f955e9bfb6986ab08
Dave Schweisguth <dave@schweisguth.org>
2011-02-07 23:23:43 -0800

    Added tests of named routes

--- spec/routing/people_routing_spec.rb
+++ spec/routing/people_routing_spec.rb
@@ -2,19 +2,35 @@ require 'spec_helper'
 
 describe PeopleController do
   describe 'routing' do
-    it 'routes to list' do
-      { :get => '/people/list/sorted-by/foo/order/bar' }.should route_to \
-        :controller => 'people', :action => 'list', :sorted_by => 'foo', :order => 'bar'
+    describe 'list' do
+      it 'is routed to' do
+        { :get => '/people/list/sorted-by/foo/order/bar' }.should route_to \
+          :controller => 'people', :action => 'list', :sorted_by => 'foo', :order => 'bar'
+      end
+
+      it 'has a named route' do
+        #noinspection RubyResolve
+        list_people_path('foo', 'bar').should == '/people/list/sorted-by/foo/order/bar'
+      end
+
     end
 
-    it 'routes to show' do
-      { :get => '/people/show/666' }.should route_to \
-        :controller => 'people', :action => 'show', :id => '666'
+    describe 'show' do
+      it 'is routed to' do
+        { :get => '/people/show/666' }.should route_to \
+          :controller => 'people', :action => 'show', :id => '666'
+      end
+
+      it 'has a named route' do
+        #noinspection RubyResolve
+        show_person_path(666).should == '/people/show/666'
+      end
+
     end
 
-    it 'routes to a plain action' do
-      { :get => '/people/top_guessers' }.should route_to :controller => 'people', :action => 'top_guessers'
-    end
+  it 'routes to a plain action' do
+    { :get => '/people/top_guessers' }.should route_to :controller => 'people', :action => 'top_guessers'
+  end
 
   end
 end
