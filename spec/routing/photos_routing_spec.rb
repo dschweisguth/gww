b253a7bca78eae0b78e715a61c00303109d7b6d0
Dave Schweisguth <dave@schweisguth.org>
2011-02-07 23:28:21 -0800

    Added tests of named routes

--- spec/routing/photos_routing_spec.rb
+++ spec/routing/photos_routing_spec.rb
@@ -2,13 +2,29 @@ require 'spec_helper'
 
 describe PhotosController do
   describe 'routing' do
-    it 'routes to list' do
-      { :get => '/photos/list/sorted-by/foo/order/bar/page/666' }.should route_to \
-        :controller => 'photos', :action => 'list', :sorted_by => 'foo', :order => 'bar', :page => '666' 
+    describe 'list' do
+      it 'is routed to' do
+        { :get => '/photos/list/sorted-by/foo/order/bar/page/666' }.should route_to \
+          :controller => 'photos', :action => 'list', :sorted_by => 'foo', :order => 'bar', :page => '666'
+      end
+
+      it 'has a named route' do
+        #noinspection RubyResolve
+        list_photos_path('foo', 'bar', '1').should == '/photos/list/sorted-by/foo/order/bar/page/1'
+      end
+
     end
 
-    it 'routes to show' do
-      { :get => '/photos/show/666' }.should route_to :controller => 'photos', :action => 'show', :id => '666'
+    describe 'show' do
+      it 'is routed to' do
+        { :get => '/photos/show/666' }.should route_to :controller => 'photos', :action => 'show', :id => '666'
+      end
+
+      it 'has a named route' do
+        #noinspection RubyResolve
+        show_photo_path(666).should == '/photos/show/666'
+      end
+
     end
 
     it 'routes to a plain action' do
