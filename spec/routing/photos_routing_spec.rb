f9d24cede01f8de7c8570b57e3e2b8ce9f547271
Dave Schweisguth <dave@schweisguth.org>
2011-02-07 22:03:39 -0800

    More tests of PhotosController routing

--- spec/routing/photos_routing_spec.rb
+++ spec/routing/photos_routing_spec.rb
@@ -6,5 +6,14 @@ describe PhotosController do
       { :get => '/photos/list/sorted-by/foo/order/bar/page/666' }.should route_to \
         :controller => 'photos', :action => 'list', :sorted_by => 'foo', :order => 'bar', :page => '666' 
     end
+
+    it 'routes to show' do
+      { :get => '/photos/show/666' }.should route_to :controller => 'photos', :action => 'show', :id => '666'
+    end
+
+    it 'routes to a plain action' do
+      { :get => '/photos/unfound' }.should route_to :controller => 'photos', :action => 'unfound'
+    end
+
   end
 end
