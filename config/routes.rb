9e631a987824d532c003c4f95f540720c9e8039c
Dave Schweisguth <dave@schweisguth.org>
2011-02-07 22:54:22 -0800

    Used map.with_options

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,29 @@
 ActionController::Routing::Routes.draw do |map|
 
-  map.admin_root 'admin', :controller => 'admin/root'
-  map.connect 'admin/:action', :controller => 'admin/root'
+  map.with_options :controller => 'admin/root' do |admin_root|
+    admin_root.admin_root 'admin'
+    admin_root.connect 'admin/:action'
+  end
 
   map.connect 'wheresies/:action', :controller => 'wheresies'
 
-  map.root :controller => 'root'
-  map.connect ':action', :controller => 'root'
+  map.with_options :controller => 'root' do |root|
+    root.root
+    root.connect ':action'
+  end
 
-  map.list_people 'people/list/sorted-by/:sorted_by/order/:order',
-    :controller => 'people', :action => 'list'
-  map.show_person 'people/show/:id', :controller => 'people', :action => 'show'
-  map.list_comments 'people/comments/:id/page/:page',
-    :controller => 'people', :action => 'comments'
+  map.with_options :controller => 'people' do |people|
+    people.list_people 'people/list/sorted-by/:sorted_by/order/:order', :action => 'list'
+    people.show_person 'people/show/:id', :action => 'show'
+    people.list_comments 'people/comments/:id/page/:page', :action => 'comments'
+  end
 
-  map.list_photos 'photos/list/sorted-by/:sorted_by/order/:order/page/:page',
-    :controller => 'photos', :action => 'list'
-  map.show_photo 'photos/show/:id', :controller => 'photos', :action => 'show'
-  map.edit_photo 'admin/photos/edit/:id',
-    :controller => 'admin/photos', :action => 'edit'
+  map.with_options :controller => 'photos' do |photos|
+    photos.list_photos 'photos/list/sorted-by/:sorted_by/order/:order/page/:page', :action => 'list'
+    photos.show_photo 'photos/show/:id', :action => 'show'
+  end
+
+  map.edit_photo 'admin/photos/edit/:id', :controller => 'admin/photos', :action => 'edit'
 
   map.longest_and_shortest 'guesses/longest_and_shortest',
     :controller => 'guesses', :action => 'longest_and_shortest'
