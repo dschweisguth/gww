0e32ff8d5b4b2ecda949d18a7c1a33aabc14b74e
Dave Schweisguth <dave@schweisguth.org>
2011-02-07 23:33:11 -0800

    Added tests of named routes

--- spec/routing/admin/photos_routing_spec.rb
+++ spec/routing/admin/photos_routing_spec.rb
@@ -6,8 +6,16 @@ describe Admin::PhotosController do
       { :get => '/admin/photos/update' }.should route_to :controller => 'admin/photos', :action => 'update'
     end
 
-    it 'routes to edit' do
-      { :get => '/admin/photos/edit/666' }.should route_to :controller => 'admin/photos', :action => 'edit', :id => '666'
+    describe 'edit' do
+      it 'is routed to' do
+        { :get => '/admin/photos/edit/666' }.should route_to :controller => 'admin/photos', :action => 'edit', :id => '666'
+      end
+
+      it 'has a named route' do
+        #noinspection RubyResolve
+        edit_photo_path(666).should == '/admin/photos/edit/666'
+      end
+
     end
 
   end
