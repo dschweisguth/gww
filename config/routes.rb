fcf90f731b2bd854c49e34cae2e0d82d81101db7
Dave Schweisguth <dave@schweisguth.org>
2011-02-07 22:58:05 -0800

    Reordered routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
 
-  map.with_options :controller => 'admin/root' do |admin_root|
-    admin_root.admin_root 'admin'
-    admin_root.connect 'admin/:action'
-  end
-
-  map.connect 'wheresies/:action', :controller => 'wheresies'
-
-  map.with_options :controller => 'root' do |root|
-    root.root
-    root.connect ':action'
-  end
-
   map.with_options :controller => 'people' do |people|
     people.list_people 'people/list/sorted-by/:sorted_by/order/:order', :action => 'list'
     people.show_person 'people/show/:id', :action => 'show'
@@ -23,10 +11,22 @@ ActionController::Routing::Routes.draw do |map|
     photos.show_photo 'photos/show/:id', :action => 'show'
   end
 
-  map.edit_photo 'admin/photos/edit/:id', :controller => 'admin/photos', :action => 'edit'
-
   map.longest_and_shortest 'guesses/longest_and_shortest',
     :controller => 'guesses', :action => 'longest_and_shortest'
+
+  map.connect 'wheresies/:action', :controller => 'wheresies'
+
+  map.edit_photo 'admin/photos/edit/:id', :controller => 'admin/photos', :action => 'edit'
+
+  map.with_options :controller => 'admin/root' do |admin_root|
+    admin_root.admin_root 'admin'
+    admin_root.connect 'admin/:action'
+  end
+
+  map.with_options :controller => 'root' do |root|
+    root.root
+    root.connect ':action'
+  end
 
   map.connect ':controller/:action/:id'
 
