4cb07cb0377d1654c1c3c52dfff34409e3754cb0
Dave Schweisguth <dave@schweisguth.org>
2011-02-17 15:34:05 -0800

    Corrected parameter name

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -837,8 +837,8 @@ describe Photo do
   # It would be nice to mock the method that deletes the owner, which handles
   # cases where the owner has a photo or other guess and shouldn't be deleted,
   # but doing so would be ugly.
-  def owner_should_not_exist(guess)
-    Person.exists?(guess.person.id).should == false
+  def owner_should_not_exist(owner)
+    Person.exists?(owner.person.id).should == false
   end
 
 end
