8839b9c4c5d2d76c5138a9ea8539f2a9794fa766
Dave Schweisguth <dave@schweisguth.org>
2011-02-23 14:06:42 -0800

    Tested that less than ten guesses are ignored

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -635,6 +635,12 @@ describe Person do
       guesser.favorite_posters.should == []
     end
 
+    it "ignores a poster which this person has guessed less than 10 times" do
+      #noinspection RubyUnusedLocalVariable
+      guesser, favorite_poster = make_potential_favorite_poster(9, 15)
+      guesser.favorite_posters.should == []
+    end
+
   end
 
   describe '#favorite_posters_of' do
@@ -651,6 +657,12 @@ describe Person do
       poster.favorite_posters_of.should == []
     end
 
+    it "ignores a guesser who has guessed this person less than 10 times" do
+      #noinspection RubyUnusedLocalVariable
+      devoted_guesser, poster = make_potential_favorite_poster(9, 15)
+      poster.favorite_posters_of.should == []
+    end
+    
   end
 
   def make_potential_favorite_poster(posts_by_favorite, posts_by_others)
