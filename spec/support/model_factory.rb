9532043edf68cf30d58647247f7088fe4b9c884a
Dave Schweisguth <dave@schweisguth.org>
2011-02-23 15:50:08 -0800

    Removed use of :label in ModelFactory extensions

--- spec/support/model_factory.rb
+++ spec/support/model_factory.rb
@@ -75,9 +75,9 @@ class Photo
       :views => 0
     }
     if ! caller_options[:person]
-      person_options = {:label => (padded_label + 'poster')}
-      options[:person] = new_or_create == :new \
-        ? Person.make(person_options) : Person.make!(person_options)
+      person_label = padded_label + 'poster'
+      options[:person] =
+        new_or_create == :new ? Person.make(person_label) : Person.make!(person_label)
     end
     options.merge! caller_options
     new_or_create == :new ? Photo.new(options) : Photo.create!(options)
@@ -97,9 +97,9 @@ class Comment
       :commented_at => Time.now
     }
     if ! caller_options[:photo]
-      photo_options = { :label => (padded_label + 'comment') }
-      options[:photo] = new_or_create == :new \
-        ? Photo.make(photo_options) : Photo.make!(photo_options)
+      photo_label = padded_label + 'comment'
+      options[:photo] =
+        new_or_create == :new ? Photo.make(photo_label) : Photo.make!(photo_label)
     end
     options.merge! caller_options
     new_or_create == :new ? Comment.new(options) : Comment.create!(options)
@@ -118,18 +118,14 @@ class Guess
       :added_at => now
     }
     if ! caller_options[:photo]
-      photo_options = { :label => (padded_label + 'guess') }
-      if caller_options[:photo_person]
-        photo_options[:person] = caller_options[:photo_person]
-      end
-      options[:photo] = new_or_create == :new \
-        ? Photo.make(photo_options) : Photo.make!(photo_options)
+      photo_label = padded_label + 'guess'
+      options[:photo] =
+        new_or_create == :new ? Photo.make(photo_label) : Photo.make!(photo_label)
     end
-    caller_options.delete :photo_person
     if ! caller_options[:person]
-      person_options = { :label => (padded_label + 'guesser') }
-      options[:person] = new_or_create == :new \
-        ? Person.make(person_options) : Person.make!(person_options)
+      person_label = padded_label + 'guesser'
+      options[:person] =
+        new_or_create == :new ? Person.make(person_label) : Person.make!(person_label)
     end
     options.merge! caller_options
     new_or_create == :new ? Guess.new(options) : Guess.create!(options)
@@ -149,9 +145,9 @@ class Revelation
       :added_at => now
     }
     if ! caller_options[:photo]
-      photo_options = { :label => (padded_label + 'revelation') }
-      options[:photo] = new_or_create == :new \
-        ? Photo.make(photo_options) : Photo.make!(photo_options)
+      photo_label = padded_label + 'revelation'
+      options[:photo] =
+        new_or_create == :new ? Photo.make(photo_label) : Photo.make!(photo_label)
     end
     options.merge! caller_options
     new_or_create == :new ? Revelation.new(options) : Revelation.create!(options)
