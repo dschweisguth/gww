185849f0bae5410b1ccc33c5109e93985d93c606
Dave Schweisguth <dave@schweisguth.org>
2011-02-23 14:28:42 -0800

    Reformatted options hashes

--- spec/support/model_factory.rb
+++ spec/support/model_factory.rb
@@ -23,6 +23,7 @@ end
 class FlickrUpdate
   extend ModelFactory
 
+  #noinspection RubyUnusedLocalVariable
   def self.make_for_test(new_or_create, padded_label, caller_options)
     options = { :member_count => 0 }
     options.merge! caller_options
@@ -36,8 +37,10 @@ class Person
   extend ModelFactory
 
   def self.make_for_test(new_or_create, padded_label, caller_options)
-    options = { :flickrid => padded_label + 'person_flickrid',
-      :username => padded_label + 'username' }
+    options = {
+      :flickrid => padded_label + 'person_flickrid',
+      :username => padded_label + 'username'
+    }
     options.merge! caller_options
     new_or_create == :new ? Person.new(options) : Person.create!(options)
   end
@@ -50,10 +53,18 @@ class Photo
 
   def self.make_for_test(new_or_create, padded_label, caller_options)
     now = Time.now
-    options = { :flickrid => padded_label + 'photo_flickrid',
-      :farm => '0', :server => 'server', :secret => 'secret',
-      :dateadded => now, :lastupdate => now, :seen_at => now,
-      :mapped => 'false', :game_status => 'unfound', :views => 0 }
+    options = {
+      :flickrid => padded_label + 'photo_flickrid',
+      :farm => '0',
+      :server => 'server',
+      :secret => 'secret',
+      :dateadded => now,
+      :lastupdate => now,
+      :seen_at => now,
+      :mapped => 'false',
+      :game_status => 'unfound',
+      :views => 0
+    }
     if ! caller_options[:person]
       person_options = {:label => (padded_label + 'poster')}
       options[:person] = new_or_create == :new \
@@ -70,9 +81,12 @@ class Comment
   extend ModelFactory
 
   def self.make_for_test(new_or_create, padded_label, caller_options)
-    options = { :flickrid => padded_label + 'comment_flickrid',
+    options = {
+      :flickrid => padded_label + 'comment_flickrid',
       :username => padded_label + 'comment_username',
-      :comment_text => padded_label + 'comment text', :commented_at => Time.now }
+      :comment_text => padded_label + 'comment text',
+      :commented_at => Time.now
+    }
     if ! caller_options[:photo]
       photo_options = { :label => (padded_label + 'comment') }
       options[:photo] = new_or_create == :new \
@@ -89,7 +103,11 @@ class Guess
 
   def self.make_for_test(new_or_create, padded_label, caller_options)
     now = Time.now
-    options = { :guess_text => padded_label + 'guess text', :guessed_at => now, :added_at => now }
+    options = {
+      :guess_text => padded_label + 'guess text',
+      :guessed_at => now,
+      :added_at => now
+    }
     if ! caller_options[:photo]
       photo_options = { :label => (padded_label + 'guess') }
       if caller_options[:photo_person]
@@ -100,7 +118,7 @@ class Guess
     end
     caller_options.delete :photo_person
     if ! caller_options[:person]
-      person_options = {:label => (padded_label + 'guesser')}
+      person_options = { :label => (padded_label + 'guesser') }
       options[:person] = new_or_create == :new \
         ? Person.make(person_options) : Person.make!(person_options)
     end
@@ -116,7 +134,11 @@ class Revelation
 
   def self.make_for_test(new_or_create, padded_label, caller_options)
     now = Time.now
-    options = { :revelation_text => padded_label + 'revelation text', :revealed_at => now, :added_at => now }
+    options = {
+      :revelation_text => padded_label + 'revelation text',
+      :revealed_at => now,
+      :added_at => now
+    }
     if ! caller_options[:photo]
       photo_options = { :label => (padded_label + 'revelation') }
       options[:photo] = new_or_create == :new \
