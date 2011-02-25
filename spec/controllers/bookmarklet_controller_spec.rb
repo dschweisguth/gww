a9ec2d02f6bc2c8a9a10ac6ae88f4c9fd097cd73
Dave Schweisguth <dave@schweisguth.org>
2011-02-24 18:22:24 -0800

    Tested missing person

--- spec/controllers/bookmarklet_controller_spec.rb
+++ spec/controllers/bookmarklet_controller_spec.rb
@@ -9,11 +9,13 @@ describe BookmarkletController do
       #noinspection RubyResolve
       stub(Photo).find_by_flickrid('0123456789') { photo }
       get :view, :from => 'http://www.flickr.com/photos/person_flickrid/0123456789/'
+
       #noinspection RubyResolve
       response.should redirect_to show_photo_path :id => photo
+
     end
 
-    it 'punts unknown photo Flickr IDs' do
+    it 'punts an unknown photo Flickr ID' do
       #noinspection RubyResolve
       stub(Photo).find_by_flickrid('0123456789') { nil }
       get :view, :from => 'http://www.flickr.com/photos/person_flickrid/0123456789/'
@@ -29,8 +31,21 @@ describe BookmarkletController do
       #noinspection RubyResolve
       stub(Person).find_by_flickrid(person.flickrid) { person }
       get :view, :from => "http://www.flickr.com/people/#{person.flickrid}/"
+
       #noinspection RubyResolve
       response.should redirect_to show_person_path person
+
+    end
+
+    it 'punts an unknown person Flickr ID' do
+      #noinspection RubyResolve
+      stub(Person).find_by_flickrid('person_flickrid') { nil }
+      get :view, :from => "http://www.flickr.com/people/person_flickrid/"
+
+      #noinspection RubyResolve
+      response.should be_success
+      response.should have_text /Sorry, Guess Where Watcher doesn't know anything about that person/
+
     end
 
     it 'punts unknown URLs' do
