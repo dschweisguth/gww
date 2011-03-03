72801b943d51faad54d15906751e91b2c7a8951c
Dave Schweisguth <dave@schweisguth.org>
2011-03-03 14:42:37 -0800

    Added stored procedure to migration to fill score_reports from the data

--- db/migrate/20110303010929_create_score_reports.rb
+++ db/migrate/20110303010929_create_score_reports.rb
@@ -4,7 +4,36 @@ class CreateScoreReports < ActiveRecord::Migration
       t.datetime :created_at
     end
     if RAILS_ENV != 'test'
-      ScoreReport.create! :created_at => Time.local(2011, 2, 26, 20, 0, 0)
+      execute <<EOS
+        create procedure insert_score_reports()
+        begin
+          declare done int default 0;
+          declare n int default 0;
+          declare start, previous, current datetime;
+          declare cu cursor for (select added_at from guesses) union (select added_at from revelations) order by added_at;
+          declare continue handler for not found set done = 1;
+
+          open cu;
+          fetch cu into previous;
+          set start = previous;
+          read_loop: loop
+            fetch cu into current;
+            set	n = n +	1;
+            if done then
+              leave read_loop;
+            end if;
+            if previous < subtime(current, '1 0:0:0') then
+              insert into score_reports values(null, start);
+              set n = 0;
+              set start	= current;
+            end	if;
+            set previous = current;
+          end loop;
+
+        end
+EOS
+      execute 'call insert_score_reports()'
+      execute 'drop procedure insert_score_reports'
     end
   end
 
