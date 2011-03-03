6beeb65c45fe4e7e628b8312132ae3e2c269fcc2
Dave Schweisguth <dave@schweisguth.org>
2011-03-02 21:34:43 -0800

    Tested route I'm about to use

--- spec/routing/admin/score_reports_routing_spec.rb
+++ spec/routing/admin/score_reports_routing_spec.rb
@@ -1,6 +1,18 @@
 require 'spec_helper'
 
 describe Admin::ScoreReportsController do
+  describe 'index' do
+    it 'is routed to' do
+      { :get => '/admin/score_reports' }.should route_to :controller => 'admin/score_reports', :action => 'index'
+    end
+
+    it 'has a named route' do
+      #noinspection RubyResolve
+      score_reports_path.should == '/admin/score_reports'
+    end
+
+  end
+
   describe 'new' do
     it 'is routed to' do
       { :get => '/admin/score_reports/new' }.should route_to :controller => 'admin/score_reports', :action => 'new'
