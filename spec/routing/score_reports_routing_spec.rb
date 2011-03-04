2b1474466bd1a8d3a524ce1eef2d4c555988e343
Dave Schweisguth <dave@schweisguth.org>
2011-03-04 14:32:05 -0800

    Tested user score report routes

---
+++ spec/routing/score_reports_routing_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe ScoreReportsController do
+  describe '#index' do
+    it 'is routed to' do
+      { :get => '/score_reports' }.should route_to :controller => 'score_reports', :action => 'index'
+    end
+
+    it 'has a named route' do
+      #noinspection RubyResolve
+      score_reports_path.should == '/score_reports'
+    end
+
+  end
+
+  describe '#show' do
+    it 'is routed to' do
+      { :get => '/score_reports/666' }.should route_to :controller => 'score_reports', :action => 'show', :id => '666'
+    end
+
+    it 'has a named route' do
+      #noinspection RubyResolve
+      score_report_path('666').should == '/score_reports/666'
+    end
+
+  end
+
+end
