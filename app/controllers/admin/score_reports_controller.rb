a2e65fcb17b2c835e4ee999f7bb263e718252945
Dave Schweisguth <dave@schweisguth.org>
2011-03-04 11:06:17 -0800

    Moved partials to now-correct location

--- app/controllers/admin/score_reports_controller.rb
+++ app/controllers/admin/score_reports_controller.rb
@@ -63,9 +63,9 @@ class Admin::ScoreReportsController < ApplicationController
     @total_single_guessers = @people_by_score[1].nil? ? 1 : @people_by_score[1].length
 
     @html = CGI.escapeHTML \
-      render_to_string :partial => 'admin/score_reports/new/topic_content'
+      render_to_string :partial => 'admin/score_reports/topic_content'
     @topic_content =
-      render_to_string(:partial => 'admin/score_reports/new/topic_content') \
+      render_to_string(:partial => 'admin/score_reports/topic_content') \
         .gsub /$/, '<br/>'
 
   end
