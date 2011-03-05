7cbdbebe83cc0d05d75a895e39aea8bacf02cc4d
Dave Schweisguth <dave@schweisguth.org>
2011-03-05 11:56:54 -0800

    Tested counting guesses in a non-first score report

--- spec/models/score_report_spec.rb
+++ spec/models/score_report_spec.rb
@@ -8,12 +8,22 @@ describe ScoreReport do
 
   describe '.all_with_guess_counts' do
     it "counts the guesses in the first score report" do
-      Guess.make 1, :added_at => Time.utc(2011)
+      Guess.make :added_at => Time.utc(2011)
       report = ScoreReport.make :created_at => Time.utc(2011)
       reports = ScoreReport.all_with_guess_counts
       reports.should == [ report ]
       reports[0][:count].should == 1
     end
+
+    it "counts the guesses in a non-first score report" do
+      Guess.make :added_at => Time.utc(2011)
+      ScoreReport.make :created_at => Time.utc(2010)
+      report1 = ScoreReport.make :created_at => Time.utc(2011)
+      reports = ScoreReport.all_with_guess_counts
+      reports.should == [ report1 ]
+      reports[0][:count].should == 1
+    end
+
   end
 
   describe '.previous' do
