4b8c1c38df6d37f21cd0ceea4b705ae11c45cda1
Dave Schweisguth <dave@schweisguth.org>
2011-03-06 15:11:28 -0800

    Moved ordinal to Fixnum and used it in ScoreReportsControllerSupport

---
+++ spec/lib/fixnum_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Fixnum do
+  without_transactions
+
+  describe '#ordinal' do
+    expected = {
+      1 => '1st',
+      2 => '2nd',
+      3 => '3rd',
+      4 => '4th',
+      11 => '11th',
+      12 => '12th',
+      13 => '13th',
+      21 => '21st',
+      22 => '22nd',
+      23 => '23rd'
+    }
+    expected.keys.sort.each do |cardinal|
+      it "converts the number #{cardinal} to its ordinal, #{expected[cardinal]}" do
+        cardinal.ordinal.should == expected[cardinal]
+      end
+    end
+  end
+
+end
