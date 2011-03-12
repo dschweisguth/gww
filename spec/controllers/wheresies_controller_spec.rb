30ed31ed474312e3ba78a1caa1e6cea7a6f24749
Dave Schweisguth <dave@schweisguth.org>
2011-03-12 14:19:20 -0800

    Added "preliminary" to wheresies/show title

--- spec/controllers/wheresies_controller_spec.rb
+++ spec/controllers/wheresies_controller_spec.rb
@@ -6,85 +6,96 @@ describe WheresiesController do
 
   describe '#show' do
     it 'renders the page' do
-      stub(ScoreReport).first { ScoreReport.make :created_at => Time.local(2009).getutc }
-      stub(Time).now { Time.local(2010) }
+      stub_queries 2009, 2010, 2010
+      get :show, :year => '2010'
 
-      year = 2010
-
-      most_points_in_year = Person.make
-      most_points_in_year[:points] = 111
-      stub(Person).most_points_in(year) { [ most_points_in_year ] }
-
-      most_posts_in_year = Person.make
-      most_posts_in_year[:posts] = 222
-      stub(Person).most_posts_in(year) { [ most_posts_in_year ] }
-
-      rookie_with_most_points_in_year = Person.make
-      rookie_with_most_points_in_year[:points] = 333
-      stub(Person).rookies_with_most_points_in(year) { [ rookie_with_most_points_in_year ] }
-
-      rookie_with_most_posts_in_year = Person.make
-      rookie_with_most_posts_in_year[:posts] = 444
-      stub(Person).rookies_with_most_posts_in(year) { [ rookie_with_most_posts_in_year ] }
-
-      most_viewed_in_year = Photo.make
-      most_viewed_in_year[:views] = 555
-      stub(Photo).most_viewed_in(year) { [ most_viewed_in_year ] }
-
-      most_commented_in_year = Photo.make
-      most_commented_in_year[:comments] = 666
-      stub(Photo).most_commented_in(year) { [ most_commented_in_year ] }
-
-      longest_in_year_photo = Photo.make :dateadded => Time.local(year).getutc
-      longest_in_year = Guess.make :photo => longest_in_year_photo,
-        :guessed_at => Time.local(year).getutc + 1.year
-      stub(Guess).longest_in(year) { [ longest_in_year ] }
-
-      shortest_in_year_photo = Photo.make :dateadded => Time.local(year).getutc
-      shortest_in_year = Guess.make :photo => shortest_in_year_photo,
-        :guessed_at => Time.local(year).getutc + 1.second
-      stub(Guess).shortest_in(year) { [ shortest_in_year ] }
-
-      get :show, :year => year.to_s
       #noinspection RubyResolve
       response.should be_success
 
       response.should have_tag 'a[href=/wheresies/2009]', :text => '2009'
       response.should_not have_tag 'a[href=/wheresies/2010]'
 
+      response.should have_tag 'h1', :text => /2010 Wheresies \(preliminary\)/
+
       response.should have_tag 'div' do
-        with_tag 'h3', :text => "Most points in #{year}"
+        with_tag 'h3', :text => "Most points in 2010"
         with_tag 'td', :text => '333'
       end
       response.should have_tag 'div' do
-        with_tag 'h3', :text => "Most posts in #{year}"
+        with_tag 'h3', :text => "Most posts in 2010"
         with_tag 'td', :text => '444'
       end
       response.should have_tag 'div' do
-        with_tag 'h3', :text => "Most points in #{year}"
+        with_tag 'h3', :text => "Most points in 2010"
         with_tag 'td', :text => '111'
       end
       response.should have_tag 'div' do
-        with_tag 'h3', :text => "Most posts in #{year}"
+        with_tag 'h3', :text => "Most posts in 2010"
         with_tag 'td', :text => '222'
       end
       response.should have_tag 'div' do
-        with_tag 'h2', :text => "Most-viewed photos of #{year}"
+        with_tag 'h2', :text => "Most-viewed photos of 2010"
         with_tag 'td', :text => '555'
       end
       response.should have_tag 'div' do
-        with_tag 'h2', :text => "Most-commented photos of #{year}"
+        with_tag 'h2', :text => "Most-commented photos of 2010"
         with_tag 'td', :text => '666'
       end
       response.should have_tag 'td', :text => '1&nbsp;year'
       response.should have_tag 'td', :text => '1&nbsp;second'
+
     end
 
     it 'bails out if the year is invalid' do
-      stub(ScoreReport).first { ScoreReport.make :created_at => Time.local(2010).getutc }
-      stub(Time).now { Time.local(2010) }
-      lambda { get :show, :year => "2011" }.should raise_error ActiveRecord::RecordNotFound
+      stub_queries 2010, 2010, 2011
+      lambda { get :show, :year => '2011' }.should raise_error ActiveRecord::RecordNotFound
     end
+
+    it "doesn't says 'preliminary' if it's not for this year" do
+      stub_queries 2009, 2010, 2009
+      get :show, :year => '2009'
+      response.should_not have_tag 'h1', :text => /2009 Wheresies \(preliminary\)/
+    end
+
+  end
+
+  def stub_queries(first_year, current_year, year)
+    stub(ScoreReport).first { ScoreReport.make :created_at => Time.local(first_year).getutc }
+    stub(Time).now { Time.local(current_year) }
+
+    most_points_in_year = Person.make
+    most_points_in_year[:points] = 111
+    stub(Person).most_points_in(year) { [ most_points_in_year ] }
+
+    most_posts_in_year = Person.make
+    most_posts_in_year[:posts] = 222
+    stub(Person).most_posts_in(year) { [ most_posts_in_year ] }
+
+    rookie_with_most_points_in_year = Person.make
+    rookie_with_most_points_in_year[:points] = 333
+    stub(Person).rookies_with_most_points_in(year) { [ rookie_with_most_points_in_year ] }
+
+    rookie_with_most_posts_in_year = Person.make
+    rookie_with_most_posts_in_year[:posts] = 444
+    stub(Person).rookies_with_most_posts_in(year) { [ rookie_with_most_posts_in_year ] }
+
+    most_viewed_in_year = Photo.make
+    most_viewed_in_year[:views] = 555
+    stub(Photo).most_viewed_in(year) { [ most_viewed_in_year ] }
+
+    most_commented_in_year = Photo.make
+    most_commented_in_year[:comments] = 666
+    stub(Photo).most_commented_in(year) { [ most_commented_in_year ] }
+
+    longest_in_year_photo = Photo.make :dateadded => Time.local(year).getutc
+    longest_in_year = Guess.make :photo => longest_in_year_photo,
+      :guessed_at => Time.local(year).getutc + 1.year
+    stub(Guess).longest_in(year) { [ longest_in_year ] }
+
+    shortest_in_year_photo = Photo.make :dateadded => Time.local(year).getutc
+    shortest_in_year = Guess.make :photo => shortest_in_year_photo,
+      :guessed_at => Time.local(year).getutc + 1.second
+    stub(Guess).shortest_in(year) { [ shortest_in_year ] }
 
   end
 
