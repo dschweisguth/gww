36eab17ff7f2b965fc430f77f318020e643d3f1f
Dave Schweisguth <dave@schweisguth.org>
2011-03-17 14:22:46 -0700

    Extracted method

---
+++ app/helpers/admin/photos_helper.rb
@@ -0,0 +1,10 @@
+module Admin::PhotosHelper
+  def wrap_if(condition, begin_tag, end_tag)
+    result = condition ? begin_tag : ""
+    result << yield
+    if condition
+      result << end_tag
+    end
+    result
+  end
+end
