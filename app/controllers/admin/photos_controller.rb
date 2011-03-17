eade9e9db06c8821282d765366786c516b753f81
Dave Schweisguth <dave@schweisguth.org>
2011-03-16 21:39:46 -0700

    Extracted a method to Comment

--- app/controllers/admin/photos_controller.rb
+++ app/controllers/admin/photos_controller.rb
@@ -80,73 +80,7 @@ class Admin::PhotosController < ApplicationController
   end
 
   def add_answer
-    comment_id = params[:comment_id]
-    #noinspection RailsParamDefResolve
-    comment = Comment.find comment_id, :include => { :photo => :revelation }
-    photo = comment.photo
-
-    username = params[:username]
-    if username.empty?
-      guesser_flickrid = comment.flickrid
-      guesser_username = comment.username
-    else
-      # Note that this branch results in a guess that can't be individually removed
-      guesser_flickrid = Comment.find_by_username(username).flickrid
-      guesser_username = username
-    end
-    Photo.transaction do
-      if guesser_flickrid == photo.person.flickrid
-        photo.game_status = 'revealed'
-        photo.save!
-
-        revelation = photo.revelation
-        if revelation
-          revelation.revelation_text = comment.comment_text
-          revelation.revealed_at = comment.commented_at
-          # TODO Dave update added_at
-          revelation.save!
-        else
-          Revelation.create! \
-            :photo => photo,
-            :revelation_text => comment.comment_text,
-            :revealed_at => comment.commented_at,
-            :added_at => Time.now.getutc
-        end
-
-        Guess.destroy_all_by_photo_id photo.id
-
-      else
-        photo.game_status = 'found'
-        photo.save!
-
-        guesser = Person.find_by_flickrid guesser_flickrid
-        if guesser
-          guess = Guess.find_by_photo_id_and_person_id photo.id, guesser.id
-        else
-          guesser = Person.create! \
-            :flickrid => guesser_flickrid,
-            :username => guesser_username
-          guess = nil
-        end
-        if guess
-          guess.guessed_at = comment.commented_at
-          guess.guess_text = comment.comment_text
-          # TODO Dave update added_at
-          guess.save!
-        else
-          Guess.create! \
-            :photo => photo,
-            :person => guesser,
-            :guess_text => comment.comment_text,
-            :guessed_at => comment.commented_at,
-            :added_at => Time.now.getutc
-        end
-
-        photo.revelation.destroy if photo.revelation
-
-      end
-    end
-
+    Comment.add_answer params[:comment_id], params[:username]
     PageCache.clear
     redirect_to edit_photo_path :id => params[:id], :nocomment => 'true'
   end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,74 @@ class Comment < ActiveRecord::Base
   validates_presence_of :flickrid, :username, :comment_text, :commented_at
   attr_readonly :flickrid, :username, :comment_text, :commented_at
 
+  def self.add_answer(comment_id, username)
+    #noinspection RailsParamDefResolve
+    comment = Comment.find comment_id, :include => { :photo => [ :person, :revelation ] }
+
+    if username.empty?
+      guesser_flickrid = comment.flickrid
+      guesser_username = comment.username
+    else
+      # Note that this branch results in a guess that can't be individually removed
+      guesser_flickrid = Comment.find_by_username(username).flickrid
+      guesser_username = username
+    end
+    Photo.transaction do
+      photo = comment.photo
+      if guesser_flickrid == photo.person.flickrid
+        photo.game_status = 'revealed'
+        photo.save!
+
+        revelation = photo.revelation
+        if revelation
+          revelation.revelation_text = comment.comment_text
+          revelation.revealed_at = comment.commented_at
+          # TODO Dave update added_at
+          revelation.save!
+        else
+          Revelation.create! \
+            :photo => photo,
+            :revelation_text => comment.comment_text,
+            :revealed_at => comment.commented_at,
+            :added_at => Time.now.getutc
+        end
+
+        Guess.destroy_all_by_photo_id photo.id
+
+      else
+        photo.game_status = 'found'
+        photo.save!
+
+        guesser = Person.find_by_flickrid guesser_flickrid
+        if guesser
+          guess = Guess.find_by_photo_id_and_person_id photo.id, guesser.id
+        else
+          guesser = Person.create! \
+            :flickrid => guesser_flickrid,
+            :username => guesser_username
+          guess = nil
+        end
+        if guess
+          guess.guessed_at = comment.commented_at
+          guess.guess_text = comment.comment_text
+          # TODO Dave update added_at
+          guess.save!
+        else
+          Guess.create! \
+            :photo => photo,
+            :person => guesser,
+            :guess_text => comment.comment_text,
+            :guessed_at => comment.commented_at,
+            :added_at => Time.now.getutc
+        end
+
+        photo.revelation.destroy if photo.revelation
+
+      end
+    end
+
+  end
+
   def self.remove_revelation(comment_id)
     transaction do
       #noinspection RailsParamDefResolve
