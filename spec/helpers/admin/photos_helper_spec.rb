21df2a63fe56839b3d99be92afa8627b85135972
Dave Schweisguth <dave@schweisguth.org>
2011-03-17 14:26:33 -0700

    Tested new method

---
+++ spec/helpers/admin/photos_helper_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Admin::PhotosHelper do
+  describe '#wrap_if' do
+    it "wraps if the condition is true" do
+      helper.wrap_if(true, '<begin>', '<end>') { 'content' }.should == '<begin>content<end>'
+    end
+  end
+
+  describe '#wrap_if' do
+    it "doesn't wrap if the condition is false" do
+      helper.wrap_if(false, '<begin>', '<end>') { 'content' }.should == 'content'
+    end
+  end
+
+end
