efd8d42200ddd2a40db9230a94e169446b226b62
Dave Schweisguth <dave@schweisguth.org>
2011-03-17 15:49:58 -0700

    Merged identical describe blocks

--- spec/helpers/admin/photos_helper_spec.rb
+++ spec/helpers/admin/photos_helper_spec.rb
@@ -5,12 +5,10 @@ describe Admin::PhotosHelper do
     it "wraps if the condition is true" do
       helper.wrap_if(true, '<begin>', '<end>') { 'content' }.should == '<begin>content<end>'
     end
-  end
 
-  describe '#wrap_if' do
     it "doesn't wrap if the condition is false" do
       helper.wrap_if(false, '<begin>', '<end>') { 'content' }.should == 'content'
     end
-  end
 
+  end
 end
