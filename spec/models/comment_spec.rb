4f0eb89a6d31c867195c66e7a41b67b0b8013bac
Dave Schweisguth <dave@schweisguth.org>
2011-03-16 22:36:23 -0700

    Tested Comment.add_answer

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -39,4 +39,109 @@ describe Comment do
     it { should have_readonly_attribute :commented_at }
   end
 
+  describe '.add_answer' do
+    describe 'when adding a revelation' do
+      it 'adds a revelation' do
+        photo = Photo.make
+        comment = Comment.make :photo => photo, :flickrid => photo.person.flickrid,
+          :username => photo.person.username, :commented_at => Time.utc(2011)
+        Comment.add_answer comment.id, ''
+        photo.reload
+        photo.game_status.should == 'revealed'
+        revelation = Revelation.find_by_photo_id comment.photo.id
+        revelation.revelation_text.should == comment.comment_text
+        revelation.revealed_at.should == comment.commented_at
+      end
+
+      it 'updates an existing revelation' do
+        old_revelation = Revelation.make
+        comment = Comment.make :photo => old_revelation.photo,
+          :flickrid => old_revelation.photo.person.flickrid,
+          :username => old_revelation.photo.person.username,
+          :commented_at => Time.utc(2011)
+        Comment.add_answer comment.id, ''
+        new_revelations = Revelation.find_all_by_photo_id comment.photo
+        new_revelations.should == [ old_revelation ]
+        new_revelation = new_revelations[0]
+        # Note that the following two values are different than those for old_guess
+        new_revelation.revelation_text.should == comment.comment_text
+        new_revelation.revealed_at.should == comment.commented_at
+      end
+
+      it 'deletes an existing guess' do
+        photo = Photo.make
+        comment = Comment.make :photo => photo, :flickrid => photo.person.flickrid,
+          :username => photo.person.username, :commented_at => Time.utc(2011)
+        guess = Guess.make :photo => photo
+        Comment.add_answer comment.id, ''
+        Guess.count.should == 0
+        owner_does_not_exist guess
+      end
+
+    end
+
+    describe 'when adding a guess' do
+      it 'adds a guess' do
+        guesser = Person.make
+        comment = Comment.make :flickrid => guesser.flickrid,
+          :username => guesser.username, :commented_at => Time.utc(2011)
+        Comment.add_answer comment.id, ''
+        guess = Guess.find_by_photo_id comment.photo
+        guess.person_id.should == guesser.id
+        guess.guess_text.should == comment.comment_text
+        guess.guessed_at.should == comment.commented_at
+        guess.photo.reload
+        guess.photo.game_status.should == 'found'
+      end
+
+      it 'creates the guesser if necessary' do
+        comment = Comment.make
+        Comment.add_answer comment.id, ''
+        guess = Guess.find_by_photo_id comment.photo, :include => :person
+        guess.person.flickrid.should == comment.flickrid
+        guess.person.username.should == comment.username
+      end
+
+      it 'gives the point to another user' do
+        scorer = Person.make 'scorer'
+        scorer_comment = Comment.make 'scorer',
+          :flickrid => scorer.flickrid, :username => scorer.username
+        answer_comment = Comment.make 'answer', :commented_at => Time.utc(2011)
+        Comment.add_answer answer_comment.id, scorer_comment.username
+        guess = Guess.find_by_photo_id answer_comment.photo, :include => :person
+        guess.person.flickrid.should == scorer_comment.flickrid
+        guess.person.username.should == scorer_comment.username
+        guess.guess_text.should == answer_comment.comment_text
+        guess.guessed_at.should == answer_comment.commented_at
+        answer_comment.photo.reload
+        answer_comment.photo.game_status.should == 'found'
+      end
+
+      it 'updates an existing guess' do
+        old_guess = Guess.make
+        comment = Comment.make :photo => old_guess.photo,
+          :flickrid => old_guess.person.flickrid, :username => old_guess.person.username,
+          :commented_at => Time.utc(2011)
+        Comment.add_answer comment.id, ''
+        new_guesses = Guess.find_all_by_photo_id comment.photo
+        new_guesses.should == [ old_guess ]
+        new_guess = new_guesses[0]
+        # Note that the following two values are different than those for old_guess
+        new_guess.guess_text.should == comment.comment_text
+        new_guess.guessed_at.should == comment.commented_at
+      end
+
+      it 'deletes an existing revelation' do
+        guesser = Person.make
+        comment = Comment.make :flickrid => guesser.flickrid,
+          :username => guesser.username, :commented_at => Time.utc(2011)
+        Revelation.make :photo => comment.photo
+        Comment.add_answer comment.id, ''
+        Revelation.count.should == 0
+      end
+
+    end
+
+  end
+
 end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1057,15 +1057,6 @@ describe Photo do
 
   end
 
-  # Used by specs which delete a photo or guess to assert that the owner had no
-  # other photos or other guesses, so they should have been deleted too.
-  # It would be nice to mock the method that deletes the owner, which handles
-  # cases where the owner has a photo or other guess and shouldn't be deleted,
-  # but doing so would be ugly.
-  def owner_does_not_exist(owner)
-    Person.exists?(owner.person.id).should == false
-  end
-
   describe '#years_old' do
     it "returns 0 for a photo posted moments ago" do
       Photo.make(:dateadded => Time.now).years_old.should == 0
