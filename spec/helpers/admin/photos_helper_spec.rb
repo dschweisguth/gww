1e0984d8b4e7ddc63bd456db50682295d8058432
Dave Schweisguth <dave@schweisguth.org>
2011-03-17 16:52:21 -0700

    Improved descriptions

--- spec/helpers/admin/photos_helper_spec.rb
+++ spec/helpers/admin/photos_helper_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe Admin::PhotosHelper do
   describe 'ago' do
-    it "returns 'a moment ago' if the time is < 1 second ago" do
+    it "returns 'a moment ago' if time < 1 second ago" do
       stub(Time).now { Time.utc(2011) }
       helper.ago(Time.utc(2011)).should == 'a moment ago'
     end
 
-    it "returns 'n seconds ago' if the time is < 1 minute ago " do
+    it "returns 'n seconds ago' if 1 second ago <= time < 1 minute ago " do
       stub(Time).now { Time.utc(2011, 1, 1, 0, 0, 1) }
       helper.ago(Time.utc(2011)).should == '1 second ago'
     end
@@ -17,7 +17,7 @@ describe Admin::PhotosHelper do
       helper.ago(Time.utc(2011)).should == '2 seconds ago'
     end
 
-    it "returns 'n minutes ago' if the time is < 1 hour ago" do
+    it "returns 'n minutes ago' if 1 minute ago <= time < 1 hour ago" do
       stub(Time).now { Time.utc(2011, 1, 1, 0, 1, 0) }
       helper.ago(Time.utc(2011)).should == '1 minute ago'
     end
@@ -32,7 +32,7 @@ describe Admin::PhotosHelper do
       helper.ago(Time.utc(2010, 12, 31, 23, 59, 0)).should == '1 minute ago'
     end
 
-    it "returns 'n hours ago' if the time is < 1 day ago" do
+    it "returns 'n hours ago' if 1 hour ago <= time < 1 day ago" do
       stub(Time).now { Time.utc(2011, 1, 1, 1, 0, 0) }
       helper.ago(Time.utc(2011)).should == '1 hour ago'
     end
@@ -47,7 +47,7 @@ describe Admin::PhotosHelper do
       helper.ago(Time.utc(2010, 12, 31, 23, 0, 0)).should == '1 hour ago'
     end
 
-    it "returns 'n days ago' if the time is < 1 month ago" do
+    it "returns 'n days ago' if 1 day ago <= time < 1 month ago" do
       stub(Time).now { Time.utc(2011, 1, 2, 0, 0, 0) }
       helper.ago(Time.utc(2011)).should == '1 day ago'
     end
@@ -62,7 +62,7 @@ describe Admin::PhotosHelper do
       helper.ago(Time.utc(2010, 12, 31, 0, 0, 0)).should == '1 day ago'
     end
 
-    it "returns 'n months ago'" do
+    it "returns 'n months ago' if 1 month ago <= time" do
       stub(Time).now { Time.utc(2011, 2, 1, 0, 0, 0) }
       helper.ago(Time.utc(2011)).should == '1 month ago'
     end
