e168a2248a8b8a3e0522cbfde13e7e507a11af8b
Dave Schweisguth <dave@schweisguth.org>
2011-03-17 16:22:23 -0700

    First several tests of ago

--- app/helpers/admin/photos_helper.rb
+++ app/helpers/admin/photos_helper.rb
@@ -1,4 +1,25 @@
+#require 'action_view/helpers/text_helper'
+
 module Admin::PhotosHelper
+
+  def ago(time)
+    now = Time.now
+    ago =
+      if time > now - 1.second
+        'a moment'
+      elsif time > now - 1.minute
+        pluralize (now - time).to_i, 'second'
+      elsif time > now - 1.hour
+        minutes = now.min - time.min
+        if minutes < 0
+          minutes += 60
+        end
+        pluralize minutes, 'minute'
+      end
+    ago << ' ago'
+    ago
+  end
+
   def wrap_if(condition, begin_tag, end_tag)
     result = condition ? begin_tag : ""
     result << yield
@@ -7,4 +28,5 @@ module Admin::PhotosHelper
     end
     result
   end
+
 end
--- spec/helpers/admin/photos_helper_spec.rb
+++ spec/helpers/admin/photos_helper_spec.rb
@@ -1,6 +1,39 @@
 require 'spec_helper'
 
 describe Admin::PhotosHelper do
+  describe 'ago' do
+    it "returns 'a moment ago' if the time is < 1 second ago" do
+      stub(Time).now { Time.utc(2011) }
+      helper.ago(Time.utc(2011)).should == 'a moment ago'
+    end
+
+    it "returns 'n seconds ago' if the time is < 1 minute ago " do
+      stub(Time).now { Time.utc(2011, 1, 1, 0, 0, 1) }
+      helper.ago(Time.utc(2011)).should == '1 second ago'
+    end
+
+    it "pluralizes seconds" do
+      stub(Time).now { Time.utc(2011, 1, 1, 0, 0, 2) }
+      helper.ago(Time.utc(2011)).should == '2 seconds ago'
+    end
+
+    it "returns '1 minute ago' if 1 minute <= time < 2 minutes" do
+      stub(Time).now { Time.utc(2011, 1, 1, 0, 1, 0) }
+      helper.ago(Time.utc(2011)).should == '1 minute ago'
+    end
+
+    it "pluralizes minutes" do
+      stub(Time).now { Time.utc(2011, 1, 1, 0, 2, 0) }
+      helper.ago(Time.utc(2011)).should == '2 minutes ago'
+    end
+
+    it "wraps minutes" do
+      stub(Time).now { Time.utc(2011) }
+      helper.ago(Time.utc(2010, 12, 31, 23, 59, 0)).should == '1 minute ago'
+    end
+
+  end
+
   describe '#wrap_if' do
     it "wraps if the condition is true" do
       helper.wrap_if(true, '<begin>', '<end>') { 'content' }.should == '<begin>content<end>'
@@ -11,4 +44,5 @@ describe Admin::PhotosHelper do
     end
 
   end
+
 end
