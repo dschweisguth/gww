50fbda1ec7ff182af4bfe6b7f646ea41f6581c7f
Dave Schweisguth <dave@schweisguth.org>
2011-03-18 15:44:03 -0700

    Define the function outside the loop

--- public/javascripts/editPhoto.js
+++ public/javascripts/editPhoto.js
@@ -1,4 +1,10 @@
 (function () {
+  function setUpCopyUsernameFor(comment_id) {
+    $('submit_' + comment_id).observe('click', function () {
+      $('username_' + comment_id).value = $('person_username').value;
+    });
+  }
+
   function setUpCopyUsername() {
     var comments = $('comments');
     if (comments) {
@@ -6,11 +12,7 @@
       for (var i = 0; i < forms.length; i++) {
         if (/add_selected_answer/.test(forms[i].readAttribute('action'))) {
           var comment_id = forms[i]['comment_id'].value;
-          (function (comment_id) {
-            $('submit_' + comment_id).observe('click', function () {
-              $('username_' + comment_id).value = $('person_username').value;
-            });
-          })(comment_id);
+          setUpCopyUsernameFor(comment_id);
         }
       }
     }
