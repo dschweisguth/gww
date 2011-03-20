31c9da71e46522e13565e7d680ca2a6b78bdc7dd
Dave Schweisguth <dave@schweisguth.org>
2011-03-20 09:27:31 -0700

    Used shoulda matcher

--- spec/routing/revelations_routing_spec.rb
+++ spec/routing/revelations_routing_spec.rb
@@ -4,7 +4,7 @@ describe RevelationsController do
   without_transactions
 
   it 'routes to a plain action' do
-    { :get => '/revelations/longest' }.should route_to :controller => 'revelations', :action => 'longest'
+    should route(:get, '/revelations/longest').to :controller => 'revelations', :action => 'longest'
   end
 
 end
