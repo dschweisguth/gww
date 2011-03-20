8e997c5e0dc2c94e8d7530c5d54e29aef8cc1404
Dave Schweisguth <dave@schweisguth.org>
2011-03-20 09:26:32 -0700

    Used shoulda matcher

--- spec/routing/photos_routing_spec.rb
+++ spec/routing/photos_routing_spec.rb
@@ -9,10 +9,8 @@ describe PhotosController do
       list_photos_path('foo', 'bar', '1').should == '/photos/list/sorted-by/foo/order/bar/page/1'
     end
 
-    it 'is routed to' do
-      { :get => '/photos/list/sorted-by/foo/order/bar/page/666' }.should route_to \
-        :controller => 'photos', :action => 'list', :sorted_by => 'foo', :order => 'bar', :page => '666'
-    end
+    it { should route(:get, '/photos/list/sorted-by/foo/order/bar/page/666').to(
+      :controller => 'photos', :action => 'list', :sorted_by => 'foo', :order => 'bar', :page => '666') }
 
   end
 
@@ -22,18 +20,16 @@ describe PhotosController do
       show_photo_path(666).should == '/photos/show/666'
     end
 
-    it 'is routed to' do
-      { :get => '/photos/show/666' }.should route_to :controller => 'photos', :action => 'show', :id => '666'
-    end
+    it { should route(:get, '/photos/show/666').to :controller => 'photos', :action => 'show', :id => '666' }
 
   end
 
   it 'routes to a plain action' do
-    { :get => '/photos/unfound' }.should route_to :controller => 'photos', :action => 'unfound'
+    should route(:get, '/photos/unfound').to :controller => 'photos', :action => 'unfound'
   end
 
   it 'routes to a plain action with an ID' do
-    { :get => '/photos/show/666' }.should route_to :controller => 'photos', :action => 'show', :id => '666'
+    should route(:get, '/photos/show/666').to :controller => 'photos', :action => 'show', :id => '666'
   end
 
 end
