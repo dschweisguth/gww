57818345b252c1bed2864b50d0fe3d966058045b
Dave Schweisguth <dave@schweisguth.org>
2011-03-20 13:31:42 -0700

    Moved matcher to support

--- spec/routing/admin/root_routing_spec.rb
+++ spec/routing/admin/root_routing_spec.rb
@@ -3,47 +3,6 @@ require 'spec_helper'
 describe Admin::RootController do
   without_transactions
 
-  # TODO Dave move to support
-  def have_named_route(name, *args)
-    HaveNamedRoute.new(self, name, *args)
-  end
-
-  class HaveNamedRoute
-    def initialize(context, name, *args)
-      @context = context
-      @name = name
-      @path = "#{name}_path"
-      @args = args
-      if ! args.last
-        raise ArgumentError, 'The last argument must be the expected uri'
-      end
-      @expected_uri = args.pop
-    end
-
-    def description
-      "have a route named #{@name}, where e.g. #{example_call} == #{@expected_uri}"
-    end
-
-    def matches?(subject)
-      @actual_uri = @context.send("#{@name}_path", *@args)
-      @actual_uri == @expected_uri
-    end
-
-    def failure_message_for_should
-      "expected #{example_call} to equal #{@expected_uri}, but got #{@actual_uri}"
-    end
-
-    def failure_message_for_should_not
-      "expected #{example_call} to not equal #{@expected_uri}, but it did"
-    end
-
-    def example_call
-      # TODO Dave remove parens if not needed
-      "#{@name}_path(#{@args.map(&:to_s).join(', ')})"
-    end
-
-  end
-
   describe 'index' do
     it { should have_named_route :admin_root, '/admin' }
     it { should route(:get, '/admin').to :controller => 'admin/root', :action => 'index' }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -52,4 +52,5 @@ Spec::Runner.configure do |config|
   #
   # For more information take a look at Spec::Runner::Configuration and Spec::Runner
   config.include GWW::Matchers::Model, :type => :model
+  config.include GWW::Matchers::Routing, :type => :routing
 end
