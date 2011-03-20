3d9ab01a66281ad6356bf8be94619b25b1a65e7a
Dave Schweisguth <dave@schweisguth.org>
2011-03-20 09:54:53 -0700

    Put GWW matchers in their own namespace

--- spec/support/matchers/validate_non_negative_integer_matcher.rb
+++ spec/support/matchers/validate_non_negative_integer_matcher.rb
@@ -1,24 +1,22 @@
-module Shoulda
-  module ActiveRecord
-    module Matchers
-      def validate_non_negative_integer(attr)
-        ValidateNonNegativeIntegerMatcher.new(attr)
+module GWW
+  module Matchers
+    def validate_non_negative_integer(attr)
+      ValidateNonNegativeInteger.new(attr)
+    end
+
+    class ValidateNonNegativeInteger < Shoulda::ActiveRecord::Matchers::ValidationMatcher
+      def matches?(subject)
+        super(subject)
+        disallows_value_of('abcd', :not_a_number) &&
+          disallows_value_of(-1, "must be greater than or equal to 0") &&
+          disallows_value_of(0.1, :not_a_number)
       end
 
-      class ValidateNonNegativeIntegerMatcher < ValidationMatcher
-        def matches?(subject)
-          super(subject)
-          disallows_value_of('abcd', :not_a_number) &&
-            disallows_value_of(-1, "must be greater than or equal to 0") &&
-            disallows_value_of(0.1, :not_a_number)
-        end
-
-        def description
-          "only allow non-negative integer for #{@attribute}"
-        end
-
+      def description
+        "only allow non-negative integer for #{@attribute}"
       end
 
     end
+
   end
 end
