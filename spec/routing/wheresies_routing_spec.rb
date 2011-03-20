f09206b35fe7f6643291bc62f55497b3760e39f1
Dave Schweisguth <dave@schweisguth.org>
2011-03-20 09:34:05 -0700

    Tested named route

--- spec/routing/wheresies_routing_spec.rb
+++ spec/routing/wheresies_routing_spec.rb
@@ -4,7 +4,13 @@ describe WheresiesController do
   without_transactions
 
   describe '#show' do
+    it 'has a named route' do
+      #noinspection RubyResolve
+      wheresies_path(2010).should == '/wheresies/2010'
+    end
+
     it { should route(:get, '/wheresies/2010').to :controller => 'wheresies', :action => 'show', :year => '2010' }
+
   end
 
 end
