fdac5290e98037616e57cba4731034816fb0aca1
Dave Schweisguth <dave@schweisguth.org>
2011-03-20 09:07:20 -0700

    Used shoulda route matcher

--- spec/routing/admin/score_reports_routing_spec.rb
+++ spec/routing/admin/score_reports_routing_spec.rb
@@ -11,9 +11,7 @@ describe Admin::ScoreReportsController do
   end
 
   describe '#index' do
-    it 'is routed to' do
-      { :get => '/admin/score_reports' }.should route_to :controller => 'admin/score_reports', :action => 'index'
-    end
+    it { should route(:get, '/admin/score_reports').to :controller => 'admin/score_reports', :action => 'index' }
   end
 
   describe '#new' do
@@ -22,27 +20,21 @@ describe Admin::ScoreReportsController do
       new_admin_score_report_path.should == '/admin/score_reports/new'
     end
 
-    it 'is routed to' do
-      { :get => '/admin/score_reports/new' }.should route_to :controller => 'admin/score_reports', :action => 'new'
-    end
+    it { should route(:get, '/admin/score_reports/new').to :controller => 'admin/score_reports', :action => 'new' }
 
   end
 
   describe '#create' do
-    it 'is routed to' do
-      { :post => '/admin/score_reports' }.should route_to :controller => 'admin/score_reports', :action => 'create'
-    end
+    it { should route(:post, '/admin/score_reports').to :controller => 'admin/score_reports', :action => 'create' }
   end
 
   describe '#destroy' do
-    it 'is routed to' do
-      { :delete => '/admin/score_reports/666' }.should route_to :controller => 'admin/score_reports', :action => 'destroy', :id => '666'
-    end
-
     it 'has a named route' do
       #noinspection RubyResolve
       admin_score_report_path('666').should == '/admin/score_reports/666'
     end
+
+    it { should route(:delete, '/admin/score_reports/666').to :controller => 'admin/score_reports', :action => 'destroy', :id => '666' }
 
   end
 
