843d4438bf503d420d5063fc1e4a1b82a7f07e68
Dave Schweisguth <dave@schweisguth.org>
2011-03-20 09:31:59 -0700

    Used shoulda matcher

--- spec/routing/wheresies_routing_spec.rb
+++ spec/routing/wheresies_routing_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe WheresiesController do
   without_transactions
 
-  it 'routes to show' do
-    { :get => '/wheresies/2010' }.should route_to :controller => 'wheresies', :action => 'show', :year => '2010'
+  describe '#show' do
+    it { should route(:get, '/wheresies/2010').to :controller => 'wheresies', :action => 'show', :year => '2010' }
   end
 
 end
