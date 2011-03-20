ae45d7283e2b7ec3807bb0719b86dffeaf40e892
Dave Schweisguth <dave@schweisguth.org>
2011-03-20 08:59:31 -0700

    Used shoulda route matcher

--- spec/routing/admin/root_routing_spec.rb
+++ spec/routing/admin/root_routing_spec.rb
@@ -9,14 +9,10 @@ describe Admin::RootController do
       admin_root_path.should == '/admin'
     end
 
-    it 'is routed to' do
-      {:get => '/admin'}.should route_to :controller => 'admin/root', :action => 'index'
-    end
+    it { should route(:get, '/admin').to :controller => 'admin/root', :action => 'index' }
 
   end
 
-  it 'routes to a plain action' do
-    { :get => '/admin/bookmarklet' }.should route_to :controller => 'admin/root', :action => 'bookmarklet'
-  end
+  it { should route(:get, '/admin/bookmarklet').to :controller => 'admin/root', :action => 'bookmarklet' }
 
 end
