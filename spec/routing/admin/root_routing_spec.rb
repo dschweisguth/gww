2e655cc45bd4f96c4ce990d195eab8edce00f2e4
Dave Schweisguth <dave@schweisguth.org>
2011-03-20 22:54:48 -0700

    Made generic test specific

--- spec/routing/admin/root_routing_spec.rb
+++ spec/routing/admin/root_routing_spec.rb
@@ -8,8 +8,8 @@ describe Admin::RootController do
     it { should route(:get, '/admin').to :controller => 'admin/root', :action => 'index' }
   end
 
-  it 'routes to a plain action' do
-    should route(:get, '/admin/bookmarklet').to :controller => 'admin/root', :action => 'bookmarklet'
+  describe 'bookmarklet' do
+    it { should route(:get, '/admin/bookmarklet').to :controller => 'admin/root', :action => 'bookmarklet' }
   end
 
 end
