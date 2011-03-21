3ab6adacac6a5eb73567f7498222c0bae54079fb
Dave Schweisguth <dave@schweisguth.org>
2011-03-21 12:45:43 -0700

    Used have_named_route

--- spec/routing/score_reports_routing_spec.rb
+++ spec/routing/score_reports_routing_spec.rb
@@ -4,23 +4,13 @@ describe ScoreReportsController do
   without_transactions
 
   describe '#index' do
-    it 'has a named route' do
-      #noinspection RubyResolve
-      score_reports_path.should == '/score_reports'
-    end
-
+    it { should have_named_route :score_reports, '/score_reports' }
     it { should route(:get, '/score_reports').to :controller => 'score_reports', :action => 'index' }
-
   end
 
   describe '#show' do
-    it 'has a named route' do
-      #noinspection RubyResolve
-      score_report_path('666').should == '/score_reports/666'
-    end
-
+    it { should have_named_route :score_report, 666, '/score_reports/666' }
     it { should route(:get, '/score_reports/666').to :controller => 'score_reports', :action => 'show', :id => '666' }
-
   end
 
 end
