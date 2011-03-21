afd81e2503854e182e6a2ea1aef46e0021e0edf9
Dave Schweisguth <dave@schweisguth.org>
2011-03-21 16:11:00 -0700

    Removed obsolete route

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,6 @@ ActionController::Routing::Routes.draw do |map|
     user.with_options :controller => 'people' do |people|
       people.find_person 'people/find', :action => 'find'
       people.people 'people/sorted-by/:sorted_by/order/:order', :action => 'index'
-      people.nemeses 'people/nemeses', :action => 'nemeses'
       people.show_person 'people/show/:id', :action => 'show'
       people.list_comments 'people/comments/:id/page/:page', :action => 'comments'
     end
