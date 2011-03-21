26cedcd33fed05e97e78f05d6aaa743e13d576e8
Dave Schweisguth <dave@schweisguth.org>
2011-03-20 23:04:44 -0700

    Used have_named_route

--- spec/routing/admin/photos_routing_spec.rb
+++ spec/routing/admin/photos_routing_spec.rb
@@ -5,65 +5,38 @@ describe Admin::PhotosController do
 
   %w{ update_all_from_flickr update_statistics }.each do |action|
     describe action do
-      it 'has a named route' do
-        send("#{action}_path").should == "/admin/photos/#{action}"
-      end
-
+      it { should have_named_route action, "/admin/photos/#{action}" }
       it { should route(:post, "/admin/photos/#{action}").to :controller => 'admin/photos', :action => action }
-
     end
   end
 
   %w{ unfound inaccessible multipoint }.each do |action|
     describe action do
-      it 'has a named route' do
-        send("#{action}_admin_photos_path").should == "/admin/photos/#{action}"
-      end
-
+      it { should have_named_route "#{action}_admin_photos", "/admin/photos/#{action}" }
       it { should route(:get, "/admin/photos/#{action}").to :controller => 'admin/photos', :action => action }
-
     end
   end
 
   describe 'edit' do
-    it 'has a named route' do
-      #noinspection RubyResolve
-      edit_admin_photo_path(666).should == '/admin/photos/666/edit'
-    end
-
+    it { should have_named_route :edit_admin_photo, 666, '/admin/photos/666/edit' }
     it { should route(:get, '/admin/photos/666/edit').to :controller => 'admin/photos', :action => 'edit', :id => '666' }
-
   end
 
   %w{ change_game_status add_selected_answer add_entered_answer remove_revelation remove_guess reload_comments }.each do |action|
     describe action do
-      it 'has a named route' do
-        send("#{action}_path", 666).should == "/admin/photos/666/#{action}"
-      end
-
+      it { should have_named_route action, 666, "/admin/photos/666/#{action}" }
       it { should route(:post, "/admin/photos/666/#{action}").to :controller => 'admin/photos', :action => action, :id => '666' }
-
     end
   end
 
   describe 'destroy' do
-    it 'has a named route' do
-      #noinspection RubyResolve
-      admin_photo_path(666).should == '/admin/photos/666'
-    end
-
-    it { should route(:delete, "/admin/photos/666").to :controller => 'admin/photos', :action => 'destroy', :id => '666' }
-
+    it { should have_named_route :admin_photo, 666, '/admin/photos/666' }
+    it { should route(:delete, '/admin/photos/666').to :controller => 'admin/photos', :action => 'destroy', :id => '666' }
   end
 
   describe 'edit_in_gww' do
-    it 'has a named route' do
-      #noinspection RubyResolve
-      edit_in_gww_path.should == '/admin/photos/edit_in_gww'
-    end
-
-    it { should route(:get, "/admin/photos/edit_in_gww").to :controller => 'admin/photos', :action => 'edit_in_gww' }
-
+    it { should have_named_route :edit_in_gww, '/admin/photos/edit_in_gww' }
+    it { should route(:get, '/admin/photos/edit_in_gww').to :controller => 'admin/photos', :action => 'edit_in_gww' }
   end
 
 end
