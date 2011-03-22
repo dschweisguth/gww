a6e5b9f7b03aa6e24796ba3fdd1546a8f808740b
Dave Schweisguth <dave@schweisguth.org>
2011-03-22 14:02:27 -0700

    Load map script asynchronously

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,7 +1,14 @@
-Event.observe(window, 'load', function() {
-  var map = new google.maps.Map($('map_canvas'), {
+function initializeMap() {
+  new google.maps.Map($('map_canvas'), {
     zoom: 13,
     center: new google.maps.LatLng(37.755, -122.442112),
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
+}
+
+Event.observe(window, 'load', function() {
+  var script = document.createElement('script');
+  script.type = "text/javascript";
+  script.src = 'http://maps.google.com/maps/api/js?sensor=false&callback=initializeMap';
+  document.body.appendChild(script);
 });
