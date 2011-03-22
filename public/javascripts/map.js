db7aa6ca29f3b0886bb02c9796b4fcadf8e6830a
Dave Schweisguth <dave@schweisguth.org>
2011-03-22 16:13:34 -0700

    Display guesses on map

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,12 +1,29 @@
-var GWW = {};
+var GWW = {
+  initializeMap: function () {
+    GWW.map = new google.maps.Map($('map_canvas'), {
+      zoom: 13,
+      center: new google.maps.LatLng(37.755, -122.442112),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+    new Ajax.Request(window.location + '_markers', {
+      method: 'get',
+      requestHeaders: { Accept: 'application/json' },
+      onSuccess: GWW.addMarkers
+    });
+  },
 
-GWW.initializeMap = function () {
-  new google.maps.Map($('map_canvas'), {
-    zoom: 13,
-    center: new google.maps.LatLng(37.755, -122.442112),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
-}
+  addMarkers: function (transport) {
+    var guesses = transport.responseText.evalJSON(true);
+    for (var i = 0; i < guesses.length; i++) {
+      var photo = guesses[i].guess.photo;
+      new google.maps.Marker({
+        map: GWW.map,
+        position: new google.maps.LatLng(photo.latitude, photo.longitude)
+      });
+    }
+  }
+
+};
 
 Event.observe(window, 'load', function() {
   var script = document.createElement('script');
