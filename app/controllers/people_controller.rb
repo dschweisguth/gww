570cd2c051d0303f23c0be7d6747c92807f1b2b4
Dave Schweisguth <dave@schweisguth.org>
2011-03-22 18:09:49 -0700

    Changed guess pin color to green

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -148,8 +148,11 @@ class PeopleController < ApplicationController
     time_of_first_guess = guesses.first.guessed_at.to_f
     time_of_last_guess = guesses.last.guessed_at.to_f
     guesses.each do |guess|
-      non_red_intensity = (223 * (1 - (guess.guessed_at.to_f - time_of_first_guess) / (time_of_last_guess - time_of_first_guess))).to_i
-      guess.photo[:pin_color] = "FF%02X%02X" % [ non_red_intensity, non_red_intensity ]
+      position_in_range = (guess.guessed_at.to_f - time_of_first_guess) / (time_of_last_guess - time_of_first_guess)
+      # DDFFDD .. 008800
+      intensity = (255.0 * (1 - 0.5 * position_in_range)).to_i
+      others_intensity = (223.0 * (1 - position_in_range)).to_i
+      guess.photo[:pin_color] = "%02X%02X%02X" % [ others_intensity, intensity, others_intensity ]
     end
     photos = guesses.map &:photo
     render :json => photos
