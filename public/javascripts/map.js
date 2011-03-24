6708a421d3510369276459a7bf89448a1074dc61
Dave Schweisguth <dave@schweisguth.org>
2011-03-23 17:29:04 -0700

    Added SF border

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -23,6 +23,23 @@ GWW.userMap = (function () {
         mapTypeId: google.maps.MapTypeId.ROADMAP
       });
 
+      new google.maps.Polyline({
+        path: [
+          new google.maps.LatLng(37.831853, -122.472668),
+          new google.maps.LatLng(37.852543, -122.418787),
+          new google.maps.LatLng(37.92944,  -122.432027),
+          new google.maps.LatLng(37.885151, -122.376194),
+          new google.maps.LatLng(37.811411, -122.345982),
+          new google.maps.LatLng(37.708333, -122.281437),
+          new google.maps.LatLng(37.708333, -122.557983),
+          new google.maps.LatLng(37.815209, -122.584505),
+          new google.maps.LatLng(37.815209, -122.529659)
+        ],
+        strokeColor: '#FF6600',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+      }).setMap(map);
+
       var photos = GWW.config;
       infoWindow = new google.maps.InfoWindow();
       for (var i = 0; i < photos.length; i++) {
