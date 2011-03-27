ee1b331db507d051db2487b0a0e1bfcbd280f1ac
Dave Schweisguth <dave@schweisguth.org>
2011-03-26 22:28:13 -0700

    Made GWW.map a module

--- public/javascripts/app/shared/map.js
+++ public/javascripts/app/shared/map.js
@@ -1,56 +1,66 @@
 GWW = {};
-GWW.map = {
-  map: null,
-  infoWindow: null,
+GWW.map = (function () {
+  var map = null;
+  var infoWindow = null;
+  var publicMethods = {
 
-  registerOnLoad: function (callbackName) {
-    Event.observe(window, 'load', function() {
-      var script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.src = 'http://maps.google.com/maps/api/js?v=3.4&sensor=false&callback=' + callbackName;
-      document.body.appendChild(script);
-    });
-  },
+    registerOnLoad: function (callbackName) {
+      Event.observe(window, 'load', function() {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'http://maps.google.com/maps/api/js?v=3.4&sensor=false&callback=' + callbackName;
+        document.body.appendChild(script);
+      });
+    },
 
-  mapsAPIIsLoadedCallback: function () {
-    this.map = new google.maps.Map($('map_canvas'), {
-      zoom: 13,
-      center: new google.maps.LatLng(37.76, -122.442112),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
+    mapsAPIIsLoadedCallback: function () {
+      this.map = new google.maps.Map($('map_canvas'), {
+        zoom: 13,
+        center: new google.maps.LatLng(37.76, -122.442112),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      });
 
-    new google.maps.Polyline({
-      path: [
-        new google.maps.LatLng(37.831853, -122.472668),
-        new google.maps.LatLng(37.852543, -122.418787),
-        new google.maps.LatLng(37.92944,  -122.432027),
-        new google.maps.LatLng(37.885151, -122.376194),
-        new google.maps.LatLng(37.811411, -122.345982),
-        new google.maps.LatLng(37.708333, -122.281437),
-        new google.maps.LatLng(37.708333, -122.557983),
-        new google.maps.LatLng(37.815209, -122.584505),
-        new google.maps.LatLng(37.815209, -122.529659)
-      ],
-      strokeColor: '#FF6600',
-      strokeOpacity: 1.0,
-      strokeWeight: 2
-    }).setMap(this.map);
+      new google.maps.Polyline({
+        path: [
+          new google.maps.LatLng(37.831853, -122.472668),
+          new google.maps.LatLng(37.852543, -122.418787),
+          new google.maps.LatLng(37.92944,  -122.432027),
+          new google.maps.LatLng(37.885151, -122.376194),
+          new google.maps.LatLng(37.811411, -122.345982),
+          new google.maps.LatLng(37.708333, -122.281437),
+          new google.maps.LatLng(37.708333, -122.557983),
+          new google.maps.LatLng(37.815209, -122.584505),
+          new google.maps.LatLng(37.815209, -122.529659)
+        ],
+        strokeColor: '#FF6600',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+      }).setMap(this.map);
 
-    this.infoWindow = new google.maps.InfoWindow();
+      this.infoWindow = new google.maps.InfoWindow();
 
-  },
+    },
 
-  createMarker: function (photo, symbol, infoWindowContentPath) {
-    var marker = new google.maps.Marker({
-      map: this.map,
-      position: new google.maps.LatLng(photo.latitude, photo.longitude),
-      icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=' + symbol + '|' + photo.pin_color + '|000000'
-    });
-    google.maps.event.addListener(marker, 'click', this.loadInfoWindow(photo, marker, infoWindowContentPath));
-    return marker;
-  },
+    createMarker: function (photo, symbol, infoWindowContentPath) {
+      var marker = new google.maps.Marker({
+        map: this.map,
+        position: new google.maps.LatLng(photo.latitude, photo.longitude),
+        icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=' + symbol + '|' + photo.pin_color + '|000000'
+      });
+      google.maps.event.addListener(marker, 'click', loadInfoWindow(photo, marker, infoWindowContentPath));
+      return marker;
+    },
 
-  loadInfoWindow: function (photo, marker, infoWindowContentPath) {
+    openInfoWindow: function (marker) {
+      return function (transport) {
+        GWW.map.infoWindow.setContent(transport.responseText);
+        GWW.map.infoWindow.open(GWW.map.map, marker);
+      }
+    }
+
+  };
+
+  var loadInfoWindow = function (photo, marker, infoWindowContentPath) {
     return function () {
       new Ajax.Request(infoWindowContentPath(photo), {
         method: 'get',
@@ -58,13 +68,7 @@ GWW.map = {
         onSuccess: GWW.map.openInfoWindow(marker)
       });
     };
-  },
+  };
 
-  openInfoWindow: function (marker) {
-    return function (transport) {
-      GWW.map.infoWindow.setContent(transport.responseText);
-      GWW.map.infoWindow.open(GWW.map.map, marker);
-    }
-  }
-
-};
+  return publicMethods;
+})();
