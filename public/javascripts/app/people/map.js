3d6f747c0982baeabe51523387fbee6b91ff9a41
Dave Schweisguth <dave@schweisguth.org>
2011-03-26 21:11:00 -0700

    Extracted some duplication into GWW.map

--- public/javascripts/app/people/map.js
+++ public/javascripts/app/people/map.js
@@ -1,5 +1,4 @@
 GWW.personMap = (function () {
-  var map = null;
   var guesses = [];
   var posts = [];
   var infoWindow = null;
@@ -16,35 +15,14 @@ GWW.personMap = (function () {
     },
 
     mapsAPIIsLoadedCallback: function () {
-      map = new google.maps.Map($('map_canvas'), {
-        zoom: 13,
-        center: new google.maps.LatLng(37.76, -122.442112),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      });
-
-      new google.maps.Polyline({
-        path: [
-          new google.maps.LatLng(37.831853, -122.472668),
-          new google.maps.LatLng(37.852543, -122.418787),
-          new google.maps.LatLng(37.92944,  -122.432027),
-          new google.maps.LatLng(37.885151, -122.376194),
-          new google.maps.LatLng(37.811411, -122.345982),
-          new google.maps.LatLng(37.708333, -122.281437),
-          new google.maps.LatLng(37.708333, -122.557983),
-          new google.maps.LatLng(37.815209, -122.584505),
-          new google.maps.LatLng(37.815209, -122.529659)
-        ],
-        strokeColor: '#FF6600',
-        strokeOpacity: 1.0,
-        strokeWeight: 2
-      }).setMap(map);
+      GWW.map.mapsAPIIsLoadedCallback();
 
       var photos = GWW.config;
       infoWindow = new google.maps.InfoWindow();
       for (var i = 0; i < photos.length; i++) {
         var photo = photos[i].photo;
         var marker = new google.maps.Marker({
-          map: map,
+          map: GWW.map.map,
           position: new google.maps.LatLng(photo.latitude, photo.longitude),
           icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%' +
             (photo.pin_type === 'guess' ? '21' : '3F') + '|' + photo.pin_color + '|000000'
@@ -76,7 +54,7 @@ GWW.personMap = (function () {
   var openInfoWindow = function(marker) {
     return function (transport) {
       infoWindow.setContent(transport.responseText);
-      infoWindow.open(map, marker);
+      infoWindow.open(GWW.map.map, marker);
     }
   };
 
@@ -89,7 +67,7 @@ GWW.personMap = (function () {
 
   var toggle = function (markers) {
     return function (event) {
-      var markerParent = this.checked ? map : null;
+      var markerParent = this.checked ? GWW.map.map : null;
       for (var i = 0; i < markers.length; i++) {
         markers[i].setMap(markerParent);
       }
