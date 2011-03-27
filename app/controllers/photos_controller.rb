447dc2e4bfd9549bff80487ba202049cb30376a0
Dave Schweisguth <dave@schweisguth.org>
2011-03-27 12:56:20 -0700

    Used module in other map action

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,5 @@
 class PhotosController < ApplicationController
+  include Color
 
   caches_page :index
   def index
@@ -15,44 +16,14 @@ class PhotosController < ApplicationController
         post[:color] = 'FFFF00'
         post[:symbol] = '?'
       elsif post.game_status == 'found'
-        post[:color] = PhotosController.scaled_green posts.first.dateadded, posts.last.dateadded, post.dateadded
+        post[:color] = scaled_green posts.first.dateadded, posts.last.dateadded, post.dateadded
         post[:symbol] = '!'
       else # revealed
-        post[:color] = PhotosController.scaled_red posts.first.dateadded, posts.last.dateadded, post.dateadded
+        post[:color] = scaled_red posts.first.dateadded, posts.last.dateadded, post.dateadded
         post[:symbol] = '-'
       end
     end
     @json = posts.to_json;
-  end
-
-  def self.scaled_red(start_of_range, end_of_range, position)
-    start_of_range = start_of_range.to_f
-    end_of_range = end_of_range.to_f
-    fractional_position = start_of_range == end_of_range \
-      ? 1 : (position.to_f - start_of_range) / (end_of_range - start_of_range)
-    intensity = (256.0 * (1 - 0.125 * fractional_position)).to_i
-    intensity -= intensity % 4
-    if intensity == 256
-      intensity = 252
-    end
-    others_intensity = (192.0 * (1 - fractional_position)).to_i
-    others_intensity -= others_intensity % 4
-    "%02X%02X%02X" % [ intensity, others_intensity, others_intensity ]
-  end
-
-  def self.scaled_green(start_of_range, end_of_range, position)
-    start_of_range = start_of_range.to_f
-    end_of_range = end_of_range.to_f
-    fractional_position = start_of_range == end_of_range \
-      ? 1 : (position.to_f - start_of_range) / (end_of_range - start_of_range)
-    intensity = (256.0 * (1 - 0.5 * fractional_position)).to_i
-    intensity -= intensity % 4
-    if intensity == 256
-      intensity = 252
-    end
-    others_intensity = (224.0 * (1 - fractional_position)).to_i
-    others_intensity -= others_intensity % 4
-    "%02X%02X%02X" % [ others_intensity, intensity, others_intensity ]
   end
 
   caches_page :map_post
