aea8b4507a59241e07ee28414cce83f03d147e9b
Dave Schweisguth <dave@schweisguth.org>
2011-03-26 19:31:41 -0700

    Remove obsolete loop

--- spec/routing/people_routing_spec.rb
+++ spec/routing/people_routing_spec.rb
@@ -42,11 +42,9 @@ describe PeopleController do
     it { should route(:get, '/people/666/comments/page/1').to :controller => 'people', :action => 'comments', :id => '666', :page => '1' }
   end
 
-  %w{ guess }.each do |pin_type|
-    describe pin_type do
-      it { should have_named_route "person_map_#{pin_type}", 666, 777, "/people/666/map/777/#{pin_type}" }
-      it { should route(:get, "/people/666/map/777/#{pin_type}").to :controller => 'people', :action => "map_#{pin_type}", :id => '666', :photo_id => '777' }
-    end
+  describe 'map_guess' do
+    it { should have_named_route 'person_map_guess', 666, 777, '/people/666/map/777/guess' }
+    it { should route(:get, '/people/666/map/777/guess').to :controller => 'people', :action => 'map_guess', :id => '666', :photo_id => '777' }
   end
 
 end
