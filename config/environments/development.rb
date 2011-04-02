0e383d246f1c62b9c6f7ec2d69682ac65388f1da
Dave Schweisguth <dave@schweisguth.org>
2011-04-02 11:52:05 -0700

    Resolved TODO. Built path to cache dir in new way.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ GWW::Application.configure do
   config.consider_all_requests_local       = true
   config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = true
-  config.action_controller.page_cache_directory = File.join(Rails.root.to_s, "public/cache")
+  config.action_controller.page_cache_directory = Rails.root.join('public/cache').to_s
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
