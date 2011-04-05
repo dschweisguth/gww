2986afcd9ca923c9030b836ba1e55c585b555653
Dave Schweisguth <dave@schweisguth.org>
2011-04-05 14:22:41 -0700

    Made routes more concise

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
-# TODO Dave refactor
 GWW::Application.routes.draw do
-  match 'autocomplete_person_username' => 'root#autocomplete_person_username', :as => :root_autocomplete_person_username, :via => :get
-  match '/' => 'root#index', :as => :root, :via => :get
-  match 'about' => 'root#about', :as => :root_about, :via => :get
-  match 'bookmarklet' => 'root#bookmarklet', :as => :root_bookmarklet, :via => :get
+  get 'autocomplete_person_username' => 'root#autocomplete_person_username', :as => :root_autocomplete_person_username
+  get '/' => 'root#index', :as => :root
+  get 'about' => 'root#about', :as => :root_about
+  get 'bookmarklet' => 'root#bookmarklet', :as => :root_bookmarklet
   
   resources :score_reports, :only => [ :index, :show ]
 
-  match 'people/find' => 'people#find', :as => :find_person, :via => :get
-  match 'people/sorted-by/:sorted_by/order/:order' => 'people#index', :as => :people, :via => :get
-  match 'people/:id/guesses' => 'people#guesses', :as => :person_guesses, :via => :get
-  match 'people/:id/posts' => 'people#posts', :as => :person_posts, :via => :get
-  match 'people/:id/map' => 'people#map', :as => :person_map, :via => :get
-  match 'people/:id/comments/page/:page' => 'people#comments', :as => :person_comments, :via => :get
+  get 'people/find' => 'people#find', :as => :find_person
+  get 'people/sorted-by/:sorted_by/order/:order' => 'people#index', :as => :people
+  get 'people/:id/guesses' => 'people#guesses', :as => :person_guesses
+  get 'people/:id/posts' => 'people#posts', :as => :person_posts
+  get 'people/:id/map' => 'people#map', :as => :person_map
+  get 'people/:id/comments/page/:page' => 'people#comments', :as => :person_comments
   resources :people, :only => [ :show ] do
     collection do
       get :nemeses
@@ -20,7 +19,7 @@ GWW::Application.routes.draw do
     end
   end
 
-  match 'photos/sorted-by/:sorted_by/order/:order/page/:page' => 'photos#index', :as => :photos, :via => :get
+  get 'photos/sorted-by/:sorted_by/order/:order/page/:page' => 'photos#index', :as => :photos
   resources :photos, :only => [ :show ] do
     member do
       get :map_popup
@@ -44,29 +43,29 @@ GWW::Application.routes.draw do
     end
   end
 
-  match 'wheresies/:year' => 'wheresies#show', :as => :wheresies, :via => :get
+  get 'wheresies/:year' => 'wheresies#show', :as => :wheresies
 
-  match 'bookmarklet/show' => 'bookmarklet#show', :as => :bookmarklet, :via => :get
+  get 'bookmarklet/show' => 'bookmarklet#show', :as => :bookmarklet
 
-  match 'admin' => 'admin/root#index', :as => :admin_root, :via => :get
-  match 'admin/bookmarklet' => 'admin/root#bookmarklet', :as => :admin_root_bookmarklet, :via => :get
+  get 'admin' => 'admin/root#index', :as => :admin_root
+  get 'admin/bookmarklet' => 'admin/root#bookmarklet', :as => :admin_root_bookmarklet
 
-  match 'admin/photos/edit_in_gww' => 'admin/photos#edit_in_gww', :as => :edit_in_gww, :via => :get
-  match 'admin/photos/autocomplete_person_username' => 'admin/photos#autocomplete_person_username', :as => :admin_photos_autocomplete_person_username, :via => :get
-  match 'admin/photos/update_all_from_flickr' => 'admin/photos#update_all_from_flickr', :as => :update_all_from_flickr, :via => :post
-  match 'admin/photos/update_statistics' => 'admin/photos#update_statistics', :as => :update_statistics, :via => :post
-  match 'admin/photos/:id/change_game_status' => 'admin/photos#change_game_status', :as => :change_game_status, :via => :post
-  match 'admin/photos/:id/add_selected_answer' => 'admin/photos#add_selected_answer', :as => :add_selected_answer, :via => :post
-  match 'admin/photos/:id/add_entered_answer' => 'admin/photos#add_entered_answer', :as => :add_entered_answer, :via => :post
-  match 'admin/photos/:id/remove_revelation' => 'admin/photos#remove_revelation', :as => :remove_revelation, :via => :post
-  match 'admin/photos/:id/remove_guess' => 'admin/photos#remove_guess', :as => :remove_guess, :via => :post
-  match 'admin/photos/:id/reload_comments' => 'admin/photos#reload_comments', :as => :reload_comments, :via => :post
+  get 'admin/photos/edit_in_gww' => 'admin/photos#edit_in_gww', :as => :edit_in_gww
+  get 'admin/photos/autocomplete_person_username' => 'admin/photos#autocomplete_person_username', :as => :admin_photos_autocomplete_person_username
+  post 'admin/photos/update_all_from_flickr' => 'admin/photos#update_all_from_flickr', :as => :update_all_from_flickr
+  post 'admin/photos/update_statistics' => 'admin/photos#update_statistics', :as => :update_statistics
+  post 'admin/photos/:id/change_game_status' => 'admin/photos#change_game_status', :as => :change_game_status
+  post 'admin/photos/:id/add_selected_answer' => 'admin/photos#add_selected_answer', :as => :add_selected_answer
+  post 'admin/photos/:id/add_entered_answer' => 'admin/photos#add_entered_answer', :as => :add_entered_answer
+  post 'admin/photos/:id/remove_revelation' => 'admin/photos#remove_revelation', :as => :remove_revelation
+  post 'admin/photos/:id/remove_guess' => 'admin/photos#remove_guess', :as => :remove_guess
+  post 'admin/photos/:id/reload_comments' => 'admin/photos#reload_comments', :as => :reload_comments
   namespace :admin do
     resources :photos, :only => [ :edit, :destroy ] do
       collection do
-	get :inaccessible
-	get :multipoint
-	get :unfound
+        get :inaccessible
+        get :multipoint
+        get :unfound
       end
     end
     resources :score_reports, :only => [ :index, :new, :create, :destroy ]
