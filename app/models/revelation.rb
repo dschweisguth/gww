5024940adcfa18a4a3e324e00765965b8174b4de
Dave Schweisguth <dave@schweisguth.org>
2011-04-06 09:36:56 -0700

    Used new query syntax

--- app/models/revelation.rb
+++ app/models/revelation.rb
@@ -4,18 +4,13 @@ class Revelation < ActiveRecord::Base
   belongs_to :photo, :inverse_of => :revelation
   validates_presence_of :comment_text, :commented_at, :added_at
 
-  #noinspection RailsParamDefResolve
   def self.longest
-    all :include => { :photo => :person },
-      :order => 'unix_timestamp(revelations.commented_at) - ' +
-        'unix_timestamp(photos.dateadded) desc',
-      :limit => 10
+    includes(:photo => :person) \
+      .order('unix_timestamp(revelations.commented_at) - unix_timestamp(photos.dateadded) desc').limit(10)
   end
 
-  #noinspection RailsParamDefResolve
   def self.all_between(from, to)
-    all :conditions => [ '? < added_at and added_at <= ?', from.getutc, to.getutc ],
-      :include => { :photo => :person }
+    includes(:photo => :person).where('? < added_at and added_at <= ?', from.getutc, to.getutc)
   end
 
   def time_elapsed
