1654dbef03236553124cfe60bdb58eb72ff8c1fc
Dave Schweisguth <dave@schweisguth.org>
2011-04-09 20:13:05 -0700

    Test unwanted names

--- spec/lib/location_parser_spec.rb
+++ spec/lib/location_parser_spec.rb
@@ -2,20 +2,24 @@ require 'spec_helper'
 
 describe LocationParser do
 
-  before do
-    @parser = LocationParser.new [ '26TH', 'VALENCIA', 'SAN JOSE' ]
-  end
-
   it "extracts the two names of streets at an intersection" do
-    @parser.parse('26th and Valencia').should == Location.make_valid('26th', 'Valencia')
+    parser = LocationParser.new [ '26TH', 'VALENCIA', 'SAN JOSE' ]
+    parser.parse('26th and Valencia').should == Location.make_valid('26th', 'Valencia')
   end
 
   it "extracts a street with more than one word in its name" do
-    @parser.parse('26th and San Jose').should == Location.make_valid('26th', 'San Jose')
+    parser = LocationParser.new [ '26TH', 'VALENCIA', 'SAN JOSE' ]
+    parser.parse('26th and San Jose').should == Location.make_valid('26th', 'San Jose')
   end
 
   it "returns an invalid Location, given the empty string" do
-    @parser.parse('').should == Location.make_invalid
+    parser = LocationParser.new [ '26TH', 'VALENCIA', 'SAN JOSE' ]
+    parser.parse('').should == Location.make_invalid
   end
 
+  it "ignores unwanted multi-word names" do
+    parser = LocationParser.new [ 'UNNAMED 1' ]
+    parser.parse('Unnamed 1 and Valencia').should == Location.make_valid('1', 'Valencia')
+  end
+  
 end
