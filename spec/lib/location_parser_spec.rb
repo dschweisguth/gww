e3e97e5f3740eae3533e828773508e0928196dd6
Dave Schweisguth <dave@schweisguth.org>
2011-04-10 11:03:15 -0700

    Test that unknown multi-word names aren't found. Admit that only multi-word names need to be passed in. Miscellaneous cleanup.

--- spec/lib/location_parser_spec.rb
+++ spec/lib/location_parser_spec.rb
@@ -2,22 +2,27 @@ require 'spec_helper'
 
 describe LocationParser do
 
-  it "extracts the two names of streets at an intersection" do
-    parser = LocationParser.new [ '26TH', 'VALENCIA' ]
-    parser.parse('26th and Valencia').should == [ Location.new '26th', 'Valencia' ]
-  end
-
-  it "extracts a street with more than one word in its name" do
-    parser = LocationParser.new [ '26TH', 'SAN JOSE' ]
-    parser.parse('26th and San Jose').should == [ Location.new '26th', 'San Jose' ]
-  end
-
-  it "returns an invalid Location, given the empty string" do
-    parser = LocationParser.new [ '26TH', 'VALENCIA' ]
+  it "finds no Locations in the empty string" do
+    parser = LocationParser.new []
     parser.parse('').should == []
   end
 
-  it "ignores unwanted multi-word names" do
+  it "finds a Location" do
+    parser = LocationParser.new []
+    parser.parse('26th and Valencia').should == [ Location.new '26th', 'Valencia' ]
+  end
+
+  it "finds a Location with a street with more than one word in its name" do
+    parser = LocationParser.new [ 'SAN JOSE' ]
+    parser.parse('26th and San Jose').should == [ Location.new '26th', 'San Jose' ]
+  end
+
+  it "treats a multi-word name that it doesn't know about as a series of single words" do
+    parser = LocationParser.new []
+    parser.parse('26th and San Jose').should == [ Location.new '26th', 'San' ]
+  end
+
+  it "treats an unwanted multi-word name as a series of single words" do
     parser = LocationParser.new [ 'UNNAMED 1' ]
     parser.parse('Unnamed 1 and Valencia').should == [ Location.new '1', 'Valencia' ]
   end
