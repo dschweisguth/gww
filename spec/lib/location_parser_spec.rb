f5a6f89d0a1c2d8c3734633a77c7646847bf1576
Dave Schweisguth <dave@schweisguth.org>
2011-04-10 10:15:23 -0700

    Trimmed test data

--- spec/lib/location_parser_spec.rb
+++ spec/lib/location_parser_spec.rb
@@ -3,17 +3,17 @@ require 'spec_helper'
 describe LocationParser do
 
   it "extracts the two names of streets at an intersection" do
-    parser = LocationParser.new [ '26TH', 'VALENCIA', 'SAN JOSE' ]
+    parser = LocationParser.new [ '26TH', 'VALENCIA' ]
     parser.parse('26th and Valencia').should == [ Location.make_valid('26th', 'Valencia') ]
   end
 
   it "extracts a street with more than one word in its name" do
-    parser = LocationParser.new [ '26TH', 'VALENCIA', 'SAN JOSE' ]
+    parser = LocationParser.new [ '26TH', 'SAN JOSE' ]
     parser.parse('26th and San Jose').should == [ Location.make_valid('26th', 'San Jose') ]
   end
 
   it "returns an invalid Location, given the empty string" do
-    parser = LocationParser.new [ '26TH', 'VALENCIA', 'SAN JOSE' ]
+    parser = LocationParser.new [ '26TH', 'VALENCIA' ]
     parser.parse('').should == []
   end
 
