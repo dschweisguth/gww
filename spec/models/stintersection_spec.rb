2996165b6761de3c25d6b546912e38185ef3d973
Dave Schweisguth <dave@schweisguth.org>
2011-04-11 11:30:38 -0700

    Make intersections in database all caps and give them st_types

--- spec/models/stintersection_spec.rb
+++ spec/models/stintersection_spec.rb
@@ -5,19 +5,19 @@ describe Stintersection do
   describe '.geocode' do
     it "converts an intersection to a lat + long" do
       point = RGeo::Cartesian.preferred_factory.point(37, -122)
-      Stintersection.create! :cnn => 1, :st_name => '26th', :SHAPE => point
-      Stintersection.create! :cnn => 1, :st_name => 'Valencia', :SHAPE => point
+      Stintersection.create! :cnn => 1, :st_name => '26TH', :st_type => 'ST', :SHAPE => point
+      Stintersection.create! :cnn => 1, :st_name => 'VALENCIA', :st_type => 'ST', :SHAPE => point
       location = Intersection.new '26th and Valencia', '26th', nil, 'Valencia', nil
       Stintersection.geocode(location).should == point
     end
 
     it "converts a block to a lat + long" do
       point1 = RGeo::Cartesian.preferred_factory.point(38, -121)
-      Stintersection.create! :cnn => 1, :st_name => '25th', :SHAPE => point1
-      Stintersection.create! :cnn => 1, :st_name => 'Valencia', :SHAPE => point1
+      Stintersection.create! :cnn => 1, :st_name => '25TH', :st_type => 'ST', :SHAPE => point1
+      Stintersection.create! :cnn => 1, :st_name => 'VALENCIA', :st_type => 'ST', :SHAPE => point1
       point2 = RGeo::Cartesian.preferred_factory.point(37, -122)
-      Stintersection.create! :cnn => 2, :st_name => '26th', :SHAPE => point2
-      Stintersection.create! :cnn => 2, :st_name => 'Valencia', :SHAPE => point2
+      Stintersection.create! :cnn => 2, :st_name => '26TH', :st_type => 'ST', :SHAPE => point2
+      Stintersection.create! :cnn => 2, :st_name => 'VALENCIA', :st_type => 'ST', :SHAPE => point2
       location = Block.new 'Valencia between 25th and 26th', 'Valencia', nil, '25th', nil, '26th', nil
       Stintersection.geocode(location).should == RGeo::Cartesian.preferred_factory.point(37.5, -121.5)
     end
