143475bd2bc490cd053db50738f3d64b0431eb4e
Dave Schweisguth <dave@schweisguth.org>
2011-04-11 11:19:07 -0700

    Move calls of name into method

--- app/models/stintersection.rb
+++ app/models/stintersection.rb
@@ -4,11 +4,11 @@ class Stintersection < ActiveRecord::Base
   # TODO Dave handle 'street' etc. and abbreviations
   def self.geocode(location)
     if location.respond_to? :on
-      point1 = geocode_intersection location.on.name, location.between1.name
+      point1 = geocode_intersection location.on, location.between1
       if ! point1
         return nil
       end
-      point2 = geocode_intersection location.on.name, location.between2.name
+      point2 = geocode_intersection location.on, location.between2
       if point2
         midpoint = RGeo::Cartesian.preferred_factory.point(
           (point1.x + point2.x) / 2, (point1.y + point2.y) / 2)
@@ -22,24 +22,24 @@ class Stintersection < ActiveRecord::Base
           (point1.x + point2.x) / 2, (point1.y + point2.y) / 2) \
         : nil
     else
-      geocode_intersection location.at1.name, location.at2.name
+      geocode_intersection location.at1, location.at2
     end
   end
 
-  def self.geocode_intersection(name1, name2)
+  def self.geocode_intersection(street1, street2)
     nodes = find_by_sql [
       %q[
         select i1.* from stintersections i1, stintersections i2
         where i1.cnn = i2.cnn and i1.st_name = ? and i2.st_name = ?
       ],
-      name1, name2
+      street1.name, street2.name
     ]
     if nodes.length == 1
       point = nodes[0].SHAPE
-      logger.info "Found intersection of #{name1} and #{name2} at #{point.x}, #{point.y}."
+      logger.info "Found intersection of #{street1} and #{street2} at #{point.x}, #{point.y}."
       point
     else
-      logger.info "Found #{nodes.length} intersections of #{name1} and #{name2}."
+      logger.info "Found #{nodes.length} intersections of #{street1} and #{street2}."
       nil
     end
   end
