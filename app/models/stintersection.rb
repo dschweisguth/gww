cb127cd5dcdc2371b7248bbd7c2bb871e3c572a6
Dave Schweisguth <dave@schweisguth.org>
2011-04-10 22:15:38 -0700

    Cleaned up

--- app/models/stintersection.rb
+++ app/models/stintersection.rb
@@ -12,11 +12,7 @@ class Stintersection < ActiveRecord::Base
           (point1.x + point2.x) / 2, (point1.y + point2.y) / 2) \
         : nil
     else
-      point = geocode_intersection location.street1, location.street2
-      if point
-      else
-      end
-      point
+      geocode_intersection location.street1, location.street2
     end
   end
 
