3f2969fec5ccacc87320c70c82c430b43df537e1
Dave Schweisguth <dave@schweisguth.org>
2011-04-11 14:13:54 -0700

    Condensed examples

--- spec/lib/location_parser_spec.rb
+++ spec/lib/location_parser_spec.rb
@@ -16,28 +16,15 @@ describe LocationParser do
     LocationParser.new([]).parse(text).should == [ Intersection.new text, '26th', nil, 'Valencia', nil ]
   end
 
-  it "finds a block" do
-    text = 'Valencia between 25th and 26th'
-    LocationParser.new([]).parse(text).should == [ Block.new text, 'Valencia', nil, '25th', nil, '26th', nil ]
+  %w{ between bet bet. betw betw. }.each do |between|
+    it "finds a block delimited with '#{between}'" do
+      text = 'Valencia bet 25th and 26th'
+      LocationParser.new([]).parse(text).should == [ Block.new text, 'Valencia', nil, '25th', nil, '26th', nil ]
+    end
   end
 
   it "tolerates periods and commas around a block's connecting words" do
     text = 'Valencia ., between ., 25th ., and ., 26th'
-    LocationParser.new([]).parse(text).should == [ Block.new text, 'Valencia', nil, '25th', nil, '26th', nil ]
-  end
-
-  it "understands 'bet'" do
-    text = 'Valencia bet 25th and 26th'
-    LocationParser.new([]).parse(text).should == [ Block.new text, 'Valencia', nil, '25th', nil, '26th', nil ]
-  end
-
-  it "understands 'bet.'" do
-    text = 'Valencia bet. 25th and 26th'
-    LocationParser.new([]).parse(text).should == [ Block.new text, 'Valencia', nil, '25th', nil, '26th', nil ]
-  end
-
-  it "understands 'betw.'" do
-    text = 'Valencia bet. 25th and 26th'
     LocationParser.new([]).parse(text).should == [ Block.new text, 'Valencia', nil, '25th', nil, '26th', nil ]
   end
 
@@ -46,7 +33,7 @@ describe LocationParser do
     LocationParser.new([ 'SAN JOSE' ]).parse(text).should == [ Intersection.new text, '26th', nil, 'San Jose', nil ]
   end
 
-  it "treats a multi-word name that it doesn't know about as a series of single words" do
+  it "treats an unknown multi-word name as a series of single words" do
     LocationParser.new([]).parse('26th and San Jose').should == [ Intersection.new '26th and San', '26th', nil, 'San', nil ]
   end
 
