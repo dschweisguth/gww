7f1c497a19ab750f30cbcecbff88b81b0f53ad59
Dave Schweisguth <dave@schweisguth.org>
2011-04-12 16:13:40 -0700

    Added optional between streets to Address

--- lib/address.rb
+++ lib/address.rb
@@ -1,5 +1,14 @@
-class Address < Struct.new :text, :number, :street
-  def initialize(text, number, street_name, street_type)
-    super text, number, Street.new(street_name, street_type)
+class Address < Struct.new :text, :number, :street, :between1, :between2
+
+  def initialize(text, number, street_name, street_type,
+      between1_name = nil, between1_type = nil, between2_name = nil, between2_type = nil)
+    if between1_name.nil? ^ between2_name.nil?
+      raise ArgumentError, "between1_name and between2_name must both be nil or non-nil, but " +
+        "between1_name == #{between1_name} and between2_name == #{between2_name}"
+    end
+    between1, between2 = between1_name.nil? ? [ nil, nil ] : \
+      [ Street.new(between1_name, between1_type), Street.new(between2_name, between2_type) ]
+    super text, number, Street.new(street_name, street_type), between1, between2
   end
+
 end
---
+++ spec/lib/address_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe Address do
+  describe '.new' do
+
+    it "makes an address with no between streets" do
+      address = Address.new 'text', 1, 'street_name', nil
+      address.text.should == 'text'
+      address.number.should == 1
+      address.street.should == Street.new('street_name')
+    end
+
+    it "makes an address with between streets" do
+      address = Address.new 'text', 1, 'street_name', nil, 'between1_name', nil, 'between2_name', nil
+      address.text.should == 'text'
+      address.number.should == 1
+      address.street.should == Street.new('street_name')
+      address.between1.should == Street.new('between1_name')
+      address.between2.should == Street.new('between2_name')
+    end
+
+    it "blows up if the first between street is nil and the second is non-nil" do
+      lambda { Address.new('text', 1, 'street_name', nil, nil, nil, 'between2_name', nil) }.should raise_error ArgumentError
+    end
+
+    it "blows up if the first between street is non-nil and the second is nil" do
+      lambda { Address.new('text', 1, 'street_name', nil, 'between1_name', nil, nil, nil) }.should raise_error ArgumentError
+    end
+
+  end
+end
