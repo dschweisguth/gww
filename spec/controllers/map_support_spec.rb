daae772e6f2db09129d2227746da38e0d81d3bc0
Dave Schweisguth <dave@schweisguth.org>
2011-04-23 11:51:18 -0700

    Test MapSupport.thin directly

--- spec/controllers/map_support_spec.rb
+++ spec/controllers/map_support_spec.rb
@@ -6,7 +6,26 @@ describe MapSupport do
     self.extend MapSupport
   end
 
-  # TODO Dave test more stuff directly
+  describe '#thin' do
+    it "thins out photos in dense areas of the map" do
+      bounds = PhotosController::INITIAL_MAP_BOUNDS
+      photo1 = Photo.make :id => 1, :latitude => bounds.min_lat, :longitude => bounds.min_long, :dateadded => 1.day.ago
+      photo2 = Photo.make :id => 2, :latitude => bounds.min_lat, :longitude => bounds.min_long, :dateadded => 2.days.ago
+      stub(self).too_many { 0 }
+      stub(self).photos_per_bin { 1 }
+      thin([ photo1, photo2 ], bounds, 20).should == [ photo1 ]
+    end
+
+    it "does nothing if the number of photos is below a threshold" do
+      bounds = PhotosController::INITIAL_MAP_BOUNDS
+      photo1 = Photo.make :id => 1, :latitude => bounds.min_lat, :longitude => bounds.min_long, :dateadded => 1.day.ago
+      photo2 = Photo.make :id => 2, :latitude => bounds.min_lat, :longitude => bounds.min_long, :dateadded => 2.days.ago
+      stub(self).too_many { 2 }
+      stub(self).photos_per_bin { 1 }
+      thin([ photo1, photo2 ], bounds, 20).should == [ photo1, photo2 ]
+    end
+
+  end
 
   describe '#scaled_red' do
     it "starts at FCC0C0 (more or less FFBFBF)" do
