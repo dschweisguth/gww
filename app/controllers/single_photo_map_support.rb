a56ac41e3460e3cfd784cf54b182a07d60977594
Dave Schweisguth <dave@schweisguth.org>
2011-04-26 15:54:52 -0700

    Resolved TODO

--- app/controllers/single_photo_map_support.rb
+++ app/controllers/single_photo_map_support.rb
@@ -4,7 +4,7 @@ module SinglePhotoMapSupport
   def set_config_to(photo)
     if photo.mapped_or_automapped?
       first_photo = Photo.oldest
-      use_inferred_geocode_if_necessary([ photo ]) # TODO Dave *
+      use_inferred_geocode_if_necessary [ photo ]
       prepare_for_display photo, first_photo.dateadded
       @json = photo.to_json :only => [ :id, :latitude, :longitude, :color, :symbol ]
     else
