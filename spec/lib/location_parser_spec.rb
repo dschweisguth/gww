1ae965cbfc3c5ad263e91ed4515fb4aac6df9cca
Dave Schweisguth <dave@schweisguth.org>
2011-05-29 13:23:30 -0700

    Work around ruby 1.9 bug

--- spec/lib/location_parser_spec.rb
+++ spec/lib/location_parser_spec.rb
@@ -8,39 +8,39 @@ describe LocationParser do
 
   it "finds an intersection" do
     text = '26th and Valencia'
-    LocationParser.new([]).parse(text).should == [ Intersection.new text, '26th', nil, 'Valencia', nil ]
+    LocationParser.new([]).parse(text).should == [ Intersection.new(text, '26th', nil, 'Valencia', nil) ]
   end
 
   it "accepts periods and commas around an intersection's connecting word(s)" do
     text = '26th ., and ., Valencia'
-    LocationParser.new([]).parse(text).should == [ Intersection.new text, '26th', nil, 'Valencia', nil ]
+    LocationParser.new([]).parse(text).should == [ Intersection.new(text, '26th', nil, 'Valencia', nil) ]
   end
 
   %w{ between bet bet. betw betw. btwn btwn. }.each do |between|
     it "finds a block delimited with '#{between}'" do
       text = "Valencia #{between} 25th and 26th"
-      LocationParser.new([]).parse(text).should == [ Block.new text, 'Valencia', nil, '25th', nil, '26th', nil ]
+      LocationParser.new([]).parse(text).should == [ Block.new(text, 'Valencia', nil, '25th', nil, '26th', nil) ]
     end
   end
 
   it "accepts periods and commas around a block's connecting words" do
     text = 'Valencia ., between ., 25th ., and ., 26th'
-    LocationParser.new([]).parse(text).should == [ Block.new text, 'Valencia', nil, '25th', nil, '26th', nil ]
+    LocationParser.new([]).parse(text).should == [ Block.new(text, 'Valencia', nil, '25th', nil, '26th', nil) ]
   end
 
   it "finds a plain address" do
     text = '555 California'
-    LocationParser.new([]).parse(text).should == [ Address.new text, '555', 'California', nil ]
+    LocationParser.new([]).parse(text).should == [ Address.new(text, '555', 'California', nil) ]
   end
 
   it "finds an address at an intersection" do
     text = '555 California near Kearny'
-    LocationParser.new([]).parse(text).should == [ Address.new text, '555', 'California', nil, 'Kearny', nil ]
+    LocationParser.new([]).parse(text).should == [ Address.new(text, '555', 'California', nil, 'Kearny', nil) ]
   end
 
   it "finds an address with adjacent streets" do
     text = '555 California between Montgomery and Kearny'
-    LocationParser.new([]).parse(text).should == [ Address.new text, '555', 'California', nil, 'Montgomery', '', 'Kearny', '']
+    LocationParser.new([]).parse(text).should == [ Address.new(text, '555', 'California', nil, 'Montgomery', '', 'Kearny', '')]
   end
 
   it "ignores a potential address number not on a word boundary" do
@@ -49,26 +49,26 @@ describe LocationParser do
 
   it "finds an address with multiple address numbers" do
     text = '393-399 Valencia'
-    LocationParser.new([]).parse(text).should == [ Address.new text, '393', 'Valencia', nil ]
+    LocationParser.new([]).parse(text).should == [ Address.new(text, '393', 'Valencia', nil) ]
   end
 
   it "finds an address with a letter" do
     text = '393A Valencia'
-    LocationParser.new([]).parse(text).should == [ Address.new text, '393', 'Valencia', nil ]
+    LocationParser.new([]).parse(text).should == [ Address.new(text, '393', 'Valencia', nil) ]
   end
 
   it "finds a location with a street with a multiword name" do
     text = '26th and San Jose'
-    LocationParser.new([ 'SAN JOSE' ]).parse(text).should == [ Intersection.new text, '26th', nil, 'San Jose', nil ]
+    LocationParser.new([ 'SAN JOSE' ]).parse(text).should == [ Intersection.new(text, '26th', nil, 'San Jose', nil) ]
   end
 
   it "treats an unknown multi-word name as a series of single words" do
-    LocationParser.new([]).parse('26th and San Jose').should == [ Intersection.new '26th and San', '26th', nil, 'San', nil ]
+    LocationParser.new([]).parse('26th and San Jose').should == [ Intersection.new('26th and San', '26th', nil, 'San', nil) ]
   end
 
   it "finds a location with a street type" do
     text = '26th St and Valencia'
-    LocationParser.new([]).parse(text).should == [ Intersection.new text, '26th', 'St', 'Valencia', nil ]
+    LocationParser.new([]).parse(text).should == [ Intersection.new(text, '26th', 'St', 'Valencia', nil) ]
   end
 
   it "finds multiple locations" do
