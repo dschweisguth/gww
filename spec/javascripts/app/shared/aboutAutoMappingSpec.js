ff7a75ae83eb3639445cca9439350f807301f8be
Dave Schweisguth <dave@schweisguth.org>
2011-06-12 15:35:02 -0700

    Specced aboutAutoMapping

---
+++ spec/javascripts/app/shared/aboutAutoMappingSpec.js
@@ -0,0 +1,11 @@
+describe('aboutAutoMapping', function () {
+  describe('setUp', function () {
+    it("sets up the link to open a window", function () {
+      loadFixtures('aboutAutoMappingSpec.html');
+      spyOn(window, 'open');
+      GWW.shared.aboutAutoMapping.setUp();
+      $('#about-auto-mapping').click();
+      expect(window.open).toHaveBeenCalled();
+    });
+  });
+});
