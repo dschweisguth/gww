5dd1592ec5fb79014464d90acfd15298ab268ad0
Dave Schweisguth <dave@schweisguth.org>
2014-05-01 16:48:17 -0700

    Updated TODOs

--- doc/old_usernames/20100526043831_create_old_usernames.rb
+++ doc/old_usernames/20100526043831_create_old_usernames.rb
@@ -8,6 +8,7 @@ class CreateOldUsernames < ActiveRecord::Migration
     execute "alter table old_usernames add constraint old_usernames_person_id_fk foreign key(person_id) references people(id)"
     execute "alter table old_usernames add constraint old_usernames_old_username_unique unique key (old_username)"
     execute "insert into old_usernames select distinct null, p.id person_id, c.username old_username, now() from people p, comments c where p.flickrid = c.flickrid and p.username != c.username order by p.id"
+    # The above doesn't address commenters whose usernames changed but who are not players
   end
 
   def self.down
