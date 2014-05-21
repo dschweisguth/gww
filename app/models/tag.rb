c5349d62e20646a79afe6a206ab4b3b1d31ef319
Dave Schweisguth <dave@schweisguth.org>
2014-05-21 12:55:13 -0700

    Tested and constrained Tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,8 @@
 class Tag < ActiveRecord::Base
   belongs_to :photo, inverse_of: :tags
 
-  # TODO Dave constrain
+  validates_presence_of :raw
+  validates_uniqueness_of :raw, scope: :photo_id
+  validates_inclusion_of :machine_tag, in: [false, true]
 
 end
---
+++ spec/models/tag_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+# TODO Dave review shoulda-matchers
+describe Tag do
+  describe '#raw' do
+    it { should validate_presence_of :raw }
+
+    it "is unique for a given photo" do
+      existing = create :tag, raw: 'text'
+      build(:tag, photo: existing.photo, raw: 'text').should_not be_valid
+    end
+
+    it "is not unique across photos" do
+      create :tag, raw: 'text'
+      build(:tag, raw: 'text').should be_valid
+    end
+
+  end
+
+  describe '#machine_tag' do
+    it "defaults to false" do
+      Tag.new(raw: 'text').machine_tag.should == false
+    end
+
+    [false, true].each do |boolean|
+      it "may be #{boolean}" do
+        Tag.new(raw: 'text', machine_tag: boolean).should be_valid
+      end
+    end
+
+    it "may not be nil" do
+      Tag.new(raw: 'text', machine_tag: nil).should_not be_valid
+    end
+
+    # There does not seem to be a way to set machine_tag to a non-nil non-boolean
+
+  end
+
+end
