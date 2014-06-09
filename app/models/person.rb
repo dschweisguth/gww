92e0e0dda7cf9abddd06f8e026c90fa748ede01a
Dave Schweisguth <dave@schweisguth.org>
2014-06-09 11:13:51 -0700

    Extracted PersonIndexSupport

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,5 @@
 class Person < ActiveRecord::Base
-  include PersonWheresiesSupport, PersonScoreReportsSupport
+  include PersonIndexSupport, PersonScoreReportsSupport, PersonWheresiesSupport
 
   MIN_GUESSES_FOR_FAVORITE = 10
   MIN_BIAS_FOR_FAVORITE = 2.5
@@ -27,121 +27,6 @@ class Person < ActiveRecord::Base
 
   def self.find_by_multiple_fields(username)
     find_by_username(username) || find_by_flickrid(username) || (username =~ /\d+/ && find_by_id(username))
-  end
-
-  CRITERIA = {
-    'username' => %i(downcased_username),
-    'score' => %i(guess_count post_count downcased_username),
-    'posts' => %i(post_count guess_count downcased_username),
-    'score-plus-posts' => %i(score_plus_posts guess_count downcased_username),
-    'guesses-per-day' => %i(guesses_per_day guess_count downcased_username),
-    'posts-per-day' => %i(posts_per_day post_count downcased_username),
-    'posts-per-guess' => %i(posts_per_guess post_count downcased_username),
-    'time-to-guess' => %i(guess_speed guess_count downcased_username),
-    'time-to-be-guessed' => %i(be_guessed_speed post_count downcased_username),
-    'comments-to-guess' => %i(comments_to_guess guess_count downcased_username),
-    'comments-per-post' => %i(comments_per_post post_count downcased_username),
-    'comments-to-be-guessed' => %i(comments_to_be_guessed post_count downcased_username),
-    'views-per-post' => %i(views_per_post post_count downcased_username),
-    'faves-per-post' => %i(faves_per_post post_count downcased_username)
-  }
-
-  def self.all_sorted(sorted_by, order)
-    # I'd have raised ArgumentError in the case below to avoid duplication,
-    # but when I do that something eats the raised error.
-    if ! CRITERIA.has_key? sorted_by
-      raise ArgumentError, "#{sorted_by} is not a valid sort order"
-    end
-    if ! ['+', '-'].include? order
-      raise ArgumentError, "#{order} is not a valid sort direction"
-    end
-
-    post_counts = Photo.group(:person_id).count
-    guess_counts = Guess.group(:person_id).count
-    guesses_per_days = Person.guesses_per_day
-    posts_per_days = Person.posts_per_day
-    guess_speeds = Person.guess_speeds
-    be_guessed_speeds = Person.be_guessed_speeds
-    views_per_post = Person.views_per_post
-    faves_per_post = Person.faves_per_post
-
-    people = all
-    people.each do |person|
-      person.downcased_username = person.username.downcase
-      person.post_count = post_counts[person.id] || 0
-      person.guess_count = guess_counts[person.id] || 0
-      person.score_plus_posts = person.post_count + person.guess_count
-      person.guesses_per_day = guesses_per_days[person.id] || 0
-      person.posts_per_day = posts_per_days[person.id] || 0
-      person.posts_per_guess = person.guess_count == 0 ? Float::MAX : person.post_count.to_f / person.guess_count
-      person.guess_speed = guess_speeds[person.id] || Float::MAX
-      person.be_guessed_speed = be_guessed_speeds[person.id] || Float::MAX
-      person.comments_to_guess ||= Float::MAX
-      person.comments_to_be_guessed ||= Float::MAX
-      person.views_per_post = views_per_post[person.id] || 0.0
-      person.faves_per_post = faves_per_post[person.id] || 0.0
-    end
-
-    people.to_a.sort! do |x, y|
-      total_comparison = 0
-      CRITERIA[sorted_by].each do |attr|
-        comparison = y.send(attr) <=> x.send(attr)
-        if comparison != 0
-          total_comparison = comparison
-          if attr == :downcased_username
-            total_comparison *= -1
-          end
-          break
-        end
-      end
-      order == '+' ? total_comparison : -total_comparison
-    end
-
-    people
-  end
-
-  def self.guesses_per_day
-    statistic_by_person [ %q{
-      select person_id id, count(*) / datediff(?, min(commented_at)) statistic
-      from guesses group by person_id
-    }, Time.now.getutc ]
-  end
-
-  def self.posts_per_day
-    statistic_by_person [ %q{
-      select person_id id, count(*) / datediff(?, min(dateadded)) statistic
-      from photos group by person_id
-    }, Time.now.getutc ]
-  end
-
-  def self.guess_speeds
-    statistic_by_person %q{
-      select g.person_id id, avg(unix_timestamp(g.commented_at) - unix_timestamp(p.dateadded)) statistic
-      from guesses g, photos p
-      where g.photo_id = p.id and unix_timestamp(g.commented_at) > unix_timestamp(p.dateadded)
-      group by g.person_id
-    }
-  end
-
-  def self.be_guessed_speeds
-    statistic_by_person %q{
-      select p.person_id id, avg(unix_timestamp(g.commented_at) - unix_timestamp(p.dateadded)) statistic
-      from guesses g, photos p
-      where g.photo_id = p.id and unix_timestamp(g.commented_at) > unix_timestamp(p.dateadded)
-      group by p.person_id
-    }
-  end
-
-  def self.views_per_post
-    statistic_by_person 'select person_id id, avg(views) statistic from photos group by person_id'
-  end
-  
-  def self.faves_per_post
-    statistic_by_person 'select person_id id, avg(faves) statistic from photos group by person_id'
-  end
-
-  private_class_method def self.statistic_by_person(sql)
-    find_by_sql(sql).each_with_object({}) { | person, statistic| statistic[person.id] = person[:statistic].to_f }
   end
 
   def self.nemeses
---
+++ spec/models/concerns/person_index_support_spec.rb
@@ -0,0 +1,395 @@
+require 'spec_helper'
+
+describe PersonIndexSupport do
+  describe '.all_sorted' do
+    before do
+      stub(Photo).group(:person_id).stub!.count { {} }
+      stub(Guess).group(:person_id).stub!.count { {} }
+      stub(Person).guesses_per_day { {} }
+      stub(Person).posts_per_day { {} }
+      stub(Person).guess_speeds { {} }
+      stub(Person).be_guessed_speeds { {} }
+      stub(Person).views_per_post { {} }
+      stub(Person).faves_per_post { {} }
+    end
+
+    it 'sorts by username' do
+      create_people_named 'z', 'a'
+      puts_person2_before_person1 'username'
+    end
+
+    it 'ignores case' do
+      create_people_named 'Z', 'a'
+      puts_person2_before_person1 'username'
+    end
+
+    it 'sorts by score' do
+      create_people_named 'a', 'z'
+      stub_score 1, 2
+      stub_post_count 2, 1
+      puts_person2_before_person1 'score'
+    end
+
+    it 'sorts by score, post count' do
+      create_people_named 'a', 'z'
+      stub_score 1, 1
+      stub_post_count 1, 2
+      puts_person2_before_person1 'score'
+    end
+
+    it 'sorts by score, post count, username' do
+      create_people_named 'z', 'a'
+      stub_score 1, 1
+      stub_post_count 1, 1
+      puts_person2_before_person1 'score'
+    end
+
+    it 'sorts by post count' do
+      create_people_named 'a', 'z'
+      stub_post_count 1, 2
+      stub_score 2, 1
+      puts_person2_before_person1 'posts'
+    end
+
+    it 'sorts by post count, score' do
+      create_people_named 'a', 'z'
+      stub_post_count 1, 1
+      stub_score 1, 2
+      puts_person2_before_person1 'posts'
+    end
+
+    it 'sorts by post count, score, username' do
+      create_people_named 'z', 'a'
+      stub_post_count 1, 1
+      stub_score 1, 1
+      puts_person2_before_person1 'posts'
+    end
+
+    it "sorts by score + post count, even when that's different from score alone" do
+      create_people_named 'a', 'z'
+      stub_score 1, 0
+      stub_post_count 1, 3
+      puts_person2_before_person1 'score-plus-posts'
+    end
+
+    it "sorts by score + post count, even when that's different from post count alone" do
+      create_people_named 'a', 'z'
+      stub_score 1, 3
+      stub_post_count 1, 0
+      puts_person2_before_person1 'score-plus-posts'
+    end
+
+    it "sorts by score + post count, score" do
+      create_people_named 'a', 'z'
+      stub_score 1, 2
+      stub_post_count 1, 0
+      puts_person2_before_person1 'score-plus-posts'
+    end
+
+    it "sorts by score + post count, score, username" do
+      create_people_named 'z', 'a'
+      stub_score 1, 1
+      stub_post_count 1, 1
+      puts_person2_before_person1 'score-plus-posts'
+    end
+
+    it 'sorts by guesses per day' do
+      create_people_named 'a', 'z'
+      stub(Person).guesses_per_day { { @person1.id => 1, @person2.id => 2 } }
+      stub_score 2, 1
+      puts_person2_before_person1 'guesses-per-day'
+    end
+
+    it 'sorts by guesses per day, score' do
+      create_people_named 'a', 'z'
+      stub(Person).guesses_per_day { { @person1.id => 1, @person2.id => 1 } }
+      stub_score 1, 2
+      puts_person2_before_person1 'guesses-per-day'
+    end
+
+    it 'sorts by guesses per day, score, username' do
+      create_people_named 'z', 'a'
+      stub(Person).guesses_per_day { { @person1.id => 1, @person2.id => 1 } }
+      stub_score 1, 1
+      puts_person2_before_person1 'guesses-per-day'
+    end
+
+    it 'sorts by posts per day' do
+      create_people_named 'a', 'z'
+      stub(Person).posts_per_day { { @person1.id => 1, @person2.id => 2 } }
+      stub_post_count 2, 1
+      puts_person2_before_person1 'posts-per-day'
+    end
+
+    it 'sorts by posts per day, post count' do
+      create_people_named 'a', 'z'
+      stub(Person).posts_per_day { { @person1.id => 1, @person2.id => 1 } }
+      stub_post_count 1, 2
+      puts_person2_before_person1 'posts-per-day'
+    end
+
+    it 'sorts by posts per day, post count, username' do
+      create_people_named 'z', 'a'
+      stub(Person).posts_per_day { { @person1.id => 1, @person2.id => 1 } }
+      stub_post_count 1, 1
+      puts_person2_before_person1 'posts-per-day'
+    end
+
+    it 'sorts by posts/guess' do
+      create_people_named 'a', 'z'
+      stub_post_count 4, 3
+      stub_score 4, 1
+      puts_person2_before_person1 'posts-per-guess'
+    end
+
+    it 'sorts by posts/guess, post count' do
+      create_people_named 'a', 'z'
+      stub_post_count 2, 4
+      stub_score 1, 2
+      puts_person2_before_person1 'posts-per-guess'
+    end
+
+    it 'sorts by posts/guess, post count, username' do
+      create_people_named 'z', 'a'
+      stub_post_count 1, 1
+      stub_score 1, 1
+      puts_person2_before_person1 'posts-per-guess'
+    end
+
+    it 'sorts by time-to-guess' do
+      create_people_named 'a', 'z'
+      stub(Person).guess_speeds { { @person1.id => 1, @person2.id => 2 } }
+      stub_score 2, 1
+      puts_person2_before_person1 'time-to-guess'
+    end
+
+    it 'sorts by time-to-guess, score' do
+      create_people_named 'a', 'z'
+      stub(Person).guess_speeds { { @person1.id => 1, @person2.id => 1 } }
+      stub_score 1, 2
+      puts_person2_before_person1 'time-to-guess'
+    end
+
+    it 'sorts by time-to-guess, score, username' do
+      create_people_named 'z', 'a'
+      stub(Person).guess_speeds { { @person1.id => 1, @person2.id => 1 } }
+      stub_score 1, 1
+      puts_person2_before_person1 'time-to-guess'
+    end
+
+    it 'sorts by time-to-be-guessed' do
+      create_people_named 'a', 'z'
+      stub(Person).be_guessed_speeds { { @person1.id => 1, @person2.id => 2 } }
+      stub_post_count 2, 1
+      puts_person2_before_person1 'time-to-be-guessed'
+    end
+
+    it 'sorts by time-to-be-guessed, post count' do
+      create_people_named 'a', 'z'
+      stub(Person).be_guessed_speeds { { @person1.id => 1, @person2.id => 1 } }
+      stub_post_count 1, 2
+      puts_person2_before_person1 'time-to-be-guessed'
+    end
+
+    it 'sorts by time-to-be-guessed, post count, username' do
+      create_people_named 'z', 'a'
+      stub(Person).be_guessed_speeds { { @person1.id => 1, @person2.id => 1 } }
+      stub_post_count 1, 1
+      puts_person2_before_person1 'time-to-be-guessed'
+    end
+
+    it 'sorts by comments-to-guess' do
+      create_people_named 'a', 'z'
+      @person1.update! comments_to_guess: 1
+      @person2.update! comments_to_guess: 2
+      stub_score 2, 1
+      puts_person2_before_person1 'comments-to-guess'
+    end
+
+    it 'sorts by comments-to-guess, score' do
+      create_people_named 'a', 'z'
+      @person1.update! comments_to_guess: 1
+      @person2.update! comments_to_guess: 1
+      stub_score 1, 2
+      puts_person2_before_person1 'comments-to-guess'
+    end
+
+    it 'sorts by comments-to-guess, score, username' do
+      create_people_named 'z', 'a'
+      @person1.update! comments_to_guess: 1
+      @person2.update! comments_to_guess: 1
+      stub_score 1, 1
+      puts_person2_before_person1 'comments-to-guess'
+    end
+
+    it 'sorts by comments-per-post' do
+      create_people_named 'a', 'z'
+      @person1.update! comments_per_post: 1
+      @person2.update! comments_per_post: 2
+      stub_post_count 2, 1
+      puts_person2_before_person1 'comments-per-post'
+    end
+
+    it 'sorts by comments-per-post, post count' do
+      create_people_named 'a', 'z'
+      @person1.update! comments_per_post: 1
+      @person2.update! comments_per_post: 1
+      stub_post_count 1, 2
+      puts_person2_before_person1 'comments-per-post'
+    end
+
+    it 'sorts by comments-per-post, post count, username' do
+      create_people_named 'z', 'a'
+      @person1.update! comments_per_post: 1
+      @person2.update! comments_per_post: 1
+      stub_post_count 1, 1
+      puts_person2_before_person1 'comments-per-post'
+    end
+
+    it 'sorts by comments-to-be-guessed' do
+      create_people_named 'a', 'z'
+      @person1.update! comments_to_be_guessed: 1
+      @person2.update! comments_to_be_guessed: 2
+      stub_post_count 2, 1
+      puts_person2_before_person1 'comments-to-be-guessed'
+    end
+
+    it 'sorts by comments-to-be-guessed, post count' do
+      create_people_named 'a', 'z'
+      @person1.update! comments_to_be_guessed: 1
+      @person2.update! comments_to_be_guessed: 1
+      stub_post_count 1, 2
+      puts_person2_before_person1 'comments-to-be-guessed'
+    end
+
+    it 'sorts by comments-to-be-guessed, post count, username' do
+      create_people_named 'z', 'a'
+      @person1.update! comments_to_be_guessed: 1
+      @person2.update! comments_to_be_guessed: 1
+      stub_post_count 1, 1
+      puts_person2_before_person1 'comments-to-be-guessed'
+    end
+
+    it 'sorts by views-per-post' do
+      create_people_named 'a', 'z'
+      stub(Person).views_per_post { { @person1.id => 1, @person2.id => 2 } }
+      stub_post_count 2, 1
+      puts_person2_before_person1 'views-per-post'
+    end
+
+    it 'sorts by views-per-post, post count' do
+      create_people_named 'a', 'z'
+      stub(Person).views_per_post { { @person1.id => 1, @person2.id => 1 } }
+      stub_post_count 1, 2
+      puts_person2_before_person1 'views-per-post'
+    end
+
+    it 'sorts by views-per-post, post count, username' do
+      create_people_named 'z', 'a'
+      stub(Person).views_per_post { { @person1.id => 1, @person2.id => 1 } }
+      stub_post_count 1, 1
+      puts_person2_before_person1 'views-per-post'
+    end
+
+    it 'sorts by faves-per-post' do
+      create_people_named 'a', 'z'
+      stub(Person).faves_per_post { { @person1.id => 1, @person2.id => 2 } }
+      stub_post_count 2, 1
+      puts_person2_before_person1 'faves-per-post'
+    end
+
+    it 'sorts by faves-per-post, post count' do
+      create_people_named 'a', 'z'
+      stub(Person).faves_per_post { { @person1.id => 1, @person2.id => 1 } }
+      stub_post_count 1, 2
+      puts_person2_before_person1 'faves-per-post'
+    end
+
+    it 'sorts by faves-per-post, post count, username' do
+      create_people_named 'z', 'a'
+      stub(Person).faves_per_post { { @person1.id => 1, @person2.id => 1 } }
+      stub_post_count 1, 1
+      puts_person2_before_person1 'faves-per-post'
+    end
+
+    it 'sorts the other direction, too' do
+      create_people_named 'a', 'z'
+      Person.all_sorted('username', '-').should == [ @person2, @person1 ]
+    end
+
+    def create_people_named(username1, username2)
+      @person1 = create :person, username: username1
+      @person2 = create :person, username: username2
+    end
+
+    def stub_post_count(count1, count2)
+      # noinspection RubyArgCount
+      stub(Photo).group(:person_id).stub!.count { { @person1.id => count1, @person2.id => count2 } }
+    end
+
+    def stub_score(count1, count2)
+      # noinspection RubyArgCount
+      stub(Guess).group(:person_id).stub!.count { { @person1.id => count1, @person2.id => count2 } }
+    end
+
+    def puts_person2_before_person1(sorted_by)
+      Person.all_sorted(sorted_by, '+').should == [ @person2, @person1 ]
+    end
+
+    it 'explodes if sorted_by is invalid' do
+      lambda { Person.all_sorted('hat-size', '+') }.should raise_error ArgumentError
+    end
+
+    it 'explodes if order is invalid' do
+      lambda { Person.all_sorted('username', '?') }.should raise_error ArgumentError
+    end
+
+  end
+
+  describe '.guesses_per_day' do
+    it 'returns a map of person ID to average guesses per day' do
+      guess = create :guess, commented_at: 4.days.ago
+      Person.guesses_per_day.should == { guess.person.id => 0.25 }
+    end
+  end
+
+  describe '.posts_per_day' do
+    it 'returns a map of person ID to average posts per day' do
+      photo = create :photo, dateadded: 4.days.ago
+      Person.posts_per_day.should == { photo.person.id => 0.25 }
+    end
+  end
+
+  describe '.guess_speeds' do
+    it 'returns a map of person ID to average seconds to guess' do
+      now = Time.now
+      photo = create :photo, dateadded: now - 5
+      guess = create :guess, photo: photo, commented_at: now - 1
+      Person.guess_speeds.should == { guess.person.id => 4 }
+    end
+  end
+
+  describe '.be_guessed_speeds' do
+    it 'returns a map of person ID to average seconds for their photos to be guessed' do
+      now = Time.now
+      photo = create :photo, dateadded: now - 5
+      create :guess, photo: photo, commented_at: now - 1
+      Person.be_guessed_speeds.should == { photo.person.id => 4 }
+    end
+  end
+
+  describe '.views_per_post' do
+    it 'returns a map of person ID to average # of views per post' do
+      photo = create :photo, views: 1
+      Person.views_per_post.should == { photo.person.id => 1 }
+    end
+  end
+
+  describe '.faves_per_post' do
+    it 'returns a map of person ID to average # of faves per post' do
+      photo = create :photo, faves: 1
+      Person.faves_per_post.should == { photo.person.id => 1 }
+    end
+  end
+
+end
