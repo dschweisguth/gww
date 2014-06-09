8198d911a98895ef22439cc7b1a83f223c53e0e3
Dave Schweisguth <dave@schweisguth.org>
2014-06-09 12:29:40 -0700

    Extracted PersonShowSupport

---
+++ app/models/concerns/person_show_support.rb
@@ -0,0 +1,88 @@
+module PersonShowSupport
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def standing(person)
+      place = 1
+      tied = false
+      scores_by_person = Guess.group(:person_id).count
+      people_by_score = scores_by_person.keys.group_by { |person_id| scores_by_person[person_id] }
+      scores = people_by_score.keys.sort { |a, b| b <=> a }
+      scores.each do |score|
+        people_with_score = people_by_score[score]
+        if people_with_score.include? person.id
+          tied = people_with_score.length > 1
+          break
+        else
+          place += people_with_score.length
+        end
+      end
+      return place, tied
+    end
+
+    def posts_standing(person)
+      place = 1
+      tied = false
+      posts_by_person = Photo.group(:person_id).count
+      people_by_post_count = posts_by_person.keys.group_by { |person_id| posts_by_person[person_id] }
+      post_counts = people_by_post_count.keys.sort { |a, b| b <=> a }
+      post_counts.each do |post_count|
+        people_with_post_count = people_by_post_count[post_count]
+        if people_with_post_count.include? person.id
+          tied = people_with_post_count.length > 1
+          break
+        else
+          place += people_with_post_count.length
+        end
+      end
+      return place, tied
+    end
+
+  end
+
+  def favorite_posters
+    favorite_posters = Person.find_by_sql [
+      %Q[
+        select posters.*,
+          count(*) / posters_posts.post_count /
+            (select count(*) from guesses where person_id = ?) *
+            (select count(*) from photos) bias
+        from guesses g, photos f, people posters,
+          (select person_id, count(*) post_count from photos
+            group by person_id having count(*) >= #{Person::MIN_GUESSES_FOR_FAVORITE}) posters_posts
+        where g.photo_id = f.id and
+          g.person_id = ? and f.person_id = posters.id and
+          f.person_id = posters_posts.person_id
+        group by posters.id
+        having count(*) >= #{Person::MIN_GUESSES_FOR_FAVORITE} and bias >= #{Person::MIN_BIAS_FOR_FAVORITE}
+        order by bias desc
+      ],
+      id, id
+    ]
+    favorite_posters.each { |fp| fp.bias = fp[:bias] }
+    favorite_posters
+  end
+
+  def favorite_posters_of
+    favorite_posters_of = Person.find_by_sql [
+      %Q[
+        select guessers.*,
+          count(*) / (select count(*) from photos where person_id = ?) /
+            guessers_guesses.guess_count * (select count(*) from photos) bias
+        from guesses g, photos f, people guessers,
+          (select person_id, count(*) guess_count from guesses
+            group by person_id) guessers_guesses
+        where g.photo_id = f.id and
+          g.person_id = guessers.id and f.person_id = ? and
+          g.person_id = guessers_guesses.person_id
+        group by guessers.id
+        having count(*) >= #{Person::MIN_GUESSES_FOR_FAVORITE} and bias >= #{Person::MIN_BIAS_FOR_FAVORITE}
+        order by bias desc
+      ],
+      id, id
+    ]
+    favorite_posters_of.each { |fp| fp.bias = fp[:bias] }
+    favorite_posters_of
+  end
+
+end
---
+++ spec/models/concerns/person_show_support_spec.rb
@@ -0,0 +1,121 @@
+describe PersonShowSupport do
+  describe '.standing' do
+    let(:person) { create :person }
+
+    it "returns the person's score position" do
+      Person.standing(person).should == [ 1, false ]
+    end
+
+    it "considers other players' scores" do
+      create :guess
+      Person.standing(person).should == [ 2, false ]
+    end
+
+    it "detects ties" do
+      create :guess, person: person
+      create :guess
+      Person.standing(person).should == [ 1, true ]
+    end
+
+  end
+
+  describe '.posts_standing' do
+    let(:person) { create :person }
+
+    it "returns the person's post position" do
+      Person.posts_standing(person).should == [ 1, false ]
+    end
+
+    it "considers other players' posts" do
+      create :photo
+      Person.posts_standing(person).should == [ 2, false ]
+    end
+
+    it "detects ties" do
+      create :photo, person: person
+      create :photo
+      Person.posts_standing(person).should == [ 1, true ]
+    end
+
+  end
+
+  describe '#favorite_posters' do
+    it "lists the posters which this person has guessed #{Person::MIN_BIAS_FOR_FAVORITE} or more times as often as this person has guessed all posts" do
+      guesser, favorite_poster = make_potential_favorite_poster(10, 15)
+      favorite_posters = guesser.favorite_posters
+      favorite_posters.should == [ favorite_poster ]
+      favorite_posters[0].bias.should == Person::MIN_BIAS_FOR_FAVORITE
+    end
+
+    it "ignores a poster which this person has guessed less than #{Person::MIN_BIAS_FOR_FAVORITE} times as often as this person has guessed all posts" do
+      #noinspection RubyUnusedLocalVariable
+      guesser, favorite_poster = make_potential_favorite_poster(10, 14)
+      guesser.favorite_posters.should == []
+    end
+
+    it "ignores a poster which this person has guessed less than #{Person::MIN_GUESSES_FOR_FAVORITE} times" do
+      #noinspection RubyUnusedLocalVariable
+      guesser, favorite_poster = make_potential_favorite_poster(9, 15)
+      guesser.favorite_posters.should == []
+    end
+
+  end
+
+  describe '#favorite_posters_of' do
+    it "lists the guessers who have guessed this person #{Person::MIN_BIAS_FOR_FAVORITE} or more times as often as those guessers have guessed all posts" do
+      devoted_guesser, poster = make_potential_favorite_poster(10, 15)
+      favorite_posters_of = poster.favorite_posters_of
+      favorite_posters_of.should == [ devoted_guesser ]
+      favorite_posters_of[0].bias.should == Person::MIN_BIAS_FOR_FAVORITE
+    end
+
+    it "ignores a guesser who has guessed this person less than #{Person::MIN_BIAS_FOR_FAVORITE} times as often as that guesser has guessed all posts" do
+      #noinspection RubyUnusedLocalVariable
+      devoted_guesser, poster = make_potential_favorite_poster(10, 14)
+      poster.favorite_posters_of.should == []
+    end
+
+    it "ignores a guesser who has guessed this person less than #{Person::MIN_GUESSES_FOR_FAVORITE} times" do
+      #noinspection RubyUnusedLocalVariable
+      devoted_guesser, poster = make_potential_favorite_poster(9, 15)
+      poster.favorite_posters_of.should == []
+    end
+
+  end
+
+  describe '#paginated_commented_photos' do
+    let(:person) { create :person }
+
+    it "returns the photos commented on by a given user" do
+      comment = create :comment, flickrid: person.flickrid, username: person.username
+      person.paginated_commented_photos(1).should == [comment.photo]
+    end
+
+    it "ignores photos commented on by another user" do
+      create :comment
+      person.paginated_commented_photos(1).should == []
+    end
+
+    it "paginates" do
+      3.times { create :comment, flickrid: person.flickrid, username: person.username }
+      person.paginated_commented_photos(1, 2).length.should == 2
+    end
+
+    it "returns each photo only once, even if the person commented on it more than once" do
+      photo = create :photo
+      create :comment, photo: photo, flickrid: person.flickrid, username: person.username
+      create :comment, photo: photo, flickrid: person.flickrid, username: person.username
+      person.paginated_commented_photos(1).should == [photo]
+    end
+
+    it "sorts the most recently updated photos first" do
+      photo1 = create :photo, lastupdate: 2.days.ago
+      create :comment, photo: photo1, flickrid: person.flickrid, username: person.username
+      photo2 = create :photo, lastupdate: 1.days.ago
+      create :comment, photo: photo2, flickrid: person.flickrid, username: person.username
+      person.paginated_commented_photos(1).should == [photo2, photo1]
+    end
+
+  end
+
+end
