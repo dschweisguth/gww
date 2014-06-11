2869261f2c67052a8e70ce6df16d6114f6d26b15
Dave Schweisguth <dave@schweisguth.org>
2014-06-10 22:50:42 -0700

    Extracted longest_or_shortest_in

--- app/models/concerns/guess_wheresies_support.rb
+++ app/models/concerns/guess_wheresies_support.rb
@@ -3,17 +3,18 @@ module GuessWheresiesSupport
 
   module ClassMethods
     def longest_in(year)
-     with_valid_age
-      .where("? < guesses.commented_at", Time.local(year).getutc)
-      .where("guesses.commented_at < ?", Time.local(year + 1).getutc)
-      .order_by_age(:desc).limit(10).includes(:person, { photo: :person })
+      longest_or_shortest_in year, :desc
     end
 
     def shortest_in(year)
+      longest_or_shortest_in year, :asc
+    end
+
+    private def longest_or_shortest_in(year, direction)
       with_valid_age
         .where("? < guesses.commented_at", Time.local(year).getutc)
         .where("guesses.commented_at < ?", Time.local(year + 1).getutc)
-        .order_by_age.limit(10).includes(:person, { photo: :person })
+        .order_by_age(direction).limit(10).includes(:person, { photo: :person })
     end
 
   end
