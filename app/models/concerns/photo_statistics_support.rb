5758621acdac502ef86829c7dce3473868144584
Dave Schweisguth <dave@schweisguth.org>
2014-06-11 14:30:33 -0700

    Extracted PhotoStatisticsSupport

---
+++ app/models/concerns/photo_statistics_support.rb
@@ -0,0 +1,95 @@
+module PhotoStatisticsSupport
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def update_statistics
+      connection.execute %q{
+        update photos f set
+          other_user_comments =
+            ifnull(
+              (select count(*)
+                from people poster, comments c
+                where
+                  f.person_id = poster.id and
+                  f.id = c.photo_id and
+                  poster.flickrid != c.flickrid
+                group by c.photo_id),
+              0),
+          member_comments =
+            ifnull(
+              (select count(*)
+                from people poster, comments c, people commenter, guesses g
+                where
+                  f.person_id = poster.id and
+                  f.id = c.photo_id and
+                  poster.flickrid != c.flickrid and
+                  c.flickrid = commenter.flickrid and
+                  f.id = g.photo_id and
+                  c.commented_at <= g.commented_at
+                group by c.photo_id),
+              0),
+          member_questions =
+            ifnull(
+              (select count(*)
+                from people poster, comments c, people commenter, guesses g
+                where
+                  f.person_id = poster.id and
+                  f.id = c.photo_id and
+                  poster.flickrid != c.flickrid and
+                  c.flickrid = commenter.flickrid and
+                  f.id = g.photo_id and
+                  c.commented_at <= g.commented_at and
+                  c.comment_text like '%?%'
+                group by c.photo_id),
+              0)
+      }
+    end
+
+    def infer_geocodes
+      logger.info 'Inferring geocodes ...'
+      start = Time.now
+      answers = Guess.includes(:photo) + Revelation.includes(:photo)
+      parser = LocationParser.new Stcline.multiword_street_names
+      answer_count = 0
+      location_count = 0
+      inferred_count = 0
+      answers.each do |answer|
+        answer_count += 1
+        logger.debug "\nInferring geocode for \"#{answer.comment_text}\" ..."
+        locations = parser.parse answer.comment_text
+        point =
+          if locations.empty?
+            logger.debug "Found no location."
+            nil
+          else
+            location_count += 1
+            shapes = locations.map { |location| Stintersection.geocode location }.reject &:nil?
+            if shapes.length != 1
+              logger.debug "Found #{shapes.length} geocodes."
+              nil
+            else
+              inferred_count += 1
+              shapes[0]
+            end
+          end
+        answer.photo.save_geocode point
+      end
+      finish = Time.now
+      logger.info "Examined #{answer_count} photos " +
+        "(#{finish - start} s, #{(finish - start) / answer_count} s/photo); " +
+        "found #{location_count} candidate locations (#{'%.1f' % (100.0 * location_count / answer_count)}% success); " +
+        "inferred #{inferred_count} geocodes (#{'%.1f' % (100.0 * inferred_count / answer_count)}% success)"
+    end
+
+  end
+
+  def save_geocode(point)
+    lat, long = point.nil? ? [ nil, nil ] : [ point.y, point.x ]
+    if inferred_latitude != lat || inferred_longitude != long
+      self.inferred_latitude = lat
+      self.inferred_longitude = long
+      save!
+    end
+  end
+
+end
