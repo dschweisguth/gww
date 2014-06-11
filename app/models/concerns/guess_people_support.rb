f6f9c9009442e0259c3a4f4d091927ef8c0f06e0
Dave Schweisguth <dave@schweisguth.org>
2014-06-10 18:39:24 -0700

    Refactored GuessPeopleSupport in preparation for moving methods

--- app/models/concerns/guess_people_support.rb
+++ app/models/concerns/guess_people_support.rb
@@ -19,35 +19,35 @@ module GuessPeopleSupport
 
     def oldest(guesser)
       first_guess_with_place guesser, 'guesses.person_id = ?', 'desc',
-        "#{Guess::GUESS_AGE} > (select max(#{G_AGE}) from guesses g, photos p " +
-  	      'where g.person_id = ? and g.photo_id = p.id )'
-    end
-
-    def longest_lasting(poster)
-      first_guess_with_place poster, 'photos.person_id = ?', 'desc',
-        "#{Guess::GUESS_AGE} > (select max(#{G_AGE}) from guesses g, photos p " +
-  	      'where g.photo_id = p.id and p.person_id = ?)'
+        "#{Guess::GUESS_AGE} > " +
+          "(select max(#{G_AGE}) from guesses g, photos p where g.person_id = ? and g.photo_id = p.id)"
     end
 
     def fastest(guesser)
       first_guess_with_place guesser, 'guesses.person_id = ?', 'asc',
-        "#{Guess::GUESS_AGE} < (select min(#{G_AGE}) from guesses g, photos p " +
-  	      "where g.person_id = ? and g.photo_id = p.id and #{G_AGE_IS_VALID})"
+        "#{Guess::GUESS_AGE} < " +
+          "(select min(#{G_AGE}) from guesses g, photos p where g.person_id = ? and g.photo_id = p.id and #{G_AGE_IS_VALID})"
+    end
+
+    def longest_lasting(poster)
+      first_guess_with_place poster, 'photos.person_id = ?', 'desc',
+        "#{Guess::GUESS_AGE} > " +
+          "(select max(#{G_AGE}) from guesses g, photos p where g.photo_id = p.id and p.person_id = ?)"
     end
 
     def shortest_lasting(poster)
       first_guess_with_place poster, 'photos.person_id = ?', 'asc',
-        "#{Guess::GUESS_AGE} < (select min(#{G_AGE}) from guesses g, photos p " +
-  	      "where g.photo_id = p.id and p.person_id = ? and #{G_AGE_IS_VALID})"
+        "#{Guess::GUESS_AGE} < " +
+          "(select min(#{G_AGE}) from guesses g, photos p where g.photo_id = p.id and p.person_id = ? and #{G_AGE_IS_VALID})"
     end
 
     private def first_guess_with_place(person, conditions, order, place_conditions)
       guess = includes(:person, { photo: :person })
-        .where("#{conditions} and #{Guess::GUESS_AGE_IS_VALID}", person).references(:photos).order("#{Guess::GUESS_AGE} #{order}").first
+        .where(conditions, person).where(Guess::GUESS_AGE_IS_VALID).references(:photos).order("#{Guess::GUESS_AGE} #{order}").first
       if ! guess
         return nil
       end
-      guess.place = joins(:photo).where("#{place_conditions} and #{Guess::GUESS_AGE_IS_VALID}", person.id).count + 1
+      guess.place = joins(:photo).where(place_conditions, person).where(Guess::GUESS_AGE_IS_VALID).count + 1
       guess
     end
 
