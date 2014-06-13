b941ca90ea0765198022ebf087a5a75d4c56a355
Dave Schweisguth <dave@schweisguth.org>
2014-06-12 18:47:54 -0700

    Eliminated local

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -11,35 +11,32 @@ module PhotosHelper
   def ago(time)
     now = Time.now.getutc
     time = time.getutc
-    ago =
-      if time > now - 1.second
-        'a moment'
-      elsif time > now - 1.minute
-        pluralize (now - time).to_i, 'second'
-      elsif time > now - 1.hour
-        minutes = now.min - time.min
-        if minutes < 0
-          minutes += 60
-        end
-        pluralize minutes, 'minute'
-      elsif time > now - 37.hours
-        hours = now.hour - time.hour
-        if hours < 0
-          hours += 24
-        end
-        pluralize hours, 'hour'
-      elsif time > now - 1.month
-        days = (now + 12.hours).yday - time.yday
-        if days < 0
-          days += 365
-        end
-        days >= 10 ? "#{(days + 4) / 7} weeks" : "#{days} days"
-      else
-        months = now.month - time.month + 12 * (now.year - time.year)
-        pluralize months, 'month'
+    if time > now - 1.second
+      'a moment'
+    elsif time > now - 1.minute
+      pluralize (now - time).to_i, 'second'
+    elsif time > now - 1.hour
+      minutes = now.min - time.min
+      if minutes < 0
+        minutes += 60
       end
-    ago << ' ago'
-    ago
+      pluralize minutes, 'minute'
+    elsif time > now - 37.hours
+      hours = now.hour - time.hour
+      if hours < 0
+        hours += 24
+      end
+      pluralize hours, 'hour'
+    elsif time > now - 1.month
+      days = (now + 12.hours).yday - time.yday
+      if days < 0
+        days += 365
+      end
+      days >= 10 ? "#{(days + 4) / 7} weeks" : "#{days} days"
+    else
+      months = now.month - time.month + 12 * (now.year - time.year)
+      pluralize months, 'month'
+    end + ' ago'
   end
 
   def highlighted(string, text_terms, other_strings_that_count=[])
