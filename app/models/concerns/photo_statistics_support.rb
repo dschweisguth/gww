98d2dedd3c4310cb3da8219cf7792e226bfc4cd8
Dave Schweisguth <dave@schweisguth.org>
2014-06-14 10:27:29 -0700

    Refactored infer_geocodes. Made the meaning of location_count slightly clearer.

--- app/models/concerns/photo_statistics_support.rb
+++ app/models/concerns/photo_statistics_support.rb
@@ -50,40 +50,40 @@ module PhotoStatisticsSupport
       start = Time.now
       answers = Guess.includes(:photo) + Revelation.includes(:photo)
       parser = LocationParser.new Stcline.multiword_street_names
-      answer_count = 0
-      location_count = 0
-      inferred_count = 0
-      answers.each do |answer|
-        answer_count += 1
+      answer_count = answers.length
+      located_count, inferred_count = answers.each_with_object([0, 0]) do |answer, counts|
         logger.debug "\nInferring geocode for \"#{answer.comment_text}\" ..."
         locations = parser.parse answer.comment_text
-        point =
-          if locations.any?
-            location_count += 1
-            shapes = locations.map { |location| Stintersection.geocode location }.compact
-            if shapes.length == 1
-              inferred_count += 1
-              shapes.first
-            else
-              logger.debug "Found #{shapes.length} geocodes."
-              nil
-            end
-          else
-            logger.debug "Found no location."
-            nil
-          end
-        answer.photo.save_geocode point
+        point, located_one, geocoded_one = unique_geocode locations
+        answer.photo.update_geocode! point
+        counts[0] += located_one
+        counts[1] += geocoded_one
       end
       finish = Time.now
       logger.info "Examined #{answer_count} photos " +
         "(#{finish - start} s, #{(finish - start) / answer_count} s/photo); " +
-        "found #{location_count} candidate locations (#{'%.1f' % (100.0 * location_count / answer_count)}% success); " +
-        "inferred #{inferred_count} geocodes (#{'%.1f' % (100.0 * inferred_count / answer_count)}% success)"
+        "located #{located_count} photos (#{'%.1f' % (100.0 * located_count / answer_count)}% success); " +
+        "geocoded #{inferred_count} photos (#{'%.1f' % (100.0 * inferred_count / answer_count)}% success)"
+    end
+
+    private def unique_geocode(locations)
+      if locations.any?
+        shapes = locations.map { |location| Stintersection.geocode location }.compact
+        if shapes.length == 1
+          [shapes.first, 1, 1]
+        else
+          logger.debug "Found #{shapes.length} geocodes."
+          [nil, 1, 0]
+        end
+      else
+        logger.debug "Found no location."
+        [nil, 0, 0]
+      end
     end
 
   end
 
-  def save_geocode(point)
+  def update_geocode!(point)
     lat = point.try :y
     long = point.try :x
     if inferred_latitude != lat || inferred_longitude != long
