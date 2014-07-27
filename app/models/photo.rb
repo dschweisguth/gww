0f8a946c8d7e8d78b0d374af8f3af9546b9ab860
Dave Schweisguth <dave@schweisguth.org>
2014-07-27 11:51:29 -0700

    Removed invalid TODO

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ActiveRecord::Base
-  include Answer, PhotoPeopleSupport, PhotoPhotosSupport, PhotoStatisticsSupport, PhotoWheresiesSupport, SinglePhotoMapSupport # TODO Dave move this include to modules that use it?
+  include Answer, PhotoPeopleSupport, PhotoPhotosSupport, PhotoStatisticsSupport, PhotoWheresiesSupport, SinglePhotoMapSupport
 
   belongs_to :person, inverse_of: :photos
   has_many :comments, inverse_of: :photo, dependent: :destroy
