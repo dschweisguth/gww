1adcbc0a95a27944e711ccf64f9063560106f6f5
Dave Schweisguth <dave@schweisguth.org>
2016-01-03 19:36:59 -0800

    Use dot notation rather than ['']

--- app/assets/javascripts/admin/photos/edit.js
+++ app/assets/javascripts/admin/photos/edit.js
@@ -17,20 +17,20 @@ GWW.admin.photos.edit = (function () {
     for (var i = 0; i < forms.length; i++) {
       var form = forms[i];
       if (/add_selected_answer/.test(form.action)) {
-        $(form['commit']).click(copyUsernameFor(form));
+        $(form.commit).click(copyUsernameFor(form));
       }
     }
   }
 
   function copyUsernameFor(form) {
     return function () {
-      form['username'].value = $('#username')[0].value;
+      form.username.value = $('#username')[0].value;
     };
   }
 
   function preventSubmitPartialUsernameForm() {
     $('#username_form').submit(function (event) {
-      if (this['answer_text'].value === '') {
+      if (this.answer_text.value === '') {
         event.stopImmediatePropagation(); // Not necessary for functionality. Permits testing.
         return false;
       } else {
