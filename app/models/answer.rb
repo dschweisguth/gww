7d7ee68724fbb6d1ec8fdc0f35abefc6b729f7d8
Dave Schweisguth <dave@schweisguth.org>
2016-01-07 06:58:50 -0800

    Remove duplication from Answer#formatted_age_by_period

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,51 +1,47 @@
 module Answer
-
   def time_elapsed_between(from, to)
-    formatted_age_by_period from, to, %w(years months days hours minutes seconds)
+    time_elapsed_between_in_words from, to, %w(years months days hours minutes seconds)
   end
 
   def ymd_elapsed_between(from, to)
-    result = formatted_age_by_period from, to, %w(years months days)
+    result = time_elapsed_between_in_words from, to, %w(years months days)
     result.empty? ? time_elapsed_between(from, to) : result
   end
 
-  #noinspection RubyUnusedLocalVariable
-  private def formatted_age_by_period(from, to, periods)
+  private
+
+  def time_elapsed_between_in_words(from, to, units)
+    time_elapsed_in_words time_elapsed_between_by_unit(from, to), units
+  end
+
+  def time_elapsed_between_by_unit(from, to)
     utc_from = from.getutc
     utc_to = to.getutc
-    years = utc_to.year - utc_from.year
-    months = utc_to.month - utc_from.month
-    days = utc_to.day - utc_from.day
-    hours = utc_to.hour - utc_from.hour
-    minutes = utc_to.min - utc_from.min
-    seconds = utc_to.sec - utc_from.sec
-    if seconds < 0
-      seconds += 60
-      minutes -= 1
-    end
-    if minutes < 0
-      minutes += 60
-      hours -= 1
-    end
-    if hours < 0
-      hours += 24
-      days -= 1
-    end
-    if days < 0
-      days += 30
-      months -= 1
-    end
-    if months < 0
-      months += 12
-      years -= 1
-    end
-    time_elapsed = periods.each_with_object([]) do |name, list|
-      value = eval name
-      if value > 0
-        list.push "#{value}&nbsp;#{value == 1 ? name.singularize : name}"
+    next_largest = nil
+    [
+      ['years', :year, nil],
+      ['months', :month, 12],
+      ['days', :day, 30],
+      ['hours', :hour, 24],
+      ['minutes', :min, 60],
+      ['seconds', :sec, 60]
+    ].
+    map do |unit, method, number_in_next_largest_unit|
+      value = utc_to.send(method) - utc_from.send(method)
+      if value < 0
+        value += number_in_next_largest_unit
+        next_largest[1] -= 1
       end
+      next_largest = [unit, value]
     end
-    time_elapsed.join ', '
+  end
+
+  def time_elapsed_in_words(time_elapsed, units)
+    time_elapsed.
+      select { |unit, _value| units.include? unit }.
+      select { |_unit, value| value > 0 }.
+      map { |unit, value| "#{value}&nbsp;#{value == 1 ? unit.singularize : unit}" }.
+      join ', '
   end
 
 end
