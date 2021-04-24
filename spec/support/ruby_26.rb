4e7fa36b6a6fe0b5992a0872fc74779310e21dd6
Dave Schweisguth <dave@schweisguth.org>
2021-04-23 20:32:42 -0700

    Add Ruby version to comment for easier finding later

--- spec/support/ruby_26.rb
+++ spec/support/ruby_26.rb
@@ -1,4 +1,5 @@
-# https://github.com/rails/rails/issues/34790
+# Adapt Rails tests to Ruby 2.6
+# From https://github.com/rails/rails/issues/34790
 class ActionController::TestResponse < ActionDispatch::TestResponse
   def recycle!
     # hack to avoid MonitorMixin double-initialize error:
