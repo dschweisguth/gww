fb8aaf617b33d4f4d5232784eb92d370809f1283
Dave Schweisguth <dave@schweisguth.org>
2021-04-23 20:21:36 -0700

    Update to ruby 2.6.7

---
+++ spec/support/ruby_26.rb
@@ -0,0 +1,9 @@
+# https://github.com/rails/rails/issues/34790
+class ActionController::TestResponse < ActionDispatch::TestResponse
+  def recycle!
+    # hack to avoid MonitorMixin double-initialize error:
+    @mon_mutex_owner_object_id = nil
+    @mon_mutex = nil
+    initialize
+  end
+end
