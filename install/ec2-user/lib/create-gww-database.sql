3087823f59dbca979d059db6364d5596c4e5fd51
Dave Schweisguth <dave@schweisguth.org>
2021-05-25 18:30:56 -0700

    Allow production user to use mysqldump
    Also, replace "all privileges" with the equivalent "all"

--- install/ec2-user/lib/create-gww-database.sql
+++ install/ec2-user/lib/create-gww-database.sql
@@ -1,3 +1,4 @@
 create user gww_prod identified by 'gww_prod';
-grant all privileges on gww_prod.* to gww_prod;
+grant all on gww_prod.* to gww_prod;
+grant process on *.* to gww_prod; # needed for mysqldump
 create database gww_prod collate utf8mb4_unicode_ci;
