1f18004a9f6076c0d8c2507480d1f7325bae4395
Dave Schweisguth <dave@schweisguth.org>
2024-10-25 04:17:48 -0700

    Correct spelling of custom capybara driver name

--- features/support/drivers.rb
+++ features/support/drivers.rb
@@ -8,7 +8,7 @@ if File.exist? firefox_path
 end
 # Register driver constructed with capabilities: rather than deprecated options:
 capabilities = Selenium::WebDriver::Firefox::Options.new.tap { |opts| opts.add_argument '-headless' }
-Capybara.register_driver :endorsed_selenum_headless do |app|
+Capybara.register_driver :endorsed_selenium_headless do |app|
   Capybara::Selenium::Driver.new(app, browser: :firefox, capabilities: capabilities)
 end
-Capybara.javascript_driver = :endorsed_selenum_headless
+Capybara.javascript_driver = :endorsed_selenium_headless
