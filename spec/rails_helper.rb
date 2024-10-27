6a025acf65b055925813edf52ace307d41243c4c
Dave Schweisguth <dave@schweisguth.org>
2024-10-27 08:53:47 -0700

    Enable Style/ExpandPathArguments, fix violations

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 
 require 'simplecov'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
