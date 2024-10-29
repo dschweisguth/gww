7b8f8f086c03f5b9606b37aad359d7d1d227d9ed
Dave Schweisguth <dave@schweisguth.org>
2024-10-29 05:01:29 -0700

    Refactor BookmarkletController#show

--- app/controllers/bookmarklet_controller.rb
+++ app/controllers/bookmarklet_controller.rb
@@ -1,35 +1,44 @@
 class BookmarkletController < ApplicationController
   def show
     from = params[:from]
-    path = root_path
-    message = nil
     if from =~ %r{^https?://www.flickr.com/photos/[^/]+/(\d+)}
-      flickrid = Regexp.last_match[1]
-      photo = Photo.find_by_flickrid flickrid
-      if photo
-        path = photo_path photo
-      else
-        message = "Sorry, Guess Where Watcher doesn't know anything about " \
-          "that photo. Perhaps it hasn't been added to Guess Where SF, " \
-          "or perhaps GWW hasn't updated since it was added. " \
-          "If you like, you can <a href=\"#{from}\">go back where you came from</a>."
-      end
+      redirect_to_photo Regexp.last_match[1], from
     elsif from =~ %r{^https?://www.flickr.com/(?:people|photos)/([^/]+)}
-      person_identifier = Regexp.last_match[1]
-      person = Person.find_by_pathalias(person_identifier) || Person.find_by_flickrid(person_identifier)
-      if person
-        path = person_path person
-      else
-        message = "Sorry, Guess Where Watcher doesn't know anything about that person. " \
-          "It might be that their custom URL is different than their username. " \
-          "Or perhaps they haven't posted or guessed in Guess Where SF, or GWW hasn't updated since they did. " \
-          "If you like, you can search for them using the box below, or <a href=\"#{from}\">go back where you came from</a>."
-      end
+      redirect_to_person Regexp.last_match[1], from
     else
-      message = "Hmmm, that's strange. #{from} isn't a Flickr photo or person page. " \
+      report_error "Hmmm, that's strange. #{from} isn't a Flickr photo or person page. " \
         "How did we get here? If you like, you can <a href=\"#{from}\">go back where you came from</a>."
     end
+  end
+
+  private
+
+  def redirect_to_photo(flickrid, from)
+    photo = Photo.find_by_flickrid flickrid
+    if photo
+      redirect_to photo_path photo
+    else
+      report_error "Sorry, Guess Where Watcher doesn't know anything about " \
+        "that photo. Perhaps it hasn't been added to Guess Where SF, " \
+        "or perhaps GWW hasn't updated since it was added. " \
+        "If you like, you can <a href=\"#{from}\">go back where you came from</a>."
+    end
+  end
+
+  def redirect_to_person(person_identifier, from)
+    person = Person.find_by_pathalias(person_identifier) || Person.find_by_flickrid(person_identifier)
+    if person
+      redirect_to person_path person
+    else
+      report_error "Sorry, Guess Where Watcher doesn't know anything about that person. " \
+        "It might be that their custom URL is different than their username. " \
+        "Or perhaps they haven't posted or guessed in Guess Where SF, or GWW hasn't updated since they did. " \
+        "If you like, you can search for them using the box below, or <a href=\"#{from}\">go back where you came from</a>."
+    end
+  end
+
+  def report_error(message)
     flash[:general_error] = message
-    redirect_to path
+    redirect_to root_path
   end
 end
