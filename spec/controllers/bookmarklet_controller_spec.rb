31ef0ce17ef24b6cc42916ab03bf34a42e24fa7f
Dave Schweisguth <dave@schweisguth.org>
2024-10-29 04:54:15 -0700

    Reframe and update bookmarklet spec

--- spec/controllers/bookmarklet_controller_spec.rb
+++ spec/controllers/bookmarklet_controller_spec.rb
@@ -1,6 +1,5 @@
 describe BookmarkletController do
   describe '#show' do
-    # This test is probably obsolete, in that Flickr seems to always use https now. But leave it in for a while just in case.
     it "redirects to the given photo" do
       photo = build_stubbed :photo, flickrid: '0123456789' # must be all digits like the real thing
       allow(Photo).to receive(:find_by_flickrid).with(photo.flickrid) { photo }
@@ -10,10 +9,11 @@ describe BookmarkletController do
 
     end
 
-    it "handles https when redirecting to a photo" do
+    # Used in development
+    it "handles http when redirecting to a photo" do
       photo = build_stubbed :photo, flickrid: '0123456789' # must be all digits like the real thing
       allow(Photo).to receive(:find_by_flickrid).with(photo.flickrid) { photo }
-      get :show, from: url_for_flickr_photo(photo)
+      get :show, from: "http://www.flickr.com/photos/#{photo.person.identifier}/#{photo.flickrid}/"
 
       expect(response).to redirect_to photo_path photo
 
@@ -21,14 +21,13 @@ describe BookmarkletController do
 
     it "punts an unknown photo Flickr ID" do
       allow(Photo).to receive(:find_by_flickrid).with('0123456789') { nil }
-      get :show, from: 'http://www.flickr.com/photos/person_flickrid/0123456789/'
+      get :show, from: 'https://www.flickr.com/photos/person_flickrid/0123456789/'
 
       expect(response).to redirect_to root_path
       expect(flash[:general_error]).to match(/Sorry, Guess Where Watcher doesn't know anything about that photo/)
 
     end
 
-    # This test is probably obsolete, in that Flickr seems to always use https now. But leave it in for a while just in case.
     it "redirects to the given person" do
       person = build_stubbed :person, pathalias: 'pathalias'
       allow(Person).to receive(:find_by_pathalias).with(person.pathalias) { person }
@@ -38,10 +37,11 @@ describe BookmarkletController do
 
     end
 
-    it "handles https when redirecting to a person" do
+    # Used in development
+    it "handles http when redirecting to a person" do
       person = build_stubbed :person, pathalias: 'pathalias'
       allow(Person).to receive(:find_by_pathalias).with(person.pathalias) { person }
-      get :show, from: url_for_flickr_person(person)
+      get :show, from: "http://www.flickr.com/people/#{person.identifier}/"
 
       expect(response).to redirect_to person_path person
 
@@ -78,7 +78,7 @@ describe BookmarkletController do
     end
 
     it "punts unknown URLs" do
-      get :show, from: 'http://www.notflickr.com/'
+      get :show, from: 'https://www.notflickr.com/'
 
       expect(response).to redirect_to root_path
       expect(flash[:general_error]).to match(/Hmmm/)
