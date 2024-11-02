b6b0774d42da1dcefc5faefb12fb1d3155a7bd1f
Dave Schweisguth <dave@schweisguth.org>
2024-11-01 17:19:39 -0700

    Switch ESLint config to old style so that Code Climate uses it

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: 'eslint:recommended',
+  env: {
+    es2024: true,
+    browser: true,
+    jquery: true
+  },
+  globals: {
+    google: true,
+    GWW: true
+  }
+};
