c1b91c10caa63f18346fe129c70637ef8bd3765b
Dave Schweisguth <dave@schweisguth.org>
2024-12-10 19:26:27 -0800

    Correct minor style issues in bookmarklets

--- app/assets/javascripts/admin/bookmarklet.js
+++ app/assets/javascripts/admin/bookmarklet.js
@@ -1,13 +1,13 @@
-// http://juhukinners.wordpress.com/2009/01/08/how-to-write-a-bookmarklet/
+// https://juhukinners.wordpress.com/2009/01/08/how-to-write-a-bookmarklet/
 (function () {
   "use strict";
 
   $(function() {
-    $('#bookmarklet').attr('href', 'javascript:(' + loadBookmarklet.toString() + ')();');
+    $('#bookmarklet').attr('href', `javascript:(${loadBookmarklet.toString()})();`);
   });
 
-  var loadBookmarklet = function () {
-    var script = document.createElement('script');
+  const loadBookmarklet = function () {
+    const script = document.createElement('script');
     script.src = '//guesswheresf.org/javascripts/bookmarklets/editInGWW.js';
     script.type = 'text/javascript';
     document.body.appendChild(script);
